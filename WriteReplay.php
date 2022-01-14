d15115eefdcc6698852f558b02438a2866f0aea0
ChaosGTR <gregoppenheimer95@gmail.com>
2022-01-13 22:27:27 -0800

    Errors Repaired

--- WriteReplay.php
+++ WriteReplay.php
@@ -5,23 +5,15 @@
   $gameName=$_GET["gameName"];
   $playerID=$_GET["playerID"];
   $stepCounter;
-
   function WriteReplay($playerID, $cardID, $from, $destination)
   {
     global $gameName, $stepCounter;
     $filename = "./Games/" . $gameName . "/Replay.txt";
     $handler = fopen($filename, "a+");
 
+  $output = ($stepCounter . " ID" . $playerID . " " . $cardID . " ". $from . " ". $destination);
 
-    if(filesize($filename)==0)
-    {
-        $zoneTransitions=array();
-        $stepCounter = 0;
-    }
-    $stepCounter = $stepCounter+1;
 
-    $output = ($stepCounter . " ID" . $playerID . " " . $cardID . " ". $from . " ". $destination);
-    array_push($zoneTransitions, $output);
     fwrite($handler, $output . "\r\n");
     fclose($handler);
   }
