10ad89a0cb7fa0b1038d1683a6eb8f3f1fc99b52
OotTheMonk <nathanandrecoder@gmail.com>
2022-03-26 19:33:37 -0500

    Make player text bold

--- SubmitChat.php
+++ SubmitChat.php
@@ -10,7 +10,7 @@
 
   $filename = "./Games/" . $gameName . "/gamelog.txt";
   $handler = fopen($filename, "a");
-  $output = "<span style='color:<PLAYER" . $playerID . "COLOR>;'>Player " . $playerID . ": </span>" . $chatText;
+  $output = "<span style='font-weight:bold; color:<PLAYER" . $playerID . "COLOR>;'>Player " . $playerID . ": </span>" . $chatText;
   fwrite($handler, $output . "\r\n");
   fclose($handler);
 ?>
