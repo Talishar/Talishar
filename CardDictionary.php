f9b91bc01d8a7094cebc12105f1c3a83784d1096
OotTheMonk <nathanandrecoder@gmail.com>
2022-04-23 09:24:34 -0500

    Scour fix and dynpitch game term

--- CardDictionary.php
+++ CardDictionary.php
@@ -794,7 +794,7 @@
       case "ARC009": return "0,2,4,6,8,10,12";
       case "MON231": return "0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40";
       case "EVR022": return "3,4,5,6,7,8,9,10,11,12";
-      case "EVR124": return GetIndices(SearchCount(SearchAura(($currentPlayer == 1 ? 2 : 1), "", "", 0)));
+      case "EVR124": return GetIndices(SearchCount(SearchAura(($currentPlayer == 1 ? 2 : 1), "", "", 0))+1);
       default:
         return "";
     }
