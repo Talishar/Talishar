eb3dfc61e64bea5ac020730a4e557c36954b1232
PvtVoid <jotrickr@gmail.com>
2022-05-23 14:56:51 +0200

    Create profile.php
    Under construction

---
+++ profile.php
@@ -0,0 +1,15 @@
+<?php
+  require 'header.php';
+?>
+
+<main>
+    <section class="signup-form">
+      <h2>Under construction!</h2>
+      <div class="signup-form-form">
+        <a href="MainMenu.php">Return to the Main Menu</a>
+  </div>
+</main>
+
+<?php
+  require 'footer.php';
+?>
