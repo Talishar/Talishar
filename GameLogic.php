37f42f377e690353278e08585755d2bededbc1e1
OotTheMonk <nathanandrecoder@gmail.com>
2022-05-28 08:39:45 -0500

    Add break

--- GameLogic.php
+++ GameLogic.php
@@ -2499,6 +2499,7 @@ function EquipPayAdditionalCosts($cardIndex, $from)
       break;
     case "DVR004": case "RVD004":
         DestroyCharacter($currentPlayer, $cardIndex);
+        break;
   }
 }
 
