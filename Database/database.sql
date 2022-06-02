5925b76f2780ef05af809ccc548dba389c76dcd5
PvtVoid <jotrickr@gmail.com>
2022-06-02 08:28:16 +0200

    Beginning Style NextTurn

---
+++ Database/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+  usersId int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  usersUid varchar(128) NOT NULL,
+  usersEmail varchar(128) NOT NULL,
+  usersPwd varchar(128) NOT NULL
+);
--- database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-  usersId int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  usersUid varchar(128) NOT NULL,
-  usersEmail varchar(128) NOT NULL,
-  usersPwd varchar(128) NOT NULL
-);
