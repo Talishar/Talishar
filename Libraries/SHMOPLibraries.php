dc3e9f69105aef6535518e13e61bca4ec63edf45
OotTheMonk <nathanandrecoder@gmail.com>
2022-07-02 19:56:10 -0500

    Some fixes, hero stats first commit

--- Libraries/SHMOPLibraries.php
+++ Libraries/SHMOPLibraries.php
@@ -11,6 +11,11 @@ function WriteCache($name, $data)
 function ReadCache($name)
 {
   $id = shmop_open($name, "a", 0, 0);
+  if($id == false)
+  {
+    WriteCache($name, "");
+    $id = shmop_open($name, "a", 0, 0);
+  }
   $data = shmop_read($id, 0, shmop_size($id));
   $data = preg_replace_callback( '!s:(\d+):"(.*?)";!', function($match) {
     return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
