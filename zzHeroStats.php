6f8a09d4f9fc9c35477bad985b9d954106474a00
OotTheMonk <nathanandrecoder@gmail.com>
2022-07-02 20:32:09 -0500

    Hero stats page

--- zzHeroStats.php
+++ zzHeroStats.php
@@ -38,7 +38,7 @@ ORDER BY Count";
 
         $sql = "SELECT WinningHero,LosingHero,count(LosingHero) AS Count
     FROM completedgame
-    WHERE WinningHero=\"DUMMY\" and LosingHero<>\"$heroID\"
+    WHERE WinningHero<>\"DUMMY\" and LosingHero=\"$heroID\"
     GROUP by WinningHero
     ORDER BY Count";
       	$stmt = mysqli_stmt_init($conn);
@@ -55,39 +55,44 @@ ORDER BY Count";
       	$loseData = mysqli_stmt_get_result($stmt);
 
 
-  $blitzPlays = 0;
-  $ccPlays = 0;
   $gameData = [];
   while ($row = mysqli_fetch_array($winData, MYSQLI_NUM)) {
-    echo($row[0] . " " . $row[1] . " " . $row[2] . "<br>");
-    /*
     array_push($gameData, []);
     $index = count($gameData)-1;
-    $gameData[$index][0] = $row[0];
-    $gameData[$index][1] = $row[1];
-    */
-  }
-/*
-  while ($row = mysqli_fetch_array($winData, MYSQLI_NUM)) {
-    $heroID = $row[0];
-    for($i=0; $i<count($gameData) && $gameData[$i][0] != $heroID; ++$i);
-    array_push($gameData[$i], $row[1]);
+    $gameData[$index][0] = $row[1];
+    $gameData[$index][1] = $row[2];
+    $gameData[$index][2] = 0;
   }
 
+  while ($row = mysqli_fetch_array($loseData, MYSQLI_NUM)) {
+    $heroID = $row[0];
+    for($i=0; $i<count($gameData) && $gameData[$i][0] != $heroID; ++$i);
+    //$value = (count($row) < 3 ? 0 : $row[2]);
+    if($i < count($gameData))
+    {
+      $gameData[$i][2] = $row[2];
+    }
+    else {
+      array_push($gameData, []);
+      $index = count($gameData)-1;
+      $gameData[$index][0] = $row[0];
+      $gameData[$index][1] = 0;//If we get here, there were no wins
+      $gameData[$index][2] = $row[2];
+    }
+  }
+
+
   echo("<table>");
-  echo("<tr><td>Hero</td><td>Num Wins</td><td>Num Plays</td><td>Win %</td><td>Played %</td></tr>");
+  echo("<tr><td>Opposing Hero</td><td>Num Wins</td><td>Num Losses</td><td>Win %</td></tr>");
 
   foreach ($gameData as $row) {
   //while ($row = mysqli_fetch_array($playData, MYSQLI_NUM)) {
     echo("<tr>");
-    echo("<td>" . CardLink($row[0], $row[0]) . "</td>");
-    echo("<td>" . (count($row) > 2 ? $row[2] : 0) . "</td>");
+    echo("<td><a href='./zzHeroStats.php?heroID=$row[0]'>" . CardLink($row[0], $row[0]) . "</a></td>");
     echo("<td>" . $row[1] . "</td>");
-    echo("<td>" . (((count($row) > 2 ? $row[2] : 0) / $row[1]) * 100) . "% </td>");
-    $formatDenominator = (CharacterHealth($row[0]) > 25 ? $ccPlays : $blitzPlays);
-    echo("<td>" . ($row[1] / $formatDenominator * 100) . "% </td>");
+    echo("<td>" . $row[2] . "</td>");
+    echo("<td>" . (($row[1] / ($row[1] + $row[2])) * 100) . "% </td>");
     echo("</tr>");
   }
   echo("</table>");
-*/
 ?>
