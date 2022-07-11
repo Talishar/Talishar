db7d0775a23c80fbd017a8ed90fa823cad5f3a88
OotTheMonk <nathanandrecoder@gmail.com>
2022-07-10 19:07:12 -0500

    Hero stats card relative win rate

--- zzHeroStats.php
+++ zzHeroStats.php
@@ -17,7 +17,7 @@ echo("<div>Detailed stats for " . CardLink($heroID, $heroID) . "</div>");
 
 
 
-    $sql = "SELECT WinningHero,LosingHero,count(WinningHero) AS Count
+    $sql = "SELECT WinningHero,LosingHero,count(WinningHero) AS Count,WinnerDeck
 FROM completedgame
 WHERE WinningHero=\"$heroID\" and LosingHero<>\"DUMMY\"
 GROUP by LosingHero
@@ -36,7 +36,7 @@ ORDER BY Count";
   	$winData = mysqli_stmt_get_result($stmt);
 
 
-        $sql = "SELECT WinningHero,LosingHero,count(LosingHero) AS Count
+        $sql = "SELECT WinningHero,LosingHero,count(LosingHero) AS Count,LoserDeck
     FROM completedgame
     WHERE WinningHero<>\"DUMMY\" and LosingHero=\"$heroID\"
     GROUP by WinningHero
@@ -56,12 +56,32 @@ ORDER BY Count";
 
 
   $gameData = [];
+  $cardData = [];
   while ($row = mysqli_fetch_array($winData, MYSQLI_NUM)) {
     array_push($gameData, []);
     $index = count($gameData)-1;
     $gameData[$index][0] = $row[1];
     $gameData[$index][1] = $row[2];
     $gameData[$index][2] = 0;
+
+    //Parse Cards
+    $deck = explode("\r\n", $row[3]);
+    if(count($deck) == 1) $deck = explode("\n", $row[3]);
+    if(count($deck) == 1) continue;
+    $character = explode(" ", $deck[0]);
+    $cards = explode(" ", $deck[1]);
+    for($i=0; $i<count($cards); ++$i)
+    {
+      $card = $cards[$i];
+      if(!array_key_exists($card, $cardData))
+      {
+        $cardData[$card] = [];
+        $cardData[$card][0] = 0;
+        $cardData[$card][1] = 0;
+      }
+      ++$cardData[$card][0];
+      ++$cardData[$card][1];
+    }
   }
 
   while ($row = mysqli_fetch_array($loseData, MYSQLI_NUM)) {
@@ -79,12 +99,33 @@ ORDER BY Count";
       $gameData[$index][1] = 0;//If we get here, there were no wins
       $gameData[$index][2] = $row[2];
     }
+
+    //Parse Cards
+    $deck = explode("\r\n", $row[3]);
+    if(count($deck) == 1) $deck = explode("\n", $row[3]);
+    if(count($deck) == 1) continue;
+    $character = explode(" ", $deck[0]);
+    $cards = explode(" ", $deck[1]);
+    for($i=0; $i<count($cards); ++$i)
+    {
+      $card = $cards[$i];
+      if(!array_key_exists($card, $cardData))
+      {
+        $cardData[$card] = [];
+        $cardData[$card][0] = 0;
+        $cardData[$card][1] = 0;
+      }
+      ++$cardData[$card][1];
+    }
   }
 
 
+  echo("<div style='display:inline-block;'>");
   echo("<table>");
   echo("<tr><td>Opposing Hero</td><td>Num Wins</td><td>Num Losses</td><td>Win %</td></tr>");
 
+  $totalWins = 0;
+  $totalGames = 0;
   foreach ($gameData as $row) {
   //while ($row = mysqli_fetch_array($playData, MYSQLI_NUM)) {
     echo("<tr>");
@@ -93,6 +134,47 @@ ORDER BY Count";
     echo("<td>" . $row[2] . "</td>");
     echo("<td>" . (($row[1] / ($row[1] + $row[2])) * 100) . "% </td>");
     echo("</tr>");
+    $totalWins += $row[1];
+    $totalGames += $row[1] + $row[2];
   }
   echo("</table>");
+  echo("</div>");
+
+  $totalWinrate = $totalWins / $totalGames;
+
+  $sortedCardData = [];
+  while(count($cardData) > 0)
+  {
+    $maxWinrate = -1;
+    $bestKey = "";
+    foreach($cardData as $key => $card)
+    {
+      $winRate = $card[0] / $card[1];
+      if($winRate > $maxWinrate)
+      {
+        $maxWinrate = $winRate;
+        $bestKey = $key;
+      }
+    }
+    $sortedCardData[$bestKey] = [];
+    $sortedCardData[$bestKey][0] = $cardData[$bestKey][0];
+    $sortedCardData[$bestKey][1] = $cardData[$bestKey][1];
+    unset($cardData[$bestKey]);
+  }
+
+  echo("<div style='display:inline-block;'>");
+  echo("<table>");
+  echo("<tr><td>Card</td><td>Num Plays</td><td>Win Rate</td><td>Relative Win Rate</td></tr>");
+  foreach($sortedCardData as $key => $card)
+  {
+    if($card[1] < 3) continue;
+    echo("<tr>");
+    echo("<td>" . CardLink($key, $key) . "</td>");
+    echo("<td>" . $card[1] . "</td>");
+    echo("<td>" . number_format($card[0] / $card[1], 2, ".", "") . "</td>");
+    echo("<td>" . number_format((($card[0] / $card[1]) - $totalWinrate) * 100, 2, ".", "") . "%</td>");
+    echo("</tr>");
+  }
+  echo("</table>");
+  echo("</div>");
 ?>
