25f45564c9818d060dbc249bde9836921c809279
PvtVoid <jotrickr@gmail.com>
2022-07-13 13:17:57 +0200

    Show PlayerID for Spectators
    Would be better if we can show the player Username

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -703,6 +703,14 @@
     if($theirCharacter[$i+8] == 1) echo("<img title='Frozen' style='position:absolute; z-index:100; border-radius:5px; top:7px; left:7px; height:" . $cardHeight . "; width:" . $cardWidth . ";' src='./Images/frozenOverlay.png' />");
     if($theirCharacter[$i+6] == 1) echo("<img title='On Combat Chain' style='position:absolute; z-index:100; top:-25px; left:7px; width:" . $cardWidth . "' src='./Images/onChain.png' />");
     if($theirCharacter[$i+1] == 0) echo("<img title='Equipment Broken' style='position:absolute; z-index:100; width:" . $cardEquipmentWidth . "; bottom: 6px; left:16px;' src='./Images/brokenEquip.png' />");
+    //Show Player Name for the spectator
+    if($type == "C" && $playerID == 3)
+    {
+      echo("<div style='margin: 0px; top: 80%; left: 50%; margin-right: -50%; border-radius: 8px; width: 70px; text-align: center;
+      line-height: 18px; height: 18px; padding: 5px; border: 3px solid " . $borderColor . ";
+      transform: translate(-50%, -50%); position:absolute; z-index: 10; background:" . $backgroundColor . ";
+      font-size:18px; font-weight:650; color:" . $fontColor . "; text-shadow: 2px 0 0 " . $borderColor . ", 0 -2px 0 " . $borderColor . ", 0 2px 0 " . $borderColor . ", -2px 0 0 " . $borderColor . ";'>Player 1");
+    }
     echo("</div>");
   }
   echo("</div>");
@@ -863,6 +871,15 @@
     if($type == "C")
     {
       if(count($mySoul) > 0) echo("<div onclick='ShowPopup(\"mySoulPopup\");' style='cursor:pointer; position:absolute; top:-23px; left: 17px; height:20px; font-size:20; font-weight: 600; color: ".$fontColor."; text-shadow: 2px 0 0 ".$borderColor.", 0 -2px 0 ".$borderColor.", 0 2px 0 ".$borderColor.", -2px 0 0 ".$borderColor."; text-align:center;'>Soul: " . count($mySoul) . "</div>");
+
+      //Show Player Name for the spectator
+      if($playerID == 3)
+      {
+        echo("<div style='margin: 0px; top: 80%; left: 50%; margin-right: -50%; border-radius: 8px; width: 70px; text-align: center;
+        line-height: 18px; height: 18px; padding: 5px; border: 3px solid " . $borderColor . ";
+        transform: translate(-50%, -50%); position:absolute; z-index: 10; background:" . $backgroundColor . ";
+        font-size:18px; font-weight:650; color:" . $fontColor . "; text-shadow: 2px 0 0 " . $borderColor . ", 0 -2px 0 " . $borderColor . ", 0 2px 0 " . $borderColor . ", -2px 0 0 " . $borderColor . ";'>Player 2");
+      }
     }
     echo("</div>");
     echo("</div>");
