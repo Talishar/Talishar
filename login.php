d30f5edadeed4a20cb8c94b8ba767aac3c54cc3b
PvtVoid <jotrickr@gmail.com>
2022-07-14 11:09:19 +0200

    Fix Style

--- login.php
+++ login.php
@@ -5,20 +5,21 @@
 <section class="signup-form">
   <h2>Log In</h2>
   <div class="signup-form-form">
-    <form action="includes/login.inc.php" method="post">
+    <form action="includes/login.inc.php" method="post" style='overflow:hidden;'>
       <input type="text" name="uid" placeholder="Username...">
       <input type="password" name="pwd" placeholder="Password...">
-      <button type="submit" name="submit">Submit</button>
       <label style='display:inline;' for="rememberMe">Remember Me?</label>
       <input style='display:inline; width:16px;' type="checkbox" id="rememberMe" name="rememberMe" value="rememberMe">
+      <button type="submit" name="submit">Submit</button>
       <a href="ResetPassword.php">
         <button type="submit" name="reset-password">Forgot Password</input>
       </a>
     </form>
   </div>
 
-  <section class='signup-form' style='position:absolute; bottom:30px; right:30px;'>
+  <section class='signup-form' style='position:absolute; bottom:30px; right:30px; padding: 5px;'>
     <i>By using the Remember Me function, you consent to a cookie being stored in your browser for purpose of identifying your account on future visits.</i>
+  </section>
   </div>
   <?php
     // Error messages
