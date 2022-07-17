a5cd55859fe3951db73cc246b7eaac12a59d73ab
OotTheMonk <nathanandrecoder@gmail.com>
2022-07-17 11:49:33 -0500

    Fixed mentors freezing game

--- CardDictionaries/Monarch/MONIllusionist.php
+++ CardDictionaries/Monarch/MONIllusionist.php
@@ -301,7 +301,7 @@
       AddDecisionQueue("FINDINDICES", $player, "DECKSPEC");
       AddDecisionQueue("CHOOSEDECK", $player, "<-", 1);
       AddDecisionQueue("ADDARSENALFACEUP", $player, "DECK", 1);
-      AddDecisionQueue("SHUFFLEDECK", $currentPlayer, "-", 1);
+      AddDecisionQueue("SHUFFLEDECK", $player, "-", 1);
     }
     WriteLog($log . ".");
   }
--- CardDictionaries/Monarch/MONWarrior.php
+++ CardDictionaries/Monarch/MONWarrior.php
@@ -272,7 +272,7 @@
       AddDecisionQueue("FINDINDICES", $player, "DECKSPEC");
       AddDecisionQueue("CHOOSEDECK", $player, "<-", 1);
       AddDecisionQueue("ADDARSENALFACEUP", $player, "DECK", 1);
-      AddDecisionQueue("SHUFFLEDECK", $currentPlayer, "-", 1);
+      AddDecisionQueue("SHUFFLEDECK", $player, "-", 1);
     }
   }
 
