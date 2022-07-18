4138d6d9426dd4e4a3e9852028b255c831d9dc1a
PvtVoid <jotrickr@gmail.com>
2022-07-18 09:41:01 +0200

    Fix Sand Cover being able to prevent unpreventable damage

--- PermanentAbilities.php
+++ PermanentAbilities.php
@@ -72,15 +72,24 @@ function PermanentEndTurnAbilities()
 function PermanentTakeDamageAbilities($player, $damage, $type)
 {
   $permanents = &GetPermanents($player);
+  $otherPlayer = $player == 1 ? 1 : 2;
+  //CR 2.1 6.4.10f If an effect states that a prevention effect can not prevent the damage of an event, the prevention effect still applies to the event but its prevention amount is not reduced. Any additional modifications to the event by the prevention effect still occur.
+  $preventable = CanDamageBePrevented($otherPlayer, $damage, $type);
   for($i=count($permanents)-PermanentPieces(); $i>=0; $i-=PermanentPieces())
   {
     $remove = 0;
     if($damage <= 0) { $damage = 0; break; }
     switch($permanents[$i])
     {
-      case "UPR439": $damage -= 4; $remove = 1; break;
-      case "UPR440": $damage -= 3; $remove = 1; break;
-      case "UPR441": $damage -= 2; $remove = 1; break;
+      case "UPR439":
+        if($preventable) $damage -= 4;
+        $remove = 1; break;
+      case "UPR440":
+        if($preventable) $damage -= 3;
+        $remove = 1; break;
+      case "UPR441":
+        if($preventable) $damage -= 2;
+        $remove = 1; break;
       default: break;
     }
     if($remove == 1)
