7c3ae2368546a37093890713a6e91e9d79f9a3fa
PvtVoid <jotrickr@gmail.com>
2022-07-19 10:36:03 +0200

    Fix Ghostly Touch getting destroyed when another ally was getting destroyed

--- AllyAbilities.php
+++ AllyAbilities.php
@@ -21,7 +21,6 @@ function DestroyAlly($player, $index, $skipDestroy=false)
   $allies = &GetAllies($player);
   if(!$skipDestroy)
   {
-    AllyDestroyAbility($player, $allies[$index]);
     AllyDestroyedAbility($player, $index);
   }
   if(IsSpecificAllyAttacking($player, $index)) { CloseCombatChain(); }
@@ -59,7 +58,7 @@ function AllyDestroyedAbility($player, $index)
   global $mainPlayer;
   $allies = &GetAllies($player);
   $cardID = $allies[$index];
-  switch($allies[$i])
+  switch($cardID)
   {
     case "UPR410":
       if($player == $mainPlayer && $allies[$i+8] > 0)
@@ -69,25 +68,15 @@ function AllyDestroyedAbility($player, $index)
         --$allies[$i+8];
       }
       break;
+    case "UPR551":
+      $gtIndex = FindCharacterIndex($player, "UPR151");
+      WriteLog($gtIndex);
+      if($gtIndex > -1)
+      {
+        DestroyCharacter($player, $gtIndex);
+      }
+      break;
     default: break;
-  }
-}
-
-function AllyDestroyAbility($player, $cardID)
-{
-  $allies = &GetAllies($player);
-  for($i=0; $i<count($allies); $i+=AllyPieces())
-  {
-    switch($allies[$i])
-    {
-      case "UPR551":
-        $gtIndex = FindCharacterIndex($player, "UPR151");
-        if($gtIndex > -1)
-        {
-          DestroyCharacter($player, $gtIndex);
-        }
-        default: break;
-    }
   }
 }
 
