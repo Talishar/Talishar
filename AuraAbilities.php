19b9b0e2bc25f1b55767032d4b1ba42126e61cd2
PvtVoid <jotrickr@gmail.com>
2022-07-29 09:29:26 +0200

    Revert Embodiment of Lightning. I don't think it's relevant.

--- AuraAbilities.php
+++ AuraAbilities.php
@@ -449,7 +449,7 @@ function AuraAttackAbilities($attackID)
     {
       case "WTR225": if($attackType == "AA" || $attackType == "W") { WriteLog("Quicken grants go again."); GiveAttackGoAgain(); $remove = 1; } break;
       case "ARC112": if($attackType == "AA" || $attackType == "W") { DealArcane(1, 1, "RUNECHANT", "ARC112"); $remove = 1; } break;
-      case "ELE110": if($attackType == "AA") { WriteLog("Embodiment of Lightning grants go again."); AddLayer("TRIGGER", $mainPlayer, $auras[$i], "-", "-", $auras[$i+6]); $remove = 1; } break;
+      case "ELE110": if($attackType == "AA") { WriteLog("Embodiment of Lightning grants go again."); GiveAttackGoAgain(); $remove = 1; } break;
       case "ELE226": if($attackType == "AA") DealArcane(1, 0, "PLAYCARD", $combatChain[0]); break;
       case "EVR140": if($auras[$i+5]>0 && DelimStringContains(CardSubtype($attackID), "Aura") && ClassContains($attackID, "ILLUSIONIST", $mainPlayer)) { WriteLog("Shimmers of Silver puts a +1 counter."); --$auras[$i+5]; ++$auras[GetClassState($mainPlayer, $CS_PlayIndex)+3]; } break;
       case "EVR142": if($auras[$i+5]>0 && ClassContains($attackID, "ILLUSIONIST", $mainPlayer)) { WriteLog("Passing Mirage makes your next attack lose Phantasm."); --$auras[$i+5]; AddCurrentTurnEffect("EVR142", $mainPlayer, true); } break;
