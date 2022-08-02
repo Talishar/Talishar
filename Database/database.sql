42fb11fa3981c5bfce50391c32f3936fbe8c5f38
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-02 07:59:15 -0500

    Add completed game table sql

--- Database/database.sql
+++ Database/database.sql
@@ -15,4 +15,28 @@ CREATE TABLE `favoritedeck` (
 ALTER TABLE `favoritedeck`
   ADD PRIMARY KEY (`decklink`),
   ADD KEY `usersId` (`usersId`);
-COMMIT;
+
+CREATE TABLE `completedgame` (
+  `CompletionTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `WinningHero` char(6) NOT NULL,
+  `LosingHero` char(6) NOT NULL,
+  `WinningPID` int(11) DEFAULT NULL,
+  `LosingPID` int(11) DEFAULT NULL,
+  `WinnerHealth` int(11) DEFAULT NULL,
+  `NumTurns` int(11) NOT NULL,
+  `Format` int(11) DEFAULT NULL,
+  `GameID` int(22) NOT NULL,
+  `WinnerDeck` varchar(1000) DEFAULT NULL,
+  `LoserDeck` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ALTER TABLE `completedgame`
+  ADD PRIMARY KEY (`GameID`),
+  ADD KEY `FK_WINNING_PLAYER` (`WinningPID`),
+  ADD KEY `FK_LOSING_PLAYER` (`LosingPID`);
+
+ALTER TABLE `completedgame`
+  MODIFY `GameID` int(22) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+
+ALTER TABLE `completedgame`
+  ADD CONSTRAINT `FK_LOSING_PLAYER` FOREIGN KEY (`LosingPID`) REFERENCES `users` (`usersID`),
+  ADD CONSTRAINT `FK_WINNING_PLAYER` FOREIGN KEY (`WinningPID`) REFERENCES `users` (`usersID`);
