931667e38c006343f6be1f9ba23c99a525b2b622
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-01 21:45:25 -0500

    Update database

--- Database/database.sql
+++ Database/database.sql
@@ -4,3 +4,15 @@ CREATE TABLE users (
   usersEmail varchar(128) NOT NULL,
   usersPwd varchar(128) NOT NULL
 );
+
+CREATE TABLE `favoritedeck` (
+  `decklink` varchar(128) NOT NULL,
+  `usersId` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `hero` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `favoritedeck`
+  ADD PRIMARY KEY (`decklink`),
+  ADD KEY `usersId` (`usersId`);
+COMMIT;
