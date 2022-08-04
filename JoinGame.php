b93f40e862653a56106d19db8d66117ca802ce79
PvtVoid <jotrickr@gmail.com>
2022-08-04 12:05:59 +0200

    Add more starter decks

--- JoinGame.php
+++ JoinGame.php
@@ -55,7 +55,6 @@ body {
   {
     echo("<div class='DeckToTry'>CC Starter Decks: ");
     echo("<select name='decksToTry' id='decksToTry'>");
-
       echo("<option value='1'>Bravo Starter Deck</option>");
       echo("<option value='2'>Rhinar Starter Deck</option>");
       echo("<option value='3'>Katsu Starter Deck</option>");
@@ -64,6 +63,15 @@ body {
       echo("<option value='6'>Viserai Starter Deck</option>");
       echo("<option value='7'>Kano Starter Deck</option>");
       echo("<option value='8'>Azalea Starter Deck</option>");
+      echo("<option value='9'>Prism Starter Deck</option>");
+      echo("<option value='10'>Levia Starter Deck</option>");
+      echo("<option value='11'>Boltyn Starter Deck</option>");
+      echo("<option value='12'>Chane Starter Deck</option>");
+      echo("<option value='13'>Oldhim Starter Deck</option>");
+      echo("<option value='14'>Briar Starter Deck</option>");
+      echo("<option value='15'>Lexi Starter Deck</option>");
+      echo("<option value='16'>Fai Starter Deck</option>");
+      echo("<option value='17'>Dromai Starter Deck</option>");
     echo("</select></div><br>");
   }
 
