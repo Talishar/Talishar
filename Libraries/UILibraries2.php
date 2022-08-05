a22639ac0e91c59a2a7d0ea693bf37635c8de033
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-04 20:57:10 -0500

    Trying to make counters dynamically scale a bit

--- Libraries/UILibraries2.php
+++ Libraries/UILibraries2.php
@@ -80,6 +80,8 @@
 
   // Counters Style
 
+  $counterHeight = IsDynamicScalingEnabled($playerID) ? intval($maxHeight / 4.6) : 28;
+  $steamCounterHeight = IsDynamicScalingEnabled($playerID) ? intval($maxHeight / 2.5) : 44;
   //Attacker Label Style
   if($counters == "Attacker" || $counters == "Arsenal") {
     $rv .= "<div style='margin: 0px; top: 80%; left: 50%;
@@ -90,47 +92,47 @@
   //Steam Counters style
   elseif ($counters != 0 && $from == "ITEMS" && ClassContains($cardNumber, "MECHANOLOGIST")) {
     if($lifeCounters == 0 && $defCounters == 0 && $atkCounters == 0){ $left = "50%"; } else { $left = "70%"; }
-    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width: 28px; height: 28px; padding: 2px;
+    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width:" . $counterHeight . "px; height:" . $counterHeight . "px; padding: 2px;
     text-align: center; transform: translate(-50%, -50%);
-    position:absolute; z-index: 5; font-size:26px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $counters ."
-    <img style='height: 44px; width: 44px; opacity: 0.9; position: absolute; margin: auto; top: 3px; left: -6px; right: 0px; bottom: 0px; -webkit-box-shadow:inset 0px 0px 0px 15px black;
+    position:absolute; z-index: 5; font-size:" . ($steamCounterHeight-18) . "px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $counters ."
+    <img style='width:" . $steamCounterHeight . "px; height:" . $steamCounterHeight . "px; opacity: 0.9; position: absolute; margin: auto; top: 3px; left: -6px; right: 0px; bottom: 0px; -webkit-box-shadow:inset 0px 0px 0px 15px black;
     -moz-box-shadow:inset 0px 0px 0px 15px black; box-shadow:inset 0px 0px 0px 15px black; border-radius: 50%; padding:-5; z-index:-3;' src='./Images/steamCounter.png'></img></div>";
   }
   //Equipments, Hero and default counters style
   elseif($counters != 0) {
     if($lifeCounters == 0 && $defCounters == 0 && $atkCounters == 0){ $left = "50%"; } else { $left = "70%"; }
     $rv .= "<div style='margin: 0px; top: 50%; left: $left;
-    margin-right: -50%; border-radius: 50%; width: 26px; height: 26px; padding: 5px; border: 3px solid " . PopupBorderColor($darkMode) . "; text-align: center;
+    margin-right: -50%; border-radius: 50%; width:" . $counterHeight . "px; height:" . $counterHeight . "px; padding: 5px; border: 3px solid " . PopupBorderColor($darkMode) . "; text-align: center;
     transform: translate(-50%, -50%); position:absolute; z-index: 10; background:" . BackgroundColor($darkMode) . ";
-    font-family: Helvetica; font-size:24px; font-weight:550; color:".TextCounterColor($darkMode)."; text-shadow: 2px 0 0 ".PopupBorderColor($darkMode).", 0 -2px 0 ".PopupBorderColor($darkMode).", 0 2px 0 ".PopupBorderColor($darkMode).", -2px 0 0 ".PopupBorderColor($darkMode).";'>" . $counters . "</div>";
+    font-family: Helvetica; font-size:" . ($counterHeight-1) . "px; font-weight:550; color:".TextCounterColor($darkMode)."; text-shadow: 2px 0 0 ".PopupBorderColor($darkMode).", 0 -2px 0 ".PopupBorderColor($darkMode).", 0 2px 0 ".PopupBorderColor($darkMode).", -2px 0 0 ".PopupBorderColor($darkMode).";'>" . $counters . "</div>";
   }
   //-1 Defense & Endurance Counters style
   if($defCounters != 0) {
     if($lifeCounters == 0 && $counters == 0){ $left = "50%"; } else { $left = "30%"; }
-    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width: 28px; height: 28px; padding: 2px;
+    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width:" . $counterHeight . "px; height:" . $counterHeight . "px; padding: 2px;
     text-align: center; transform: translate(-50%, -50%);
-    position:absolute; z-index: 5; font-size:26px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $defCounters ."
-    <img style='height: 41px; width: 41px; opacity: 0.8; position: absolute; margin: auto; top: 1px; left: -5px; right: 0px; bottom: 0px;
-    z-index:-3;' src='./Images/Defense.png'></img></div>";
+    position:absolute; z-index: 5; font-size:" . ($steamCounterHeight-18) . "px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $counters ."
+    <img style='width:" . $steamCounterHeight . "px; height:" . $steamCounterHeight . "px; opacity: 0.9; position: absolute; margin: auto; top: 3px; left: -6px; right: 0px; bottom: 0px; -webkit-box-shadow:inset 0px 0px 0px 15px black;
+    -moz-box-shadow:inset 0px 0px 0px 15px black; box-shadow:inset 0px 0px 0px 15px black; border-radius: 50%; padding:-5; z-index:-3;' src='./Images/Defense.png'></img></div>";
   }
   //Health Counters style
   if($lifeCounters != 0){
     if($defCounters == 0){ $left = "50%"; } else { $left = "70%"; }
-  $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width: 28px; height: 28px; padding: 2px;
-  text-align: center; transform: translate(-50%, -50%);
-  position:absolute; z-index: 5; font-size:26px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $lifeCounters ."
-  <img style='height: 41px; width: 41px; opacity: 0.8; position: absolute; margin: auto; top: 1px; left: -5px; right: 0px; bottom: 0px;
-  z-index:-3;' src='./Images/Life.png'></img></div>";
+    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width:" . $counterHeight . "px; height:" . $counterHeight . "px; padding: 2px;
+    text-align: center; transform: translate(-50%, -50%);
+    position:absolute; z-index: 5; font-size:" . ($steamCounterHeight-18) . "px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $counters ."
+    <img style='width:" . $steamCounterHeight . "px; height:" . $steamCounterHeight . "px; opacity: 0.9; position: absolute; margin: auto; top: 3px; left: -6px; right: 0px; bottom: 0px; -webkit-box-shadow:inset 0px 0px 0px 15px black;
+    -moz-box-shadow:inset 0px 0px 0px 15px black; box-shadow:inset 0px 0px 0px 15px black; border-radius: 50%; padding:-5; z-index:-3;' src='./Images/Life.png'></img></div>";
   }
 
   //Attack Counters style
   if($atkCounters != 0) {
     if($lifeCounters == 0 && $counters == 0){ $left = "50%"; } else { $left = "30%"; }
-    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width: 28px; height: 28px; padding: 2px;
+    $rv .= "<div style='margin: 0; top: 50%; left: $left; margin-right: -50%; width:" . $counterHeight . "px; height:" . $counterHeight . "px; padding: 2px;
     text-align: center; transform: translate(-50%, -50%);
-    position:absolute; z-index: 5; font-size:26px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $atkCounters ."
-    <img style='height: 41px; width: 41px; opacity: 0.8; position: absolute; margin: auto; top: 1px; left: -5px; right: 0px; bottom: 0px;
-    z-index:-3;' src='./Images/Attack.png'></img></div>";
+    position:absolute; z-index: 5; font-size:" . ($steamCounterHeight-18) . "px; font-weight: 600; color: #EEE; text-shadow: 2px 0 0 #000, 0 -2px 0 #000, 0 2px 0 #000, -2px 0 0 #000;'>" . $counters ."
+    <img style='width:" . $steamCounterHeight . "px; height:" . $steamCounterHeight . "px; opacity: 0.9; position: absolute; margin: auto; top: 3px; left: -6px; right: 0px; bottom: 0px; -webkit-box-shadow:inset 0px 0px 0px 15px black;
+    -moz-box-shadow:inset 0px 0px 0px 15px black; box-shadow:inset 0px 0px 0px 15px black; border-radius: 50%; padding:-5; z-index:-3;' src='./Images/Attack.png'></img></div>";
   }
   $rv .= "</a>";
   return $rv;
