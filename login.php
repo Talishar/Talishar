9c023e85f3ee0e73d648c02c69b3f9b1dd0fd472
PvtVoid <jotrickr@gmail.com>
2022-08-08 14:12:43 +0200

    Style Google Login

--- login.php
+++ login.php
@@ -14,25 +14,17 @@ include_once 'Header.php';
       <input style='display:inline; width:16px;' type="checkbox" id="rememberMe" name="rememberMe" value="rememberMe">
       <button type="submit" name="submit">Submit</button>
       <a href="ResetPassword.php">
-        <button type="submit" name="reset-password">Forgot Password</input>
+        <button type="submit" name="reset-password">Forgot Password</button>
       </a>
+      <div class="centeredGoogle">
+        <div id="g_id_onload" data-client_id="1089547347578-lqbvoqb0u3grqi89ibjmti2bc9lq37pr.apps.googleusercontent.com" data-login_uri="http://localhost/FaBOnline/MainMenu.php" data-auto_prompt="false">
+        </div>
+        <div class="g_id_signin" data-type="standard" data-shape="pill" data-theme="outline" data-text="signin_with" data-size="large" data-logo_alignment="left">
+        </div>
+      </div>
     </form>
   </div>
 
-  <div id="g_id_onload"
-        data-client_id="1089547347578-lqbvoqb0u3grqi89ibjmti2bc9lq37pr.apps.googleusercontent.com"
-        data-login_uri="http://localhost/FaBOnline/MainMenu.php"
-        data-auto_prompt="false">
-      </div>
-
-  <div class="g_id_signin"
-      data-type="standard"
-      data-shape="pill"
-      data-theme="outline"
-      data-text="signin_with"
-      data-size="large"
-      data-logo_alignment="left">
-  </div>
 
   <section class='signup-form' style='position:absolute; bottom:30px; right:30px; padding: 5px;'>
     <i>By using the Remember Me function, you consent to a cookie being stored in your browser for purpose of identifying your account on future visits.</i>
