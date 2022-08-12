9d671c774f991bc48a2827f96582e086bff7569e
PvtVoid <jotrickr@gmail.com>
2022-08-12 12:03:19 +0200

    Working Restricted by Reputation feature

--- MenuFiles/WriteGamefile.php
+++ MenuFiles/WriteGamefile.php
@@ -9,7 +9,7 @@
     {
       global $gameFileHandler;
       global $p1Data, $p2Data, $gameStatus, $format, $visibility, $firstPlayerChooser, $firstPlayer, $p1Key, $p2Key, $p1uid, $p2uid, $p1id, $p2id;
-      global $gameDescription, $hostIP, $p1IsPatron, $p2IsPatron, $p1DeckLink, $p2DeckLink;
+      global $gameDescription, $hostIP, $p1IsPatron, $p2IsPatron, $p1DeckLink, $p2DeckLink, $karmaRestriction;
       rewind($gameFileHandler);
       fwrite($gameFileHandler, implode(" ", $p1Data) . "\r\n");
       fwrite($gameFileHandler, implode(" ", $p2Data) . "\r\n");
@@ -30,6 +30,7 @@
       fwrite($gameFileHandler, $p2IsPatron . "\r\n");
       fwrite($gameFileHandler, $p1DeckLink . "\r\n");
       fwrite($gameFileHandler, $p2DeckLink . "\r\n");
+      fwrite($gameFileHandler, $karmaRestriction . "\r\n");
       fclose($gameFileHandler);
     }
   }
