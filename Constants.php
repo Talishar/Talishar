68c6a76bf394111c2380b225fc56dfd477e9f012
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-12 22:20:05 -0500

    Make rites of lightning gain go again off self arcane damage

--- Constants.php
+++ Constants.php
@@ -358,7 +358,7 @@ function ResetMainClassState()
   global $CS_HitsWithWeapon, $CS_ArcaneDamagePrevention, $CS_DynCostResolved, $CS_CardsEnteredGY;
   global $CS_HighestRoll, $CS_EffectContext, $CS_NumAuras, $CS_AbilityIndex, $CS_AdditionalCosts, $CS_NumRedPlayed, $CS_PlayUniqueID, $CS_AlluvionUsed;
   global $CS_NumPhantasmAADestroyed, $CS_NumLess3PowAAPlayed, $CS_MaxQuellUsed, $CS_DamageDealt, $CS_ArcaneTargetsSelected, $CS_NumDragonAttacks, $CS_NumIllusionistAttacks;
-  global $CS_LastDynCost, $CS_NumIllusionistActionCardAttacks;
+  global $CS_LastDynCost, $CS_NumIllusionistActionCardAttacks, $CS_ArcaneDamageDealt;
 
   $mainClassState[$CS_Num6PowDisc] = 0;
   $mainClassState[$CS_NumBoosted] = 0;
@@ -416,6 +416,7 @@ function ResetMainClassState()
   $mainClassState[$CS_NumIllusionistAttacks] = 0;
   $mainClassState[$CS_LastDynCost] = 0;
   $mainClassState[$CS_NumIllusionistActionCardAttacks] = 0;
+  $mainClassState[$CS_ArcaneDamageDealt] = 0;
 }
 
 function ResetCardPlayed($cardID)
