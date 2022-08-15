4a80c65d69d112292b4f2e0a521ffec0cfa546a4
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-15 15:01:55 -0500

    Update to style3 to force reload

--- GameLobby.php
+++ GameLobby.php
@@ -3,7 +3,7 @@
   <title>Flesh and Blood Online</title>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/style2.css">
+  <link rel="stylesheet" href="css/style3.css">
 </head>
 
 <?php
