9c355e49dbb995782d84d0dd2c3eea74a4bcd2ac
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-18 16:59:40 -0500

    Fix not being to activate character abilities if they're on the combat chain

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -998,7 +998,7 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
       $restrictionName = CardName($restriction);
       echo ("<img title='Restricted by: " . ($restrictionName != "" ? $restrictionName : $restriction) . "' style='position:absolute; z-index:100; top:26px; left:26px;' src='./Images/restricted.png' />");
     }
-    if ($myCharacter[$i + 6] == 1) echo ("<img title='On Combat Chain' style='position:absolute; z-index:100; width:" . $cardWidth . "; bottom: 5px; left:7px;' src='./Images/onChain.png' />");
+    if ($myCharacter[$i + 6] == 1) echo ("<img title='On Combat Chain' style='pointer-events: none; position:absolute; z-index:100; width:" . $cardWidth . "; bottom: 5px; left:7px;' src='./Images/onChain.png' />");
     if ($myCharacter[$i + 1] == 0) echo ("<img title='Equipment Broken' style='position:absolute; z-index:100; width:" . $cardEquipmentWidth . "; bottom: 6px; left:16px;' src='./Images/brokenEquip.png' />");
     if ($myCharacter[$i + 8] == 1) echo ("<img title='Frozen' style='position:absolute; z-index:100; border-radius:5px; top:7px; left:7px; height:" . $cardHeight . "; width:" . $cardWidth . ";' src='./Images/frozenOverlay.png' />");
     if ($myCharacter[$i + 9] != 2 && $myCharacter[$i + 1] != 0 && $playerID != 3) {
