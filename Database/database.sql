bff59b6524f55ddbbf1e3983d81667f60613f526
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-21 15:02:11 -0500

    Add new tables for badges

--- Database/database.sql
+++ Database/database.sql
@@ -11,22 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fabonline`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `completedgame`
---
-
 CREATE TABLE `completedgame` (
   `CompletionTime` timestamp NOT NULL DEFAULT current_timestamp(),
   `WinningHero` char(6) NOT NULL,
@@ -158,3 +142,69 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 21, 2022 at 09:57 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `badges` (
+  `badgeId` int(11) NOT NULL,
+  `topText` varchar(128) DEFAULT NULL,
+  `bottomText` varchar(128) DEFAULT NULL,
+  `image` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `badges`
+--
+
+INSERT INTO `badges` (`badgeId`, `topText`, `bottomText`, `image`) VALUES
+(1, 'Commotion #1', 'Wins: {0}', './concat/WTR175.webp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `badges`
+--
+ALTER TABLE `badges`
+  ADD PRIMARY KEY (`badgeId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `badges`
+--
+ALTER TABLE `badges`
+  MODIFY `badgeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+
+CREATE TABLE `playerbadge` (
+  `playerId` int(11) NOT NULL,
+  `badgeId` int(11) NOT NULL,
+  `intVariable` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `playerbadge`
+--
+
+--
+-- Indexes for table `playerbadge`
+--
+ALTER TABLE `playerbadge`
+  ADD PRIMARY KEY (`playerId`,`badgeId`);
+COMMIT;
