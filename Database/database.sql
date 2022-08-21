972a60c93865bf04b3ebea0c8890ef36e8822516
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-21 15:04:12 -0500

    Clean up badge tables sql

--- Database/database.sql
+++ Database/database.sql
@@ -139,19 +139,6 @@ ALTER TABLE `completedgame`
   ADD CONSTRAINT `FK_WINNING_PLAYER` FOREIGN KEY (`WinningPID`) REFERENCES `users` (`usersID`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Aug 21, 2022 at 09:57 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -207,4 +194,3 @@ CREATE TABLE `playerbadge` (
 --
 ALTER TABLE `playerbadge`
   ADD PRIMARY KEY (`playerId`,`badgeId`);
-COMMIT;
