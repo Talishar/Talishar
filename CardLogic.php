1f2405bc5b45b2c7bfdf169fca2d33ab8297f7ef
PvtVoid <jotrickr@gmail.com>
2022-08-24 08:14:44 +0200

    Add PayorDiscard Trigger to bitting Gale

--- CardLogic.php
+++ CardLogic.php
@@ -624,6 +624,9 @@ function ProcessTrigger($player, $parameter, $uniqueID, $target="-")
     case "MON186":
       SoulShackleStartTurn($player);
       break;
+    case "ELE007": case "ELE008": case "ELE009":
+      PayOrDiscard($target, 2, true); 
+      break;
     case "ELE025": case "ELE026": case "ELE027":
       WriteLog(CardLink($parameter, $parameter) . " gives the next attack action card this turn +" . EffectAttackModifier($parameter) . ".");
       AddCurrentTurnEffect($parameter, $player);
