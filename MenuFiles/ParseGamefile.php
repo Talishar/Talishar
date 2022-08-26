d0a11091908d4ea05da4a289d4a3eb2f8a394cfb
OotTheMonk <nathanandrecoder@gmail.com>
2022-08-25 20:33:32 -0500

    Refine ip logging

--- MenuFiles/ParseGamefile.php
+++ MenuFiles/ParseGamefile.php
@@ -64,18 +64,17 @@
     }
   }
 
-  if(isset($playerID))
+  if(isset($playerID) && $gameStatus == $MGS_GameStarted)
   {
     $ipTarget = ($playerID == 1 ? $hostIP : $joinerIP);
     if($ipTarget != "" && $_SERVER['REMOTE_ADDR'] != $ipTarget)
     {
-
-        $hackFileName = "./BugReports/PossibleHackAttempts.txt";
-        $hackHandler = fopen($hackFileName, "a");
-        date_default_timezone_set('America/Chicago');
-        $errorDate = date('m/d/Y h:i:s a');
-        fwrite($hackHandler, "Hack Attempt? $errorDate Game name: $gameName IP: " . $_SERVER['REMOTE_ADDR'] . " Target IP: $ipTarget PlayerID: $playerID" . "\r\n");
-        fclose($hackHandler);
+      $hackFileName = "./BugReports/PossibleHackAttempts.txt";
+      $hackHandler = fopen($hackFileName, "a");
+      date_default_timezone_set('America/Chicago');
+      $errorDate = date('m/d/Y h:i:s a');
+      fwrite($hackHandler, "Hack Attempt? $errorDate Request: " . $_SERVER['REQUEST_URI'] . " Game name: $gameName IP: " . $_SERVER['REMOTE_ADDR'] . " Target IP: $ipTarget PlayerID: $playerID" . "\r\n");
+      fclose($hackHandler);
     }
   }
 
