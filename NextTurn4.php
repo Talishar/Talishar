0fb6aaf878435c03e2ae0ae0acb9288d88f9d9f2
PvtVoid <jotrickr@gmail.com>
2022-08-29 20:44:57 +0200

    Add CardWidth

--- NextTurn4.php
+++ NextTurn4.php
@@ -189,6 +189,7 @@
         if(gem != 0)
         {
           var playerID = <?php echo($playerID); ?>;
+          var cardWidth = 96;
           gemImg = (gem == 1 ? "hexagonRedGem.png" : "hexagonGrayGem.png");
           if (gem == 1) rv += "<img " + ProcessInputLink(playerID, 102, actionDataOverride) + " title='Effect Inactive' style='position:absolute; z-index:1001; bottom:3px; left:" + (cardWidth / 2 - 15) + "px; width:34px; height:34px; cursor:pointer;' src='./Images/" + gemImg + "' />";
           else if (gem == 2) rv += "<img " + ProcessInputLink(playerID, 102, actionDataOverride) + " title='Effect Active' style='position:absolute; z-index:1001; bottom:3px; left:" + (cardWidth / 2 - 15) + "px; width:34px; height:34px; cursor:pointer;' src='./Images/" + gemImg + "' />";
