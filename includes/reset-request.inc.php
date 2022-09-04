088d0b0cc7e08039dacbaf3c65c4e27c39e4c187
stetowns <stetowns@publicigroupe.net>
2022-09-04 23:47:18 +0100

    Added https to password reset, as this could be a reason for yahoo email links not working

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -7,7 +7,7 @@ if (isset($_POST['reset-request-submit'])) {
   $selector = bin2hex(random_bytes(8));
   $token = random_bytes(32);
 
-  $url = "www.fleshandbloodonline.com/FaBOnline/CreateNewPassword.php?selector=" . $selector . "&validator=" . bin2hex($token);
+  $url = "https://www.fleshandbloodonline.com/FaBOnline/CreateNewPassword.php?selector=" . $selector . "&validator=" . bin2hex($token);
   // $url = "www.localhost/FaBOnline/CreateNewPassword.php?selector=" . $selector . "&validator=" . bin2hex($token);
 
   $expires = date("U") + 1800;
