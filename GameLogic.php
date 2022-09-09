7e82b5ad2f1e3c46e686e93cf73dc6b47daa2808
PvtVoid <jotrickr@gmail.com>
2022-09-09 08:58:09 +0200

    Fix Icebind $player parameter

--- GameLogic.php
+++ GameLogic.php
@@ -190,31 +190,29 @@ function ProcessDealDamageEffect($cardID)
   }
 }
 
-function ArcaneHitEffect($me, $source, $target, $damage)
+function ArcaneHitEffect($player, $source, $target, $damage)
 {
+  $otherPlayer = ($player == 1 ? 2 : 1);
   switch ($source) {
     case "UPR104":
-      if (MZIsPlayer($target)) {
-        AddDecisionQueue("LESSTHANPASS", MZPlayerID($me, $target), 1, 1);
-        AddDecisionQueue("ENCASEDAMAGE", MZPlayerID($me, $target), "-", 1);
+      if (MZIsPlayer($target) && $damage > 0) {
+        AddDecisionQueue("ENCASEDAMAGE", MZPlayerID($player, $target), "-", 1);
       }
       break;
     case "UPR113": case "UPR114": case "UPR115":
-      if (MZIsPlayer($target)) PayOrDiscard(MZPlayerID($me, $target), 2, true);
+      if (MZIsPlayer($target)) PayOrDiscard(MZPlayerID($player, $target), 2, true);
       break;
     case "UPR119": case "UPR120": case "UPR121":
-      if (MZIsPlayer($target)) {
-        AddDecisionQueue("LESSTHANPASS", MZPlayerID($me, $target), 1, 1);
-        AddDecisionQueue("FINDINDICES", MZPlayerID($me, $target), "SEARCHMZ,THEIRARS", 1);
-        AddDecisionQueue("SETDQCONTEXT", MZPlayerID($me, $target), "Choose which card you want to freeze", 1);
-        AddDecisionQueue("CHOOSEMULTIZONE", MZPlayerID($me, $target), "<-", 1);
-        AddDecisionQueue("MZOP", MZPlayerID($me, $target), "FREEZE", 1);
+      if (MZIsPlayer($target) && $damage > 0) {
+        AddDecisionQueue("FINDINDICES", $player, "SEARCHMZ,THEIRARS", 1);
+        AddDecisionQueue("SETDQCONTEXT", $player, "Choose which card you want to freeze", 1);
+        AddDecisionQueue("CHOOSEMULTIZONE", $player, "<-", 1);
+        AddDecisionQueue("MZOP", $player, "FREEZE", 1);
       }
       break;
     case "UPR122": case "UPR123": case "UPR124":
-      if (MZIsPlayer($target)) {
-        AddDecisionQueue("LESSTHANPASS", MZPlayerID($me, $target), 1, 1);
-        AddDecisionQueue("PLAYAURA", MZPlayerID($me, $target), "ELE111", 1);
+      if (MZIsPlayer($target) && $damage > 0) {
+        AddDecisionQueue("PLAYAURA", MZPlayerID($player, $target), "ELE111", 1);
         break;
       }
     default:
