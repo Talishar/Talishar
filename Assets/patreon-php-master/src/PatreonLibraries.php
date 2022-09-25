a375c67e6469b6cd86eaabe6ef9ee2779b6575ff
OotTheMonk <nathanandrecoder@gmail.com>
2022-09-25 08:58:29 -0500

    Patreon fixes

--- Assets/patreon-php-master/src/PatreonLibraries.php
+++ Assets/patreon-php-master/src/PatreonLibraries.php
@@ -11,15 +11,26 @@ function PatreonLogin($access_token)
     $_SESSION["isPatron"] = true;
     return;
   }
-	$api_client = new API($access_token);
+
+  $api_client = new API($access_token);
 	$api_client->api_return_format = 'object';
 
-	// Now get the current user:
 	$patron_response = $api_client->fetch_user();
 	$patron = $patron_response->data;
+
 	$relationships = $patron->relationships;
 	if(isset($relationships)) $memberships = $relationships->memberships;
-	if(isset($memberships)) $_SESSION["isPatron"] = true;
+
+	for($i=0; $i<count($patron_response->included); ++$i)
+	{
+		$include = $patron_response->included[$i];
+		if($include->type == "campaign" && $include->id == "7198186")
+		{
+			echo("Patreon login successful!<BR>");
+			$_SESSION["isPatron"] = true;
+		}
+	}
+
 }
 
 ?>
