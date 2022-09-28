ce58bbbb33434cd7252a1150258146f68a70d3d6
OotTheMonk <nathanandrecoder@gmail.com>
2022-09-27 19:59:58 -0500

    Some additional patreon api stuff

--- Assets/patreon-php-master/src/PatreonLibraries.php
+++ Assets/patreon-php-master/src/PatreonLibraries.php
@@ -21,6 +21,9 @@ function PatreonLogin($access_token)
 	$relationships = $patron->relationships;
 	if(isset($relationships)) $memberships = $relationships->memberships;
 
+  //Talishar 7198186
+  //Push the Point 7579026
+
   $loginSuccessful = false;
   if($patron_response->included == null) return;
 	for($i=0; $i<count($patron_response->included); ++$i)
