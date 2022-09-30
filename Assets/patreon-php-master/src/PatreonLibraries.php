7b1e414beccecf706a408a3a9aa0ee1a26783318
PvtVoid <jotrickr@gmail.com>
2022-09-30 18:25:58 +0200

    Fix broken patreon login

--- Assets/patreon-php-master/src/PatreonLibraries.php
+++ Assets/patreon-php-master/src/PatreonLibraries.php
@@ -21,9 +21,6 @@ function PatreonLogin($access_token)
 	$relationships = $patron->relationships;
 	if(isset($relationships)) $memberships = $relationships->memberships;
 
-  //Talishar 7198186
-  //Push the Point 7579026
-
   $loginSuccessful = false;
   if($patron_response->included == null) return;
   $yourPatronages = [];
