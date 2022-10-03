32a2dcada550620d96ec5861f9ad591aa193d0d8
PvtVoid <jotrickr@gmail.com>
2022-10-03 19:50:33 +0200

    Add Chat Log when something isn't fused

--- CardDictionaries/TalesOfAria/ELERanger.php
+++ CardDictionaries/TalesOfAria/ELERanger.php
@@ -228,6 +228,7 @@
       AddDecisionQueue("FINDINDICES", $player, "HAND" . $element, $subsequent);
       AddDecisionQueue("SETDQCONTEXT", $player, "Choose a card to fuse", 1);
       AddDecisionQueue("MAYCHOOSEHAND", $player, "<-", 1);
+      AddDecisionQueue("NOFUSE", $player, $cardID); //
       AddDecisionQueue("REVEALHANDCARDS", $player, "<-", 1);
       if ($isAndOrFuse)
       {
