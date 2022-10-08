8bc76be172a52006a31d933324dc6086063c2a38
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-08 09:34:05 -0500

    Fav deck commit 2

--- MainMenu.php
+++ MainMenu.php
@@ -74,10 +74,11 @@ $starterDecksText = ($language == 1 ? "Starter Decks" : "おすすめデッキ")
   if (isset($_SESSION["userid"])) {
     $favoriteDecks = LoadFavoriteDecks($_SESSION["userid"]);
     if (count($favoriteDecks) > 0) {
+      if(isset($settingArray[$SET_FavoriteDeckIndex])) $selIndex = $settingArray[$SET_FavoriteDeckIndex];
       echo ("<div class='FavoriteDeckMainMenu'>Favorite Decks: ");
       echo ("<select name='favoriteDecks' id='favoriteDecks'>");
       for ($i = 0; $i < count($favoriteDecks); $i += 3) {
-        echo ("<option value='" . ($i/3) . "<fav>" . $favoriteDecks[$i] . "'>" . $favoriteDecks[$i + 1] . "</option>");
+        echo ("<option value='" . $i . "<fav>" . $favoriteDecks[$i] . "'>" . $favoriteDecks[$i + 1] . "</option>");
       }
       echo ("</select></div>");
     }
