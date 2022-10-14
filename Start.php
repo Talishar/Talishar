b764289fe5546d857d2b18eb552230cc4db893eb
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-14 06:47:53 -0500

    Add new shmop piece for current player

--- Start.php
+++ Start.php
@@ -91,7 +91,7 @@ $currentTime = strval(round(microtime(true) * 1000));
 $currentUpdate = GetCachePiece($gameName, 1);
 $p1Hero = GetCachePiece($gameName, 7);
 $p2Hero = GetCachePiece($gameName, 8);
-WriteCache($gameName, ($currentUpdate + 1) . "!" . $currentTime . "!" . $currentTime . "!-1!-1!" . $currentTime . "!"  . $p1Hero . "!" . $p2Hero); //Initialize SHMOP cache for this game
+WriteCache($gameName, ($currentUpdate + 1) . "!" . $currentTime . "!" . $currentTime . "!-1!-1!" . $currentTime . "!"  . $p1Hero . "!" . $p2Hero . "!" . 0); //Initialize SHMOP cache for this game
 
 ob_start();
 include "StartEffects.php";
