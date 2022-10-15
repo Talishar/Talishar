cd10f8fd12ab96bf4d54b31e6aef8d4c41f126bf
PvtVoid <jotrickr@gmail.com>
2022-10-15 10:05:40 +0200

    Working "Don't allow starter decks in the comp games"

--- SubmitSideboard.php
+++ SubmitSideboard.php
@@ -53,12 +53,12 @@ if ($playerCharacter != "" && $playerDeck != "") //If they submitted before load
   $playerDeck = explode(",", $playerDeck);
   $deckCount = count($playerDeck);
   if ($deckCount < 60 && ($format == "cc" || $format == "compcc")) {
-    WriteLog("Unable to submit player " . $playerID . "'s deck. " . $deckCount . " cards select is under the legal minimum.");
+    WriteLog("Unable to submit player " . $playerID . "'s deck. " . $deckCount . " cards selected is under the legal minimum.");
     header("Location: " . $redirectPath . "/GameLobby.php?gameName=$gameName&playerID=$playerID");
     exit;
   }
   if ($deckCount < 40 && ($format == "blitz" || $format == "compblitz" || $format == "commoner")) {
-    WriteLog("Unable to submit player " . $playerID . "'s deck. " . $deckCount . " cards select is under the legal minimum.");
+    WriteLog("Unable to submit player " . $playerID . "'s deck. " . $deckCount . " cards selected is under the legal minimum.");
     header("Location: " . $redirectPath . "/GameLobby.php?gameName=$gameName&playerID=$playerID");
     exit;
   }
