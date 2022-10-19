a3f7567babf6f7ea5ac2c1d7ebcee85e9eeae2ea
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-18 19:54:42 -0500

    Very rough encounter AI

---
+++ AI/EncounterAI.php
@@ -0,0 +1,35 @@
+<?php
+
+function EncounterAI()
+{
+  global $currentPlayer, $p2CharEquip, $decisionQueue, $turn;
+  $currentPlayerIsAI = ($currentPlayer == 2 && $p2CharEquip[0] == "ROGUE001") ? true : false;
+  if(!IsGameOver() && $currentPlayerIsAI)
+  {
+    for($i=0; $i<100 && $currentPlayerIsAI; ++$i)
+    {
+      if(count($decisionQueue) > 0)
+      {
+        $options = explode(",", $turn[2]);
+        ContinueDecisionQueue($options[0]);//Just pick the first option
+      }
+      else if($turn[0] == "B")
+      {
+        $hand = &GetHand($currentPlayer);
+        if(count($hand) > 0)
+        {
+          ProcessInput($currentPlayer, 27, "", 0, 0, "");
+        }
+        else PassInput();
+      }
+      else
+      {
+        PassInput();
+      }
+      ProcessMacros();
+      $currentPlayerIsAI = ($currentPlayer == 2 && $p2CharEquip[0] == "ROGUE001") ? true : false;
+    }
+  }
+}
+
+?>
