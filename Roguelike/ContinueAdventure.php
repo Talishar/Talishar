41adb7d259de2c5ec88b634237ef3b4f442ecba1
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-19 16:43:59 -0500

    Add ability to continue adventure after game

---
+++ Roguelike/ContinueAdventure.php
@@ -0,0 +1,25 @@
+<?php
+
+  error_reporting(E_ALL);
+
+  //We should always have a player ID as a URL parameter
+  $gameName=$_GET["gameName"];
+  $playerID=$_GET["playerID"];
+  $remainingHealth=$_GET["health"];
+
+  //First we need to parse the game state from the file
+  include "ZoneGetters.php";
+  include "ParseGamestate.php";
+  include "../HostFiles/Redirector.php";
+  include "DecisionQueue.php";
+  include "../WriteLog.php";
+
+  
+
+  include "WriteGamestate.php";
+
+  header("Location: " . $redirectPath . "/Roguelike/NextEncounter.php?gameName=$gameName&playerID=" . $playerID);
+
+  exit;
+
+?>
