493fec1d5804e538c5f315ae162744dcf7bc8b6a
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-21 20:19:27 -0500

    Ravenous rabble encounter

--- AI/EncounterAI.php
+++ AI/EncounterAI.php
@@ -3,7 +3,7 @@
 function EncounterAI()
 {
   global $currentPlayer, $p2CharEquip, $decisionQueue, $turn, $mainPlayer, $mainPlayerGamestateStillBuilt;
-  $currentPlayerIsAI = ($currentPlayer == 2 && $p2CharEquip[0] == "ROGUE001") ? true : false;
+  $currentPlayerIsAI = ($currentPlayer == 2 && ($p2CharEquip[0] == "ROGUE001" || $p2CharEquip[0] == "ROGUE003")) ? true : false;
   if(!IsGameOver() && $currentPlayerIsAI)
   {
     for($i=0; $i<100 && $currentPlayerIsAI; ++$i)
@@ -26,7 +26,13 @@ function EncounterAI()
       else if($turn[0] == "M" && $mainPlayer == $currentPlayer)//AIs turn
       {
         $character = &GetPlayerCharacter($currentPlayer);
-        if(count($character) > CharacterPieces() && CardType($character[CharacterPieces()]) == "W")
+        $hand = &GetHand($currentPlayer);
+        if(count($hand) > 0 && CardCost($hand[0]) == 0)
+        {
+          ProcessInput($currentPlayer, 27, "", 0, 0, "");
+          CacheCombatResult();
+        }
+        else if(count($character) > CharacterPieces() && CardType($character[CharacterPieces()]) == "W")
         {
           ProcessInput($currentPlayer, 3, "", CharacterPieces(), 0, "");
           CacheCombatResult();
