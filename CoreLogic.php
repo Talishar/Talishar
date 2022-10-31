e610d35f2cbeb3a4632eb903b125999892eb43d9
OotTheMonk <nathanandrecoder@gmail.com>
2022-10-30 20:34:17 -0500

    Arakni

--- CoreLogic.php
+++ CoreLogic.php
@@ -444,7 +444,20 @@ function MainCharacterPlayCardAbilities($cardID, $from)
           $index = FindCharacterIndex($currentPlayer, "DYN088");
           ++$character[$index + 2];
         }
-
+        break;
+      case "DYN113":
+        if(ContractType($cardID) != "")
+        {
+          $otherPlayer = ($currentPlayer == 1 ? 2 : 1);
+          AddDecisionQueue("DECKCARDS", $otherPlayer, "0", 1);
+          AddDecisionQueue("SETDQVAR", $currentPlayer, "0", 1);
+          AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose if you want sink <0> with Arakni", 1);
+          AddDecisionQueue("YESNO", $currentPlayer, "if_you_want_to_sink_the_opponent's_card", 1);
+          AddDecisionQueue("NOPASS", $currentPlayer, "-", 1);
+          AddDecisionQueue("FINDINDICES", $otherPlayer, "TOPDECK", 1);
+          AddDecisionQueue("MULTIREMOVEDECK", $otherPlayer, "<-", 1);
+          AddDecisionQueue("ADDBOTDECK", $otherPlayer, "-", 1);
+        }
         break;
       default:
         break;
