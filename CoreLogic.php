93131dd80edb3d161dfec96ef35c4e9f3a00a310
PvtVoid <jotrickr@gmail.com>
2022-10-31 18:05:12 +0100

    Leave No Witnesses

--- CoreLogic.php
+++ CoreLogic.php
@@ -454,10 +454,10 @@ function MainCharacterPlayCardAbilities($cardID, $from)
           AddDecisionQueue("SETDQVAR", $currentPlayer, "0", 1);
           AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose if you want sink <0> with Arakni", 1);
           AddDecisionQueue("YESNO", $currentPlayer, "if_you_want_to_sink_the_opponent's_card", 1);
-          AddDecisionQueue("NOPASS", $currentPlayer, "-", 1);
+          AddDecisionQueue("NOPASSARAKNI", $currentPlayer, $characterID, 1);
           AddDecisionQueue("FINDINDICES", $otherPlayer, "TOPDECK", 1);
           AddDecisionQueue("MULTIREMOVEDECK", $otherPlayer, "<-", 1);
-          AddDecisionQueue("ADDBOTTOMMYDECK", $otherPlayer, "-", 1);
+          AddDecisionQueue("ADDBOTDECK", $otherPlayer, "-", 1);
         }
         break;
       default:
