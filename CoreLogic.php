518938fc503f5325da110e3e489a1423d6202329
PvtVoid <jotrickr@gmail.com>
2022-11-02 07:34:07 +0100

    Add Arakni Trigger

--- CoreLogic.php
+++ CoreLogic.php
@@ -449,15 +449,7 @@ function MainCharacterPlayCardAbilities($cardID, $from)
       case "DYN113": case "DYN114":
         if(ContractType($cardID) != "")
         {
-          $otherPlayer = ($currentPlayer == 1 ? 2 : 1);
-          AddDecisionQueue("DECKCARDS", $otherPlayer, "0", 1);
-          AddDecisionQueue("SETDQVAR", $currentPlayer, "0", 1);
-          AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose if you want sink <0> with Arakni", 1);
-          AddDecisionQueue("YESNO", $currentPlayer, "if_you_want_to_sink_the_opponent's_card", 1);
-          AddDecisionQueue("NOPASSARAKNI", $currentPlayer, $characterID, 1);
-          AddDecisionQueue("FINDINDICES", $otherPlayer, "TOPDECK", 1);
-          AddDecisionQueue("MULTIREMOVEDECK", $otherPlayer, "<-", 1);
-          AddDecisionQueue("ADDBOTDECK", $otherPlayer, "-", 1);
+          AddLayer("TRIGGER", $currentPlayer, $characterID);
         }
         break;
       default:
