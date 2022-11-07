8e7c21c41362a0ea3c003d89672fbd569040fdcc
PvtVoid <jotrickr@gmail.com>
2022-11-07 08:38:24 +0100

    Remove go again from Teklovossen's workshop

--- CardDictionaries/CrucibleOfWar/CRUShared.php
+++ CardDictionaries/CrucibleOfWar/CRUShared.php
@@ -614,8 +614,6 @@
       case "CRU072": case "CRU074": return true;
       //CRU Ranger
       case "CRU124": case "CRU135": case "CRU136": case "CRU137": return true;
-      //CRU Mechanologist
-      case "CRU115": case "CRU116": case "CRU117": return true;
       //CRU Runeblade
       case "CRU143": return true;
       case "CRU145": case "CRU146": case "CRU147": return true;
