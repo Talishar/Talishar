2059a588c3059ddf2afc7858adf093b916d77e1a
PvtVoid <jotrickr@gmail.com>
2022-11-08 09:38:15 +0100

    Test Better Logo for Meta

--- Header.php
+++ Header.php
@@ -38,12 +38,12 @@ if (isset($_SESSION["useruid"])) {
   <meta property="og:title" content="Talishar">
   <meta name="twitter:title" content="Talishar">
 
-  <meta name="description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play FAB. Please report any feedback or bugs you have on our discord">
-  <meta property="og:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play FAB. Please report any feedback or bugs you have on our discord">
-  <meta name="twitter:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play FAB. Please report any feedback or bugs you have on our discord">
+  <meta name="description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
+  <meta property="og:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
+  <meta name="twitter:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
 
-  <meta property="og:image" content="https://talishar.net/game/Images/TalisharLogo.webp">
-  <meta name="twitter:image" content="https://talishar.net/game/Images/TalisharLogo.webp">
+  <meta property="og:image" content="http://talishar.net/game/Images/TalisharLogo2.webp">
+  <meta name="twitter:image" content="http://talishar.net/game/Images/TalisharLogo2.webp">
 
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/reset.css">
