268c081bc8e04467b7cfa87661b8b18a39056f67
PvtVoid <jotrickr@gmail.com>
2022-11-09 11:05:29 +0100

    Fix Cash-in asking allowing to pay 6 coppers AND 4 silvers AND 1 gold

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1356,6 +1356,7 @@ function AddPrePitchDecisionQueue($cardID, $from, $index = -1)
 
       AddDecisionQueue("COUNTITEM", $currentPlayer, "EVR195"); //Silver
       AddDecisionQueue("LESSTHANPASS", $currentPlayer, "2");
+      AddDecisionQueue("FINDCURRENTEFFECTPASS", $currentPlayer, "CRU188");
       AddDecisionQueue("YESNO", $currentPlayer, "if_you_want_to_pay_2_" . CardLink("EVR195", "EVR195"), 1);
       AddDecisionQueue("NOPASS", $currentPlayer, "-", 1);
       AddDecisionQueue("FINDANDDESTROYITEM", $currentPlayer, "EVR195-2", 1);
@@ -1363,6 +1364,7 @@ function AddPrePitchDecisionQueue($cardID, $from, $index = -1)
 
       AddDecisionQueue("COUNTITEM", $currentPlayer, "DYN243"); //Gold
       AddDecisionQueue("LESSTHANPASS", $currentPlayer, "1");
+      AddDecisionQueue("FINDCURRENTEFFECTPASS", $currentPlayer, "CRU188");
       AddDecisionQueue("YESNO", $currentPlayer, "if_you_want_to_pay_1_" . CardLink("DYN243", "DYN243"), 1);
       AddDecisionQueue("NOPASS", $currentPlayer, "-", 1);
       AddDecisionQueue("FINDANDDESTROYITEM", $currentPlayer, "DYN243-1", 1);
