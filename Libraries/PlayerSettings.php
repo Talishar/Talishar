53d2b0003ddb5e7ece3d0d4b4054813826627199
PvtVoid <jotrickr@gmail.com>
2022-11-09 10:55:23 +0100

    Add TeamAscent member

--- Libraries/PlayerSettings.php
+++ Libraries/PlayerSettings.php
@@ -470,7 +470,7 @@ function GetSettingsUI($player)
   }
 
   $isAscentGaming = false;
-  $isAscentGaming = isset($_SESSION["useruid"]) && ($_SESSION["useruid"] == "hometowntcg" || $_SESSION["useruid"] == "ProfessorKibosh" || $_SESSION["useruid"] == "criticalclover8" || $_SESSION["useruid"] == "bomberman" || $_SESSION["useruid"] == "woodjp64" || $_SESSION["useruid"] == "TealWater" || $_SESSION["useruid"] == "Bravosaur" || $_SESSION["useruid"] == "DaganTheZookeeper" || $_SESSION["useruid"] == "Dratylis");
+  $isAscentGaming = isset($_SESSION["useruid"]) && ($_SESSION["useruid"] == "hometowntcg" || $_SESSION["useruid"] == "ProfessorKibosh" || $_SESSION["useruid"] == "criticalclover8" || $_SESSION["useruid"] == "bomberman" || $_SESSION["useruid"] == "woodjp64" || $_SESSION["useruid"] == "TealWater" || $_SESSION["useruid"] == "Bravosaur" || $_SESSION["useruid"] == "DaganTheZookeeper" || $_SESSION["useruid"] == "Dratylis" || $_SESSION["useruid"] == "MoBogsly");
   if ($isAscentGaming) {
     $hasCardBacks = true;
     $rv .= CreateRadioButton($SET_Cardback . "-37", "AscentGaming", 26, $SET_Cardback . "-" . $settings[$SET_Cardback], "Ascent Gaming");
