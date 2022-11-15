df061bf86bc8a31bdde12bd939f53251a9b80d5a
PvtVoid <jotrickr@gmail.com>
2022-11-15 09:49:08 +0100

    NumSwordAttacks counting Dagger attacks? :ant:

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -893,7 +893,7 @@ function FinalizeChainLink($chainClosed = false)
   //Don't change state until the end, in case it changes what effects are active
   if (CardType($combatChain[0]) == "W" && !$chainClosed) {
     ++$mainClassState[$CS_AtksWWeapon];
-    if (CardSubtype($combatChain[0]) == "Sword" || CardSubtype($combatChain[0]) == "Dagger") ++$mainClassState[$CS_NumSwordAttacks];
+    if (CardSubtype($combatChain[0]) == "Sword") ++$mainClassState[$CS_NumSwordAttacks];
   }
   $combatChainState[$CCS_LastAttack] = $combatChain[0];
   SetClassState($mainPlayer, $CS_LastAttack, $combatChain[0]);
