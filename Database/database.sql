cd04e3a9ef58dea107ddee1c649bdc1e5893194c
OotTheMonk <nathanandrecoder@gmail.com>
2022-11-15 19:32:58 -0600

    fabdb id

--- Database/database.sql
+++ Database/database.sql
@@ -71,7 +71,8 @@ CREATE TABLE `users` (
   `usersKarma` TINYINT(3) NOT NULL DEFAULT '75',
   `greenThumbs` int(11) NOT NULL DEFAULT '0',
   `redThumbs` int(11) NOT NULL DEFAULT '0',
-  `fabraryId` varchar(64) DEFAULT NULL
+  `fabraryId` varchar(64) DEFAULT NULL,
+  `fabdbId` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
