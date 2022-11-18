15bb57848fcd86d4391c714c52620c80a6c62c48
OotTheMonk <nathanandrecoder@gmail.com>
2022-11-17 18:42:51 -0600

    Factor out some create game logic

--- CreateGame.php
+++ CreateGame.php
@@ -33,9 +33,6 @@ if($favoriteDeckLink != 0)
   }
 }
 
-$gcFile = fopen("HostFiles/GameIDCounter.txt", "r+");
-$attemptCount = 0;
-
 $isOmegaEclipse = isset($_SESSION["useruid"]) && $_SESSION["useruid"] == "OmegaEclipse";
 
 if(isset($_SESSION["userid"]))
@@ -75,21 +72,7 @@ if ($isOmegaEclipse) {
   else if ($format == "commoner") $format = "shadowcommoner";
 }
 
-while (!flock($gcFile, LOCK_EX) && $attemptCount < 30) {  // acquire an exclusive lock
-  sleep(1);
-  ++$attemptCount;
-}
-if ($attemptCount == 30) {
-  header("Location: " . $redirectorPath . "MainMenu.php"); //We never actually got the lock
-}
-$counter = intval(fgets($gcFile));
-//$gameName = hash("sha256", $counter);
-$gameName = $counter;
-ftruncate($gcFile, 0);
-rewind($gcFile);
-fwrite($gcFile, $counter + 1);
-flock($gcFile, LOCK_UN);    // release the lock
-fclose($gcFile);
+$gameName = CreateGameFile();
 
 if ( (!file_exists("Games/$gameName")) && (mkdir("Games/$gameName", 0700, true)) ){
 } else {
