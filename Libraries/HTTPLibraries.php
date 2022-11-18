20f05544080b801db8f4f181acc0f1cdfa8a349a
OotTheMonk <nathanandrecoder@gmail.com>
2022-11-17 18:42:51 -0600

    Factor out some create game logic

--- Libraries/HTTPLibraries.php
+++ Libraries/HTTPLibraries.php
@@ -22,6 +22,28 @@ function IsDeckLinkValid($deckLink)
   */
 }
 
+function CreateGameFile()
+{
+  $gcFile = fopen("HostFiles/GameIDCounter.txt", "r+");
+  $attemptCount = 0;
+  while (!flock($gcFile, LOCK_EX) && $attemptCount < 30) {  // acquire an exclusive lock
+    sleep(1);
+    ++$attemptCount;
+  }
+  if ($attemptCount == 30) {
+    header("Location: " . $redirectorPath . "MainMenu.php"); //We never actually got the lock
+  }
+  $counter = intval(fgets($gcFile));
+  //$gameName = hash("sha256", $counter);
+  $gameName = $counter;
+  ftruncate($gcFile, 0);
+  rewind($gcFile);
+  fwrite($gcFile, $counter + 1);
+  flock($gcFile, LOCK_UN);    // release the lock
+  fclose($gcFile);
+  return $gameName;
+}
+
 function SetHeaders()
 {
   // array holding allowed Origin domains
