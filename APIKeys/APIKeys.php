bbae1d26791a4a5ecdfd1b8ae8b947a053e05988
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2022-11-30 14:19:38 +0800

    Update APIKeys.php

--- APIKeys/APIKeys.php
+++ APIKeys/APIKeys.php
@@ -1,6 +1,8 @@
 <?php
 
+  // This is a dummy/test key for development
   $FaBraryKey = "flesh-and-blood-online-f8U2U77AuKz79D6Q9sxf6ZwZkYrWFL";
 
+  // This is a dummy/test key for development.
   $adsenseClientId = "";
 ?>
