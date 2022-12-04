1fc0a0859eba6a36e8bac89dce04ec66f87a5643
OotTheMonk <nathanandrecoder@gmail.com>
2022-12-03 21:37:32 -0600

    Make it possible to use redis instead of shmop (off by default)

--- Libraries/SHMOPLibraries.php
+++ Libraries/SHMOPLibraries.php
@@ -1,43 +1,73 @@
 <?php
 
+$useRedis = false;
+
+if($useRedis)
+{
+  $redis = new Redis();
+  $redis->connect('127.0.0.1', 6379);
+}
+
 function WriteCache($name, $data)
 {
+  global $useRedis, $redis;
   //DeleteCache($name);
   if($name == 0) return;
   $serData = serialize($data);
-  $id = shmop_open($name, "c", 0644, 128);
-  if($id == false) {
-    exit;
-   } else {
-      $rv = shmop_write($id, $serData, 0);
+
+  if($useRedis)
+  {
+    $redis->set($name, $serData);
+  }
+  else {
+    $id = shmop_open($name, "c", 0644, 128);
+    if($id == false) {
+      exit;
+     } else {
+        $rv = shmop_write($id, $serData, 0);
+    }
   }
 }
 
 function ReadCache($name)
 {
+  global $useRedis, $redis;
   if($name == 0) return "";
-  @$id = shmop_open($name, "a", 0, 0);
-  if(empty($id) || $id == false)
+
+  if($useRedis)
   {
-    return "";
-    //WriteCache($name, "");
-    //$id = shmop_open($name, "a", 0, 0);
+    $data = $redis->get($name);
   }
-  $data = shmop_read($id, 0, shmop_size($id));
-  $data = preg_replace_callback( '!s:(\d+):"(.*?)";!', function($match) {
-    return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
-}, $data );
+  else {
+    @$id = shmop_open($name, "a", 0, 0);
+    if(empty($id) || $id == false)
+    {
+      return "";
+    }
+    $data = shmop_read($id, 0, shmop_size($id));
+    $data = preg_replace_callback( '!s:(\d+):"(.*?)";!', function($match) {
+      return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
+      }, $data);
+  }
+
   return unserialize($data);
 }
 
 function DeleteCache($name)
 {
+  global $useRedis, $redis;
+  if($useRedis)
+  {
+    $redis->delete($name);
+  }
+  else {
     $id=shmop_open($name, "w", 0644, 128);
     if($id)
     {
       shmop_delete($id);
       shmop_close($id); //shmop_close is deprecated
     }
+  }
 }
 
 function SetCachePiece($name, $piece, $value)
