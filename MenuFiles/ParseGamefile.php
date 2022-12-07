089e930cb3c919ecd75439bb71e3ebc3c8ee393f
OotTheMonk <nathanandrecoder@gmail.com>
2022-12-07 07:35:38 -0600

    Don't try to load data from a handler that doesn't exist

--- MenuFiles/ParseGamefile.php
+++ MenuFiles/ParseGamefile.php
@@ -4,6 +4,7 @@
   {
     function GetArray($handler)
     {
+      if(!$handler) return false;
       $line = trim(fgets($handler));
       if($line=="") return [];
       return explode(" ", $line);
