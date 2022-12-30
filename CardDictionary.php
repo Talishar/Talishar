78701f0db92cbf7295cadf1484dbc561cda9a90b
OotTheMonk <nathanandrecoder@gmail.com>
2022-12-30 13:14:04 -0600

    Re-enable using generated attack values

--- CardDictionary.php
+++ CardDictionary.php
@@ -744,14 +744,11 @@ function AttackValue($cardID)
     if (SearchCharacterForCard($mainPlayer, "MON003")) return 1;
     if (SearchCharacterForCard($mainPlayer, "MON088")) return 4;
   }
-  /*
-  //Do not enable until attack reactions get figured out
   if($set != "ROG" && $set != "DUM" && $cardID != "MON191" && $cardID != "EVR138" && $cardID != "DYN216")
   {
     $number = intval(substr($cardID, 3));
     if($number < 400) return GeneratedAttackValue($cardID);
   }
-  */
   if ($set == "WTR") {
     return WTRAttackValue($cardID);
   } else if ($set == "ARC") {
