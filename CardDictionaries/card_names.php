4e2687498c8b9ed0e929e589d8efa4d61d1aaebf
OotTheMonk <nathanandrecoder@gmail.com>
2022-12-30 19:17:16 -0600

    Use generated card names

--- CardDictionaries/card_names.php
+++ CardDictionaries/card_names.php
@@ -5,6 +5,12 @@ function CardName($cardID)
     $arr = str_split($cardID, 3);
     if(count($arr) < 2) return "";
     $set = $arr[0];
+    if($set != "ROG" && $set != "DUM")
+    {
+      $number = intval(substr($cardID, 3));
+      if($number < 400) return GeneratedCardName($cardID);
+    }
+
     $num = $arr[1];
 
 $showPitch = false;
