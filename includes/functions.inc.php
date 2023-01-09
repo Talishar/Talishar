e92ce7d8bc35b5a0ad1168d1147cac9712039870
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-08 20:17:13 -0600

    Fix add favorite deck sql injection

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -289,9 +289,10 @@ function addFavoriteDeck($userID, $decklink, $deckName, $heroID)
 	$deckName = implode("", explode("\"", $deckName));
 	$deckName = implode("", explode("'", $deckName));
 	$values = "'" . $decklink . "'," . $userID . ",'" . $deckName . "','" . $heroID . "'";
-	$sql = "INSERT IGNORE INTO favoritedeck (decklink, usersId, name, hero) VALUES (" . $values. ");";
+	$sql = "INSERT IGNORE INTO favoritedeck (decklink, usersId, name, hero) VALUES (?, ?, ?, ?);";
 	$stmt = mysqli_stmt_init($conn);
 	if (mysqli_stmt_prepare($stmt, $sql)) {
+		mysqli_stmt_bind_param($stmt, "ssss", $decklink, $userID, $deckName, $heroID);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_close($stmt);
 	}
