c16c84f6bc9ba609c73e4f017e1cc47754bba77b
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-08 20:02:14 -0600

    Fix sql injection

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -192,9 +192,10 @@ function loginFromCookie()
 {
 	$token = $_COOKIE["rememberMeToken"];
 	$conn = GetDBConnection();
-	$sql = "SELECT usersID, usersUid, usersEmail, patreonAccessToken, patreonRefreshToken, usersKarma, patreonEnum FROM users WHERE rememberMeToken='$token'";
+	$sql = "SELECT usersID, usersUid, usersEmail, patreonAccessToken, patreonRefreshToken, usersKarma, patreonEnum FROM users WHERE rememberMeToken='?'";
 	$stmt = mysqli_stmt_init($conn);
 	if (mysqli_stmt_prepare($stmt, $sql)) {
+		mysqli_stmt_bind_param($stmt, "s", $token);
 		mysqli_stmt_execute($stmt);
 		$data = mysqli_stmt_get_result($stmt);
 		$row = mysqli_fetch_array($data, MYSQLI_NUM);
