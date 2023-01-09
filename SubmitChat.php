b8cdbc182167c4821c3026e8c178acaf303dd753
Kevin Sugihara <kevin@infamousdevel.com>
2023-01-08 19:21:36 -0700

    Added more contributors
    added more of the contributors to the list.

--- SubmitChat.php
+++ SubmitChat.php
@@ -24,7 +24,7 @@ if ($playerID == 1 || $playerID == 2) {
 }
 
 //arrach for contributors
-$contributors = array("sugitime", "OotTheMonk", "Launch", "LaustinSpayce", "Star_Seraph", "Tower");
+$contributors = array("sugitime", "OotTheMonk", "Launch", "LaustinSpayce", "Star_Seraph", "Tower", "Etasus", "scary987");
  
 //its sort of sloppy, but it this will fail if you're in the contributors array because we want to give you the contributor icon, not the patron icon.
 if (isset($_SESSION["isPatron"]) && !in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Patron' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./Images/patronHeart.webp' /> " . $displayName;
