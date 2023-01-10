fab5a7381b4514286040a00020c87dc328a94998
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-09 19:23:10 -0600

    Auth key protect chat

--- SubmitChat.php
+++ SubmitChat.php
@@ -10,22 +10,26 @@ if (!IsGameNameValid($gameName)) {
 }
 $playerID = $_GET["playerID"];
 $chatText = htmlspecialchars($_GET["chatText"]);
+$authKey = $_GET["authKey"];
+
+session_start();
+echo($_SESSION["p1AuthKey"]);
+
+$targetAuthKey = "";
+if ($playerID == 1 && isset($_SESSION["p1AuthKey"])) $targetAuthKey = $_SESSION["p1AuthKey"];
+else if ($playerID == 2 && isset($_SESSION["p2AuthKey"])) $targetAuthKey = $_SESSION["p2AuthKey"];
+if($targetAuthKey == "") $targetAuthKey = $_COOKIE["lastAuthKey"];
+if($authKey != $targetAuthKey) exit;
 
 SetHeaders();
 
-session_start();
 $uid = "-";
 if (isset($_SESSION['useruid'])) $uid = $_SESSION['useruid'];
 $displayName = ($uid != "-" ? $uid : "Player " . $playerID);
 
-$authKey = "";
-if ($playerID == 1 || $playerID == 2) {
-  if (isset($_COOKIE["lastAuthKey"])) $authKey = $_COOKIE["lastAuthKey"];
-}
-
-//arrach for contributors
+//array for contributors
 $contributors = array("sugitime", "OotTheMonk", "Launch", "LaustinSpayce", "Star_Seraph", "Tower", "Etasus", "scary987", "Celenar");
- 
+
 //its sort of sloppy, but it this will fail if you're in the contributors array because we want to give you the contributor icon, not the patron icon.
 if (isset($_SESSION["isPatron"]) && !in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Patron' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./Images/patronHeart.webp' /> " . $displayName;
 
