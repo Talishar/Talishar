e90f68f3b2ec0187b5f5d1022a096b817e0595d1
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-11 18:40:41 -0600

    Add Man Sant portrait

--- Assets/patreon-php-master/src/PatreonDictionary.php
+++ Assets/patreon-php-master/src/PatreonDictionary.php
@@ -3,12 +3,14 @@
 enum ContentCreators : string
 {
   case InstantSpeed = "0";
+  case ManSant = "731";
 
   public function SessionName(): string
   {
     switch($this->value)
     {
       case "0": return "isInstantSpeedPatron";
+      case "731": return "isManSantPatron";
       default: return "";
     }
   }
@@ -18,6 +20,7 @@ enum ContentCreators : string
     switch($this->value)
     {
       case "0": return "https://www.patreon.com/instantspeedpod";
+      case "731": return "https://www.patreon.com/ManSant";
       default: return "";
     }
   }
@@ -27,6 +30,7 @@ enum ContentCreators : string
     switch($this->value)
     {
       case "0": return "https://www.youtube.com/playlist?list=PLIo1KFShm1L3e91QrlPG6ZdwfmqKk0NIP";
+      case "731": return "https://www.youtube.com/@ManSantFaB";
       default: return "";
     }
   }
@@ -48,7 +52,9 @@ enum ContentCreators : string
       case "0": //WatchFlake
         if(CardClass($heroID) == "GUARDIAN") return "./Assets/patreon-php-master/assets/ContentCreatorImages/Matt_anathos_overlay.webp";
         return "./Assets/patreon-php-master/assets/ContentCreatorImages/flakeOverlay.webp";
-        break;
+      case "731":
+        if(CardClass($heroID) == "BRUTE") return "./Assets/patreon-php-master/assets/ContentCreatorImages/ManSantLevia.webp";
+        return "";
       default: return "";
     }
   }
@@ -58,6 +64,7 @@ enum ContentCreators : string
     switch($this->value)
     {
       case "0": return "rgb(2,190,253)";
+      case "731": return "rgb(255,53,42)";
       default: return "";
     }
   }
