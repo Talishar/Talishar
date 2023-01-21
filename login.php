e6e425df74f604d2e6d8e2824ec9be4db404ff9e
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-21 07:50:15 -0600

    Delete commented code

--- login.php
+++ login.php
@@ -1,14 +1,10 @@
 <?php
 include_once 'Header.php';
 
-// Check to see if the user is logged in, and redirects to MainMenu.php if they are
-
 if (isset($_SESSION['useruid'])) {
   header("Location: /FaBOnline/MainMenu.php");
 }
 ?>
-
-<script src="https://accounts.google.com/gsi/client" async defer></script>
 
 <section class="signup-form">
   <h2>LOG IN</h2>
@@ -19,14 +15,6 @@ if (isset($_SESSION['useruid'])) {
       <label style='display:inline;' for="rememberMe">Remember Me</label>
       <input style='display:inline; width:16px;' type="checkbox" checked='checked' id="rememberMe" name="rememberMe" value="rememberMe">
       <button type="submit" name="submit">SUBMIT</button>
-      <!--
-      <div class="centeredGoogle">
-        <div id="g_id_onload" data-client_id="1089547347578-lqbvoqb0u3grqi89ibjmti2bc9lq37pr.apps.googleusercontent.com" data-login_uri="http://localhost/FaBOnline/MainMenu.php" data-auto_prompt="false">
-        </div>
-        <div class="g_id_signin" data-type="standard" data-shape="pill" data-theme="outline" data-text="signin_with" data-size="large" data-logo_alignment="left">
-        </div>
-      </div>
-    -->
     </form>
   </div>
   <div class="signup-form-form">
