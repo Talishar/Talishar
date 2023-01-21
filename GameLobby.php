952f232d0bc118f1e7e5244330f4eb308b879dea
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-21 07:02:02 -0600

    Rewrite header

--- GameLobby.php
+++ GameLobby.php
@@ -62,7 +62,6 @@ $isMobile = IsMobile();
   <meta charset="utf-8">
   <title>Talishar</title>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/style4.css">
 </head>
 
--- Header.php
+++ Header.php
@@ -27,31 +27,37 @@ $isMobile = IsMobile();
 <html lang="en" dir="ltr">
 
 <head>
-  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
   <title>Talishar</title>
-
-  <meta property="og:title" content="Talishar">
-  <meta name="twitter:title" content="Talishar">
-
-  <meta name="description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
-  <meta property="og:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
-  <meta name="twitter:description" content="Talishar is a fan-made FABTCG project not associated with Legend Story Studios. Talishar is a browser-based platform to play Flesh and Blood. Please report any feedback or bugs you have on our discord.">
-
-  <meta property="og:image" content="http://talishar.net/game/Images/TalisharLogo2.webp">
-  <meta name="twitter:image" content="http://talishar.net/game/Images/TalisharLogo2.webp">
-
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/style4.css">
-  <link rel="shortcut icon" type="image/png" href="./Images/TeenyCoin.png" />
+  <link rel="shortcut icon" type="image/png" href="Images/TeenyCoin.png" />
+  <!--<link rel="stylesheet" href="css/style4.css">-->
   <!--<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8442966023291783" crossorigin="anonymous"></script>-->
+
+  <style>
+    li {
+      display: inline;
+      font-size: 24px;
+      padding:8px;
+    }
+
+    ul {
+      display: inline;
+      padding: 0px;
+      margin: 0px;
+    }
+
+    a {
+      color: white;
+      background-color: transparent;
+      text-decoration: none;
+      font-family: helvetica;
+    }
+  </style>
 </head>
 
-<body>
+<body style="background-image: url('./Images/background.jpg');">
 
-
+  <div style='position:fixed; left:0px; top:0px; height:30px; width:100%; z-index:100; background-color:rgba(30, 30, 30, .8);'>
   <nav>
-    <div class="wrapper">
       <ul>
         <?php if(!$isMobile) echo '<li><a target="_blank" href="https://discord.gg/JykuRkdd5S">Discord</a></li>'; ?>
 
@@ -61,9 +67,9 @@ $isMobile = IsMobile();
         <!--<li><a target="_blank" href="https://www.patreon.com/talishar_online">Support Us</a></li>-->
       </ul>
 
-      <ul>
+      <ul style='float:right;'>
         <li><a href="MainMenu.php">Home Page</a></li>
-        <?php if(!$isMobile) echo '<li><a href="https://fabtcg.com/events">Find Local Events</a></li>'; ?>
+        <?php if(!$isMobile) echo '<li><a href="https://fabtcg.com/events" target="_blank">Find Local Events</a></li>'; ?>
         <?php if(!$isMobile) echo '<li><a href="https://github.com/Talishar/Talishar/labels/bug" target="_blank">Known Bugs</a></li>'; ?>
         <?php //if($isPatron) echo "<li><a href='Replays.php'>Replays[BETA]</a></li>"; ?>
         <li><a href="Draft.php">Limited</a></li>
@@ -78,5 +84,5 @@ $isMobile = IsMobile();
         }
         ?>
       </ul>
-    </div>
-  </nav>
+    </nav>
+  </div>
