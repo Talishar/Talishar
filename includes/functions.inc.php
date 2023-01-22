5adde0133dd1f9866c0489d36bdf9623a9afd055
qenya <accounts@katherina.rocks>
2023-01-22 22:16:20 +0000

    Restore deleted code that was not unused
    Commit 7be35ea removed several functions that appeared unused. Unfortunately uidExists() is in fact used in the signup flow, with the result that attempting to sign up now fails with an error. This PR fixes that.

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -46,6 +46,33 @@ function pwdMatch($pwd, $pwdrepeat) {
 	return $result;
 }
 
+// Check if username is in database, if so then return data
+function uidExists($conn, $username) {
+	$conn = GetDBConnection();
+	$sql = "SELECT * FROM users WHERE usersUid = ? OR usersEmail = ?;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+	 	header("location: ../Signup.php?error=stmtfailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "ss", $username, $email);
+	mysqli_stmt_execute($stmt);
+
+	// "Get result" returns the results from a prepared statement
+	$resultData = mysqli_stmt_get_result($stmt);
+
+	if ($row = mysqli_fetch_assoc($resultData)) {
+		return $row;
+	}
+	else {
+		$result = false;
+		return $result;
+	}
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+}
+
 // Insert new user into database
 function createUser($conn, $username, $email, $pwd, $reportingServer=false) {
 	if($reportingServer) $conn = GetReportingDBConnection();
