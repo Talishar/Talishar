540b40258c20d3f3e6a8f131b4af837d21a1fa7d
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-22 23:06:26 +0800

    Delete APIKeys.php

--- APIKeys/APIKeys.php
+++
@@ -1,6 +0,0 @@
-<?php
-  // Fake key for development / test
-  $FaBraryKey = "flesh-and-blood-online-moa4z4eauZVR8Zfdx7i7yAsutRcuvL";
-
-  // Fake key for development / test
-  $adsenseClientId = "";
