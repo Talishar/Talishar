ff7bd43103d98bf29d6dbd8b789012dd18316d6a
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-21 21:01:41 -0600

    Get initial login working

--- AccountFiles/AttemptPasswordLogin.php
+++ AccountFiles/AttemptPasswordLogin.php
@@ -1,19 +1,21 @@
 <?php
 
 include_once '../Assets/patreon-php-master/src/PatreonLibraries.php';
+include_once '../Database/ConnectionManager.php';
+include_once './AccountDatabaseAPI.php';
 
 if (isset($_POST["submit"])) {
 
   $username = $_POST["userID"];
-  $pwd = $_POST["password"];
+  $password = $_POST["password"];
   $rememberMe = isset($_POST["rememberMe"]);
 
   require_once "../includes/dbh.inc.php";
   require_once '../includes/functions.inc.php';
 
-  loginUser($username, $pwd, $rememberMe);
+  AttemptPasswordLogin($username, $password, $rememberMe);
 
 } else {
 	header("location: ../Login.php");
-    exit();
+  exit();
 }
