0809aaa0789249ba927f39275efb84e22df79292
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-22 19:41:56 -0600

    Don't break password reset

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -533,7 +533,7 @@ function SendEmail($userEmail, $url) {
   require '../vendor/autoload.php';
 
   $email = new Mail();
-  $email->setFrom("no-reply@talisharonline.com", "No-Reply");
+  $email->setFrom("no-reply@fleshandbloodonline.com", "No-Reply");
   $email->setSubject("Talishar Password Reset Link");
   $email->addTo($userEmail);
   $email->addContent(
