5b731050836970d7ffaf8fda58d427966169d385
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-22 20:41:51 -0600

    Get game list API

---
+++ APIs/APIParseGamefile.php
@@ -0,0 +1,93 @@
+<?php
+
+  if(!function_exists("GetArray"))
+  {
+    function GetArray($handler)
+    {
+      if(!$handler) return false;
+      $line = trim(fgets($handler));
+      if($line=="") return [];
+      return explode(" ", $line);
+    }
+  }
+
+  $filename = "../Games/" . $gameName . "/GameFile.txt";
+  if(!file_exists($filename)) exit;
+  $gameFileHandler = fopen($filename, "r+");
+
+  $lockTries = 0;
+  if(!$gameFileHandler) { exit; }//Game does not exist
+
+  while(!flock($gameFileHandler, LOCK_EX) && $lockTries < 10)
+  {
+    usleep(100000);//100ms
+    ++$lockTries;
+  }
+  if($lockTries == 10) exit;
+
+  $p1Data = GetArray($gameFileHandler);
+  $p2Data = GetArray($gameFileHandler);
+  $gameStatus = trim(fgets($gameFileHandler));
+  $format = trim(fgets($gameFileHandler));
+  $visibility = trim(fgets($gameFileHandler));
+  $firstPlayerChooser = trim(fgets($gameFileHandler));
+  $firstPlayer = trim(fgets($gameFileHandler));
+  $p1Key = trim(fgets($gameFileHandler));
+  $p2Key = trim(fgets($gameFileHandler));
+  $p1uid = trim(fgets($gameFileHandler));
+  $p2uid = trim(fgets($gameFileHandler));
+  $p1id = trim(fgets($gameFileHandler));
+  $p2id = trim(fgets($gameFileHandler));
+  $gameDescription = trim(fgets($gameFileHandler));
+  $hostIP = trim(fgets($gameFileHandler));
+  $p1IsPatron = trim(fgets($gameFileHandler));
+  $p2IsPatron = trim(fgets($gameFileHandler));
+  $p1DeckLink = trim(fgets($gameFileHandler));
+  $p2DeckLink = trim(fgets($gameFileHandler));
+  $p1IsChallengeActive = trim(fgets($gameFileHandler));
+  $p2IsChallengeActive = trim(fgets($gameFileHandler));
+  $joinerIP = trim(fgets($gameFileHandler));
+  $deprecated = trim(fgets($gameFileHandler));//Deprecated
+  $p1Matchups = json_decode(trim(fgets($gameFileHandler)));
+  $p2Matchups = json_decode(trim(fgets($gameFileHandler)));
+  $p1deckbuilderID = trim(fgets($gameFileHandler));
+  $p2deckbuilderID = trim(fgets($gameFileHandler));
+  $roguelikeGameID = trim(fgets($gameFileHandler));
+  $p1StartingHealth = trim(fgets($gameFileHandler));
+  $p1ContentCreatorID = trim(fgets($gameFileHandler));
+  $p2ContentCreatorID = trim(fgets($gameFileHandler));
+
+  $MGS_Initial = 0;
+  $MGS_Player2Joined = 1;
+  $MGS_ChooseFirstPlayer = 2;
+  $MGS_P2Sideboard = 3;
+  $MGS_ReadyToStart = 4;
+  $MGS_GameStarted = 5;
+
+  $FORMAT_CompCC = 1;
+  $FORMAT_CompBlitz = 3;
+
+  if(!function_exists("UnlockGamefile"))
+  {
+    function UnlockGamefile()
+    {
+      global $gameFileHandler;
+      fclose($gameFileHandler);
+    }
+  }
+/*
+  if(isset($playerID) && $gameStatus == $MGS_GameStarted)
+  {
+    $ipTarget = ($playerID == 1 ? $hostIP : $joinerIP);
+    if($ipTarget != "" && $_SERVER['REMOTE_ADDR'] != $ipTarget)
+    {
+      $hackFileName = "./BugReports/PossibleHackAttempts.txt";
+      $hackHandler = fopen($hackFileName, "a");
+      date_default_timezone_set('America/Chicago');
+      $errorDate = date('m/d/Y h:i:s a');
+      fwrite($hackHandler, "Hack Attempt? $errorDate Request: " . $_SERVER['REQUEST_URI'] . " Game name: $gameName IP: " . $_SERVER['REMOTE_ADDR'] . " Target IP: $ipTarget PlayerID: $playerID" . "\r\n");
+      fclose($hackHandler);
+    }
+  }
+*/
+?>
---
+++ APIs/GetGameList.php
@@ -0,0 +1,110 @@
+<?php
+
+include "../Libraries/SHMOPLibraries.php";
+include "../Libraries/HTTPLibraries.php";
+include "../HostFiles/Redirector.php";
+include "../CardDictionary.php";
+
+$path = "../Games";
+
+session_start();
+SetHeaders();
+
+$isMod = isset($_SESSION["useruid"]) && $_SESSION["useruid"] == "OotTheMonk";
+
+$response = new stdClass();
+$response->gamesInProgress = [];
+$response->openGames = [];
+$response->canSeeQueue = isset($_SESSION["useruid"]);
+
+
+$gameInProgressCount = 0;
+if ($handle = opendir($path)) {
+  while (false !== ($folder = readdir($handle))) {
+    if ('.' === $folder) continue;
+    if ('..' === $folder) continue;
+    $gameToken = $folder;
+    $folder = $path . "/" . $folder . "/";
+    $gs = $folder . "gamestate.txt";
+    $currentTime = round(microtime(true) * 1000);
+    if (file_exists($gs)) {
+      $lastGamestateUpdate = intval(GetCachePiece($gameToken, 6));
+      if ($currentTime - $lastGamestateUpdate < 30000) {
+        $gameInProgress = new stdClass();
+        $gameInProgress->p1Hero = GetCachePiece($gameToken, 7);
+        $gameInProgress->p2Hero = GetCachePiece($gameToken, 8);
+        $gameInProgress->secondsSinceLastUpdate = intval(($currentTime - $lastGamestateUpdate) / 1000);
+        $gameInProgress->gameName = $gameToken;
+        $gameInProgressCount += 1;
+        array_push($response->gamesInProgress, $gameInProgress);
+      }
+      else if ($currentTime - $lastGamestateUpdate > 900000) //~1 hour
+      {
+        if ($autoDeleteGames) {
+          deleteDirectory($folder);
+          DeleteCache($gameToken);
+        }
+      }
+      continue;
+    }
+
+    $gf = $folder . "GameFile.txt";
+    $gameName = $gameToken;
+    $lineCount = 0;
+    $status = -1;
+    if (file_exists($gf)) {
+      $lastRefresh = intval(GetCachePiece($gameName, 2)); //Player 1 last connection time
+      if ($lastRefresh != "" && $currentTime - $lastRefresh < 500) {
+        include 'APIParseGamefile.php';
+        $status = $gameStatus;
+        UnlockGamefile();
+      } else if ($lastRefresh == "" || $currentTime - $lastRefresh > 900000) //1 hour
+      {
+        deleteDirectory($folder);
+        DeleteCache($gameToken);
+      }
+      if ($status == 0 && $visibility == "public") {
+        $openGame = new stdClass();
+        $openGame->p1Hero = GetCachePiece($gameName, 7);
+        $formatName = "";
+        if ($format == "commoner") $formatName = "Commoner ";
+        else if ($format == "livinglegendscc") $formatName = "Open Format";
+        else if ($format == "clash") $formatName = "Clash";
+        $description = ($gameDescription == "" ? "Game #" . $gameName : $gameDescription);
+        $openGame->format = $format;
+        $openGame->formatName = $formatName;
+        $openGame->description = $description;
+        $openGame->gameName = $gameToken;
+        array_push($response->openGames, $openGame);
+      }
+    }
+
+
+  }
+  closedir($handle);
+  echo json_encode($response);
+}
+
+function deleteDirectory($dir)
+{
+  if (!file_exists($dir)) {
+    return true;
+  }
+
+  if (!is_dir($dir)) {
+    $handler = fopen($dir, "w");
+    fwrite($handler, "");
+    fclose($handler);
+    return unlink($dir);
+  }
+
+  foreach (scandir($dir) as $item) {
+    if ($item == '.' || $item == '..') {
+      continue;
+    }
+    if (!deleteDirectory($dir . "/" . $item)) {
+      return false;
+    }
+  }
+  return rmdir($dir);
+}
