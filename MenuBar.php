7882da53a93362681e4fa321d2dff291b26ec104
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-24 07:59:26 -0600

    Custom scrollbar

--- MenuBar.php
+++ MenuBar.php
@@ -108,6 +108,29 @@ $isMobile = IsMobile();
     form {
       margin-bottom:4px;
     }
+
+
+    div::-webkit-scrollbar-track
+    {
+    	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    	border-radius: 10px;
+    	background-color: #F5F5F5;
+    }
+
+    div::-webkit-scrollbar
+    {
+    	width: 12px;
+    	background-color: #F5F5F5;
+    }
+
+    div::-webkit-scrollbar-thumb
+    {
+    	border-radius: 10px;
+    	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    	background-color: #555;
+    }
+
+
   </style>
 </head>
 
