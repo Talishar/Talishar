08e83295304a540539ecf2b87b48cdbf51810033
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-23 18:28:52 -0600

    Fix favorite deck dropdown getting cut off

--- MainMenu.php
+++ MainMenu.php
@@ -89,7 +89,7 @@ if (isset($_SESSION["userid"])) {
     $selIndex = -1;
     if (isset($settingArray[$SET_FavoriteDeckIndex])) $selIndex = $settingArray[$SET_FavoriteDeckIndex];
     echo ("<div class='SelectDeckInput'>Favorite Decks: ");
-    echo ("<select style='height:26px; width:60%;' name='favoriteDecks' id='favoriteDecks'>");
+    echo ("<select style='height:32px; width:60%;' name='favoriteDecks' id='favoriteDecks'>");
     for ($i = 0; $i < count($favoriteDecks); $i += 3) {
       echo ("<option value='" . $i . "<fav>" . $favoriteDecks[$i] . "'" . ($i == $selIndex ? " selected " : "") . ">" . $favoriteDecks[$i + 1] . "</option>");
     }
