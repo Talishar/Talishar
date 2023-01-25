5321192f57b8e653ce39a9f9b3d8e049b3771ab6
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-24 19:50:11 -0600

    Add trypost function

--- Libraries/HTTPLibraries.php
+++ Libraries/HTTPLibraries.php
@@ -4,6 +4,11 @@ function TryGET($key, $default = "")
   return isset($_GET[$key]) ? $_GET[$key] : $default;
 }
 
+function TryPOST($key, $default = "")
+{
+  return isset($_POST[$key]) ? $_POST[$key] : $default;
+}
+
 function IsGameNameValid($gameName)
 {
   return is_numeric($gameName);
