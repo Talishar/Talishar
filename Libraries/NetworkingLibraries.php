c8da05729ea3bd15f94b6cffb14cf18c23cf786a
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-26 19:01:31 -0600

    Revert "Actual life of the party fix"
    This reverts commit fabdece62eb117f871b16fb95405165b4e94b02f.

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1171,8 +1171,7 @@ function PlayCard($cardID, $from, $dynCostResolved = -1, $index = -1, $uniqueID 
         AddDecisionQueue("DYNPITCH", $currentPlayer, $dynCost);
         AddDecisionQueue("SETCLASSSTATE", $currentPlayer, $CS_LastDynCost);
       }
-      if ($playingCard && $dynCost == "") AddDecisionQueue("PASSPARAMETER", $currentPlayer, CardCost($cardID));
-      else if($dynCost == "") AddDecisionQueue("PASSPARAMETER", $currentPlayer, "0");
+      if($dynCost == "") AddDecisionQueue("PASSPARAMETER", $currentPlayer, "0");
       AddDecisionQueue("RESUMEPAYING", $currentPlayer, $cardID . "-" . $from . "-" . $index);
       $decisionQueue = array_merge($decisionQueue, $dqCopy);
       ProcessDecisionQueue();
