c78b794b4a1951cf35e348dd494bcb99f0ffd77d
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-28 17:30:45 -0600

    Fix mentor pitch values

--- CardDictionary.php
+++ CardDictionary.php
@@ -501,6 +501,7 @@ function PitchValue($cardID)
     return ROGUEPitchValue($cardID);
   }
   switch ($cardID) {
+    case "MON404": case "MON405": case "MON406": case "MON407": return 0;
     default:
       return 3;
   }
