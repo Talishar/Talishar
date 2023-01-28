ce8910c88a625fbbdd19aa95194ff05aaabac6d7
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-28 09:36:24 -0600

    Fix PHP error

--- CreateGame.php
+++ CreateGame.php
@@ -38,8 +38,11 @@ session_start();
 
 if (!isset($_SESSION["userid"])) {
   if (isset($_COOKIE["rememberMeToken"])) {
-    include_once 'includes/functions.inc.php';
-    include_once 'includes/dbh.inc.php';
+    include_once './includes/functions.inc.php';
+    include_once './includes/dbh.inc.php';
+    include_once './Assets/patreon-php-master/src/PatreonLibraries.php';
+    include_once './Assets/patreon-php-master/src/API.php';
+    include_once './Assets/patreon-php-master/src/PatreonDictionary.php';
     loginFromCookie();
   }
 }
