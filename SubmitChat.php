b701b6bab32fd61eebc82dc981cf7918d2867734
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-28 19:37:21 -0600

    Add profanity filter

--- SubmitChat.php
+++ SubmitChat.php
@@ -35,10 +35,22 @@ if (isset($_SESSION["isPatron"]) && isset($_SESSION['useruid']) && !in_array($_S
 //This is the code for Contributor's icon.
 if (isset($_SESSION['useruid']) && in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Contributor' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./Images/copper.webp' /> " . $displayName;
 
+$filteredChatText = str_replace("fuck", "****", $chatText);
+$filteredChatText = str_replace("shit", "****", $filteredChatText);
+$filteredChatText = str_replace("piss", "****", $filteredChatText);
+$filteredChatText = str_replace("cock", "****", $filteredChatText);
+$filteredChatText = str_replace("bitch", "****", $filteredChatText);
+$filteredChatText = str_replace("retard", "****", $filteredChatText);
+$filteredChatText = str_replace("idiot", "****", $filteredChatText);
+
 $filename = "./Games/" . $gameName . "/gamelog.txt";
 $handler = fopen($filename, "a");
-$output = "<span style='font-weight:bold; color:<PLAYER" . $playerID . "COLOR>;'>" . $displayName . ": </span>" . $chatText;
+$output = "<span style='font-weight:bold; color:<PLAYER" . $playerID . "COLOR>;'>" . $displayName . ": </span>" . $filteredChatText;
 fwrite($handler, $output . "\r\n");
+if($chatText != $filteredChatText)
+{
+  fwrite($handler, "<span style='color:red;'>A message from the developers:</span> Talishar is a fan-made project. We spend a lot of time making this site because it makes us happy to bring joy to you all. If you want to see the site continue to be supported, please don't harass each other.\r\n");
+}
 fclose($handler);
 
 GamestateUpdated($gameName);
