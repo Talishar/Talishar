01f2ff2030c62d89c11227ce73b8832a30dfe2dc
OotTheMonk <nathanandrecoder@gmail.com>
2023-01-30 20:11:04 -0600

    change to new json url

--- zzCardCodeGenerator.php
+++ zzCardCodeGenerator.php
@@ -1,6 +1,6 @@
 <?php
 
-  $jsonUrl = "https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/json/json/english/card.json";
+  $jsonUrl = "https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/v5.0.0/json/english/card.json";
   $curl = curl_init();
   $headers = array(
     "Content-Type: application/json",
