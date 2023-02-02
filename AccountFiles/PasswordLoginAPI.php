3bf3129029ec6e75afe879702d434f487e390c61
OotTheMonk <nathanandrecoder@gmail.com>
2023-02-01 19:35:16 -0600

    First bit of password login api

--- AccountFiles/PasswordLoginAPI.php
+++ AccountFiles/PasswordLoginAPI.php
@@ -1,10 +1,15 @@
 <?php
+include_once './AccountSessionAPI.php';
 
-include_once '../Assets/patreon-php-master/src/PatreonLibraries.php';
+include_once '../Assets/patreon-php-master/src/OAuth.php';
 include_once '../Assets/patreon-php-master/src/API.php';
+include_once '../Assets/patreon-php-master/src/PatreonLibraries.php';
 include_once '../Assets/patreon-php-master/src/PatreonDictionary.php';
+include_once '../includes/functions.inc.php';
+include_once '../includes/dbh.inc.php';
 include_once '../Database/ConnectionManager.php';
 include_once './AccountDatabaseAPI.php';
+
 
 if (isset($_POST["submit"])) {
 
@@ -20,3 +25,18 @@ if (isset($_POST["submit"])) {
 	header("location: ../Login.php");
   exit();
 }
+
+
+$response = new stdClass();
+$response->isUserLoggedIn = IsUserLoggedIn();
+if($response->isUserLoggedIn)
+{
+  $response->loggedInUserID = LoggedInUser();
+  $response->loggedInUserName = LoggedInUserName();
+}
+
+echo(json_encode($response));
+
+exit;
+
+?>
