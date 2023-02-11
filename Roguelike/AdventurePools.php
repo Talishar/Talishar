8cc5b827675f2e96c60d32e8e064a699c56fa678
FuzzyCodes <minecraftfuzz@gmail.com>
2023-02-11 14:41:10 -0800

    Untesting state

--- Roguelike/AdventurePools.php
+++ Roguelike/AdventurePools.php
@@ -57,7 +57,7 @@ function GetNextEncounter() //TODO overhaul this whole function and children
 
 function RandomEvent()
 {
-  $commonEvents = array(/*"Explore_some_nearby_ruins", */"Visit_a_local_library", "Enter_a_nearby_Temple");
+  $commonEvents = array("Explore_some_nearby_ruins", "Visit_a_local_library", "Enter_a_nearby_Temple");
   $rareEvents = array("Explore_some_nearby_ruins", "Enter_a_nearby_Temple");
   $majesticEvents = array("Explore_some_nearby_ruins", "Visit_a_local_library", "Enter_a_nearby_Temple");
   $randEvent = rand(1,100);
