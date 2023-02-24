37ff0556a9ae2ac49b4ed8b49adea43b4ac83770
FuzzyCodes <minecraftfuzz@gmail.com>
2023-02-23 21:49:42 -0800

    I added more
    More generics that is

--- Roguelike/EncounterPools.php
+++ Roguelike/EncounterPools.php
@@ -17,6 +17,8 @@ function GetPool($type, $hero, $rarity, $background){
   WriteLog($type." ".$rarity." ".$background);
   if(($hero == "Bravo" || $hero == "Dorinthea") && $type == "Talent") $type = "Class";
   return GetPool2(array($type, $rarity, $background));
+
+  //Don't delete this stuff
   /*
   if($type == "Light")
   {
@@ -297,7 +299,7 @@ function GetPool($type, $hero, $rarity, $background){
 }
 
 //Input a list of parameters
-function GetPool($arrayParameters){
+function GetPool2($arrayParameters){
 
   $CardRewardPool = array(
     array("WTR043", "Class", "Majestic", "Anothos", "TitanFist", "Sledge"),
@@ -360,9 +362,268 @@ function GetPool($arrayParameters){
     array("WTR146", "Class", "Common", "Saber", "Dawnblade", "Hatchet", "Battleaxe"),
     array("WTR147", "Class", "Common", "Saber", "Dawnblade", "Hatchet", "Battleaxe"), //Nature's Path Pilgrimage
     array("WTR148", "Class", "Common", "Saber", "Dawnblade", "Hatchet", "Battleaxe"),
-    array("WTR149", "Class", "Common", "Saber", "Dawnblade", "Hatchet", "Battleaxe")
-    
+    array("WTR149", "Class", "Common", "Saber", "Dawnblade", "Hatchet", "Battleaxe"),
+  );
+  
+  $returnPool = array(); // Create an empty list of cards to be returned
+  $sizeParameters = count($arrayParameters);
+  $paramCheck = new SplFixedArray($sizeParameters); //Create a shadow of the parameters...
+  for ($i = 0; $i < $sizeParameters; $i++){ //... The same length as the list of parameters
+    $paramCheck[$i] = false;
+  }
+  $eligible = true;
+  for($i = 0; $i < count($CardRewardPool); $i++){
+    $eligible = true;
+    for($j = 0; $j < $sizeParameters; $j++){
+      $paramCheck[$j] = false;
+    }
+    for($j = 0; $j < $sizeParameters; $j++){
+      for($k = 1; $k < count($CardRewardPool[$i]); $k++){
+        if($arrayParameters[$j] == $CardRewardPool[$i][$k]){
+          $paramCheck[$j] = true; 
+        }
+      }
+      if($paramCheck[$j] == false){
+        $eligible = false;
+        break;
+      }
+    }
+    if($eligible) {
+      array_push($returnPool, $CardRewardPool[$i][0]); 
+    }
+  }
+    WriteLog(ArrayAsString($returnPool));
+    return $returnPool;
+}
+function GetPool3($arrayParameters){
 
+  //Currently, Generics are available to all heroes equally. 
+  //In the future, if we want to have certain generics available to certain heroes, we can go back and tag most cards with "All", and specific ones with the name of that hero
+  $CardRewardPool = array(
+    array("WTR159", "Majestic"), //Enlightened Strike
+    array("WTR160", "Majestic"), // Tome of Fyendal
+    array("WTR161", "Majestic"), // Last Ditch Effort
+    array("WTR162", "Majestic"), // Crazy Brew - Super, but tagged as Majestic
+    array("WTR163", "Majestic"), // Remembrance - Super, but tagged as Majestic
+    array("WTR164", "Rare"), //Drone of Brutality
+    array("WTR165", "Rare"),
+    array("WTR166", "Rare"),
+    array("WTR167", "Rare"), //Snatch
+    array("WTR168", "Rare"),
+    array("WTR169", "Rare"),
+    array("WTR170", "Rare"), //Energy potion
+    array("WTR171", "Rare"), //Potion of Strength
+    array("WTR172", "Rare"), //Timesnap Potion
+    array("WTR173", "Rare"), //Sigil of Solace - Nate thinks that these shouldn't be in the pool because they're too strong. 
+    array("WTR174", "Rare"), // Nate, if you're reading this, feel free to remove these from the pool!
+    array("WTR175", "Rare"), // Sigil of Solace (Blue)
+    array("WTR176", "Common"), //Barraging Brawnhide
+    array("WTR177", "Common"),
+    array("WTR178", "Common"),
+    array("WTR179", "Common"), //Demolition Crew
+    array("WTR180", "Common"),
+    array("WTR181", "Common"),
+    array("WTR182", "Common"), //Flock of the Feather Walkers
+    array("WTR183", "Common"),
+    array("WTR184", "Common"),
+    array("WTR185", "Common"), //Nimble Strike
+    array("WTR186", "Common"),
+    array("WTR187", "Common"),
+    array("WTR188", "Common"), //Raging Onslaught
+    array("WTR189", "Common"),
+    array("WTR190", "Common"),
+    array("WTR191", "Common"), //Scar for a Scar
+    array("WTR192", "Common"), 
+    array("WTR193", "Common"),
+    array("WTR194", "Common"), //Scour the Battlescape
+    array("WTR195", "Common"),
+    array("WTR196", "Common"),
+    array("WTR197", "Common"), //Regurgitating Slog
+    array("WTR198", "Common"),
+    array("WTR199", "Common"),
+    array("WTR200", "Common"), //Wounded Bull
+    array("WTR201", "Common"),
+    array("WTR202", "Common"),
+    array("WTR203", "Common"), //Wounding Blow
+    array("WTR204", "Common"),
+    array("WTR205", "Common"),
+    array("WTR206", "Common"), //Pummel
+    array("WTR207", "Common"),
+    array("WTR208", "Common"),
+    array("WTR209", "Common"), //Razor Reflex
+    array("WTR210", "Common"),
+    array("WTR211", "Common"),
+    array("WTR212", "Common"), //Unmovable
+    array("WTR213", "Common"),
+    array("WTR214", "Common"),
+    array("WTR215", "Common"), //Sink Below
+    array("WTR216", "Common"),
+    array("WTR217", "Common"),
+    array("WTR218", "Common"), //Nimblism
+    array("WTR219", "Common"),
+    array("WTR220", "Common"),
+    array("WTR221", "Common"), //Sloggism
+    array("WTR222", "Common"),
+    array("WTR223", "Common"),
+
+    array("ARC159", "Majestic"), //Command and motherflippin' Conquer
+    array("ARC160", "Majestic"), //Art of War
+    array("ARC161", "Majestic"), //Pursuit of Knowledge
+    array("ARC162", "Majestic"), //Chains of Eminence - Super, but tagged with Majestics
+    //ARC163 - Rusted Relic - No interaction with arcane dmg
+    array("ARC164", "Rare"), //Life for a life
+    array("ARC165", "Rare"),
+    array("ARC166", "Rare"),
+    array("ARC167", "Rare"), //Enchanting Melody
+    array("ARC168", "Rare"),
+    array("ARC169", "Rare"),
+    array("ARC170", "Rare"), //Plunder Run
+    array("ARC171", "Rare"),
+    array("ARC172", "Rare"),
+    //ARC173-175 - Eirina's Prayer - No interaction with arcane dmg
+    array("ARC176", "Common"), //Back Alley Breakline
+    array("ARC177", "Common"),
+    array("ARC178", "Common"),
+    array("ARC179", "Common"), //Cadaverous Contraband
+    array("ARC180", "Common"),
+    array("ARC181", "Common"),
+    array("ARC182", "Common"), //Fervent Forerunner
+    array("ARC183", "Common"),
+    array("ARC184", "Common"),
+    array("ARC185", "Common"), //Moon Wish
+    array("ARC186", "Common"),
+    array("ARC187", "Common"),
+    array("ARC188", "Common"), //Push the Point 
+    array("ARC189", "Common"),
+    array("ARC190", "Common"),
+    array("ARC191", "Common"), //Ravenous Rabble
+    array("ARC192", "Common"),
+    array("ARC193", "Common"),
+    array("ARC194", "Common"), //Rifting
+    array("ARC195", "Common"),
+    array("ARC196", "Common"),
+    array("ARC197", "Common"),//Vigor Rush
+    array("ARC198", "Common"),
+    array("ARC199", "Common"),
+    array("ARC200", "Common"), //Fate Foreseen
+    array("ARC201", "Common"),
+    array("ARC202", "Common"),
+    array("ARC203", "Common"), //Come to Fight 
+    array("ARC204", "Common"),
+    array("ARC205", "Common"),
+    array("ARC206", "Common"), //Force Sight
+    array("ARC207", "Common"),
+    array("ARC208", "Common"),
+    array("ARC209", "Common"), //Lead the Charge
+    array("ARC210", "Common"),
+    array("ARC211", "Common"),
+    array("ARC212", "Common"), //Sun Kiss
+    array("ARC213", "Common"),
+    array("ARC214", "Common"),
+    array("ARC215", "Common"), //Whisper of the Oracle
+    array("ARC216", "Common"),
+    array("ARC217", "Common"),
+
+    array("CRU180", "Majestic"), //Coax a Commotion
+    array("CRU181", "Majestic"), //Gorganian Tome
+    array("CRU182", "Majestic"), //Snag
+    array("CRU183", "Rare"), //Promise of Plenty
+    array("CRU184", "Rare"),
+    array("CRU185", "Rare"),
+    array("CRU186", "Rare"), //Lunging Press
+    array("CRU187", "Rare"), //Springboard Assault
+    array("CRU188", "Rare"), //Cash In
+    array("CRU189", "Rare"), //Reinforce the Line
+    array("CRU190", "Rare"),
+    array("CRU191", "Rare"),
+    array("CRU192", "Common"), //Brutal Assault
+    array("CRU193", "Common"),
+    array("CRU194", "Common"),
+
+    array("MON245", "Majestic"), //Exude Confidence
+    array("MON246", "Majestic"), //Nourishing Emptiness
+    array("MON247", "Majestic"), //Rouse the Ancients
+    array("MON248", "Rare"), //Out Muscle
+    array("MON249", "Rare"), 
+    array("MON250", "Rare"), 
+    array("MON251", "Rare"), //Seek Horizon
+    array("MON252", "Rare"),
+    array("MON253", "Rare"),
+    array("MON254", "Rare"), //Tremor of iArathael - Honestly this one we might want to take out? I'm leaving it in for now
+    array("MON255", "Rare"),
+    array("MON256", "Rare"),
+    array("MON257", "Rare"), //Rise Above
+    array("MON258", "Rare"),
+    array("MON259", "Rare"),
+    array("MON260", "Rare"), //Captain's Call
+    array("MON261", "Rare"),
+    array("MON262", "Rare"),
+    array("MON263", "Common"), //Adrenaline Rush
+    array("MON264", "Common"),
+    array("MON265", "Common"),
+    array("MON266", "Common"), //Belittle
+    array("MON267", "Common"),
+    array("MON268", "Common"),
+    array("MON269", "Common"), //Brandish
+    array("MON270", "Common"),
+    array("MON271", "Common"),
+    array("MON272", "Common"), //Frontline Scout
+    array("MON273", "Common"),
+    array("MON274", "Common"),
+    array("MON275", "Common"), //Overload
+    array("MON276", "Common"),
+    array("MON277", "Common"),
+    array("MON278", "Common"), //Pound for Pound
+    array("MON279", "Common"),
+    array("MON280", "Common"),
+    array("MON281", "Common"), //Rally the Rearguard
+    array("MON282", "Common"),
+    array("MON283", "Common"),
+    array("MON284", "Common"), //Stony Woottonhog - The unofficial Roguelike Mascot
+    array("MON285", "Common"),
+    array("MON286", "Common"),
+    array("MON287", "Common"), //Surging Militia
+    array("MON288", "Common"),
+    array("MON289", "Common"),
+    array("MON290", "Common"), //Yinti Yanti
+    array("MON291", "Common"),
+    array("MON292", "Common"),
+    array("MON293", "Common"), //Zealous Belting
+    array("MON294", "Common"),
+    array("MON295", "Common"),
+    array("MON296", "Common"), //Minnowism
+    array("MON297", "Common"),
+    array("MON298", "Common"),
+    array("MON299", "Common"), //Warmonger's Recital
+    array("MON300", "Common"),
+    array("MON301", "Common"),
+    //MON302 - Talisman of Dousing - No need for spellvoid
+    array("MON303", "Common"), //Memorial Ground
+    array("MON304", "Common"),
+    array("MON305", "Common"),
+    //
+    array("EVR156", "Majestic"), //Bingo
+    array("EVR157", "Majestic"), //Firebreathing
+    array("EVR158", "Majestic"), //Cash Out
+    array("EVR159", "Majestic"), //Knick Knack Bric-a-brac
+    array("EVR160", "Majestic"), //This Round's on Me
+    array("EVR161", "Rare"), //Life of the Party
+    array("EVR162", "Rare"), 
+    array("EVR163", "Rare"), 
+    //EVR164-166 - High Striker - I've decided to omit this one, but if someone wants to add it in feel free
+    array("EVR167", "Rare"), //Pick a Card, Any Card
+    array("EVR168", "Rare"),
+    array("EVR169", "Rare"),
+    array("EVR156", "Rare"), //Smashing Good Time
+    array("EVR157", "Rare"),
+    array("EVR158", "Rare"),
+    array("EVR159", "Rare"), //Even Bigger Than That
+    array("EVR160", "Rare"),
+    array("EVR161", "Rare"),
+    //EVER176 through EVER193 are all potions. 
+    //I decided to omit all the potions for now, but feel free to add some/all
+
+    //TODO: Uprising and Dynasty
   );
   
   $returnPool = array(); // Create an empty list of cards to be returned
