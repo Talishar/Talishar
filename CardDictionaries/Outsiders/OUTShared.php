9f7cb41268a238787b5a72b71892c9271d60c823
OotTheMonk <nathanandrecoder@gmail.com>
2023-03-07 20:39:34 -0600

    Shakedown fix for contracts

--- CardDictionaries/Outsiders/OUTShared.php
+++ CardDictionaries/Outsiders/OUTShared.php
@@ -564,7 +564,7 @@ function OUTAbilityCost($cardID)
           AddDecisionQueue("PREPENDLASTRESULT", $mainPlayer, "THEIRHAND:pitch=", 1);
           AddDecisionQueue("MULTIZONEINDICES", $mainPlayer, "<-", 1);
           AddDecisionQueue("CHOOSEMULTIZONE", $mainPlayer, "<-", 1);
-          AddDecisionQueue("MZBANISH", $mainPlayer, "HAND,-," . $defPlayer, 1);
+          AddDecisionQueue("MZBANISH", $mainPlayer, "HAND,-," . $mainPlayer, 1);
           AddDecisionQueue("MZREMOVE", $mainPlayer, "-", 1);
         }
         break;
