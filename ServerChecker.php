668de645affa6e1cde5a1f8dde29443dbbf5c26a
OotTheMonk <nathanandrecoder@gmail.com>
2023-03-14 20:47:14 -0500

    Fix private games no longer showing up in server count

--- ServerChecker.php
+++ ServerChecker.php
@@ -51,9 +51,9 @@ if ($handle = opendir($path)) {
       if ($currentTime - $lastGamestateUpdate < 30000) {
         $p1Hero = GetCachePiece($gameToken, 7);
         $p2Hero = GetCachePiece($gameToken, 8);
+        if($p2Hero != "") $gameInProgressCount += 1;
         $visibility = GetCachePiece($gameToken, 9);
         if ($p2Hero != "" && $visibility == "1") {
-          $gameInProgressCount += 1;
           $spectateLinks .= "<form style='text-align:center;' action='" . $reactFE . "'>";
           $spectateLinks .= "<center><table><tr><td style='vertical-align:middle; padding-left:8px; width:50px; height: 40px;'>";
           if ($p1Hero == "") {
