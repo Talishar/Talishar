88cda185dcda5e11485b892dcc2ece6a44d3fc18
Etasus <120968483+Etasus@users.noreply.github.com>
2023-03-17 15:55:23 -0500

    Gave the new powers Rarities
    d'oh

--- CardDictionaries/Roguelike/ROGUEShared.php
+++ CardDictionaries/Roguelike/ROGUEShared.php
@@ -379,6 +379,19 @@ function ROGUERarity($cardID)
       case "ROGUE507": case "ROGUE508": case "ROGUE509": case "ROGUE510": case "ROGUE511": case "ROGUE512": case "ROGUE513": case "ROGUE516": case "ROGUE517": return "C";
       case "ROGUE501": case "ROGUE504": case "ROGUE518": case "ROGUE519": case "ROGUE521": case "ROGUE522": case "ROGUE523": case "ROGUE524": case "ROGUE525": return "R";
       case "ROGUE502": case "ROGUE503": case "ROGUE505": case "ROGUE506": case "ROGUE516": case "ROGUE526": case "ROGUE527": case "ROGUE528": return "M";
+
+      case "ROGUE601": case "ROGUE602": case "ROGUE603": case "ROGUE604": case "ROGUE605":
+      case "ROGUE606": case "ROGUE607": case "ROGUE608": case "ROGUE609": case "ROGUE610":
+      case "ROGUE611": case "ROGUE612": case "ROGUE613": case "ROGUE614": case "ROGUE615":
+      case "ROGUE616":
+        return "C";
+      case "ROGUE701": case "ROGUE702": case "ROGUE703": case "ROGUE704": case "ROGUE705":
+      case "ROGUE706": case "ROGUE707": case "ROGUE708": case "ROGUE709": case "ROGUE710":
+      case "ROGUE711":
+        return "R";
+      case "ROGUE801": case "ROGUE802": case "ROGUE803": case "ROGUE804": case "ROGUE805":
+      case "ROGUE806":
+        return "M";
       default: return "";
     }
 }
