36c7e984f70ffb4d677674bc3a5ab1849358a160
OotTheMonk <nathanandrecoder@gmail.com>
2023-04-09 08:51:48 -0500

    Spark of Genius enforce min item cost too

--- CardDictionaries/ArcaneRising/ARCMechanologist.php
+++ CardDictionaries/ArcaneRising/ARCMechanologist.php
@@ -44,7 +44,7 @@ function ARCMechanologistPlayAbility($cardID, $from, $resourcesPaid, $target = "
       AddCurrentTurnEffect($cardID, $currentPlayer);
       return "";
     case "ARC009":
-      AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "MYDECK:subtype=Item;class=MECHANOLOGIST;maxCost=" . (GetClassState($currentPlayer, $CS_LastDynCost) / 2));
+      AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "MYDECK:subtype=Item;class=MECHANOLOGIST;minCost=" . (GetClassState($currentPlayer, $CS_LastDynCost) / 2) . ";maxCost=" . (GetClassState($currentPlayer, $CS_LastDynCost) / 2));
       AddDecisionQueue("MAYCHOOSEMULTIZONE", $currentPlayer, "<-", 1);
       AddDecisionQueue("MZREMOVE", $currentPlayer, "-", 1);
       AddDecisionQueue("PUTPLAY", $currentPlayer, 0, 1);
