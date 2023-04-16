d38d72ad029b4b2c3432da79c001ff00cf5dd657
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-04-16 14:11:14 +0800

    Adjust playmat names and ordering

--- APIs/GetCosmetics.php
+++ APIs/GetCosmetics.php
@@ -12,15 +12,13 @@ SetHeaders();
 $response = new stdClass();
 $response->cardBacks = [];
 $response->playmats = [];
-if(IsUserLoggedIn()) {
+if (IsUserLoggedIn()) {
   foreach (PatreonCampaign::cases() as $campaign) {
-    if(isset($_SESSION[$campaign->SessionID()]) || (isset($_SESSION["useruid"]) && $campaign->IsTeamMember($_SESSION["useruid"])))
-    {
+    if (isset($_SESSION[$campaign->SessionID()]) || (isset($_SESSION["useruid"]) && $campaign->IsTeamMember($_SESSION["useruid"]))) {
       //Check card backs first
       $cardBacks = $campaign->CardBacks();
       $cardBacks = explode(",", $cardBacks);
-      for($i=0; $i<count($cardBacks); ++$i)
-      {
+      for ($i = 0; $i < count($cardBacks); ++$i) {
         $cardBack = new stdClass();
         $cardBack->name = $campaign->CampaignName() . (count($cardBacks) > 1 ? " " . $i + 1 : "");
         $cardBack->id = $cardBacks[$i];
@@ -29,8 +27,7 @@ if(IsUserLoggedIn()) {
     }
   }
 
-  for($i=0; $i<8; ++$i)
-  {
+  for ($i = 0; $i < 8; ++$i) {
     $playmat = new stdClass();
     $playmat->id = $i;
     $playmat->name = GetPlaymatName($i);
@@ -41,16 +38,28 @@ echo json_encode($response);
 
 function GetPlaymatName($id)
 {
-  switch($id)
-  {
-    case 0: return "aria";
-    case 1: return "demonastery";
-    case 2: return "metrix";
-    case 3: return "misteria";
-    case 4: return "pits";
-    case 5: return "savage";
-    case 6: return "solana";
-    case 7: return "volcor";
-    default: return "N/A";
+  switch ($id) {
+    case 0:
+      return "plain";
+    case 1:
+      return "demonastery";
+    case 2:
+      return "metrix";
+    case 3:
+      return "misteria";
+    case 4:
+      return "pits";
+    case 5:
+      return "savage";
+    case 6:
+      return "solana";
+    case 7:
+      return "volcor";
+    case 8:
+      return "training-dummy";
+    case 9:
+      return "aria";
+    default:
+      return "N/A";
   }
 }
