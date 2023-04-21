9f4814c864d811605e94ff67709f87e5e2854ca8
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-04-21 16:09:41 +0800

    update images so no longer broken on new FE

--- SubmitChat.php
+++ SubmitChat.php
@@ -15,12 +15,12 @@ $authKey = $_GET["authKey"];
 
 session_start();
 
-if($authKey == "") $authKey = $_COOKIE["lastAuthKey"];
+if ($authKey == "") $authKey = $_COOKIE["lastAuthKey"];
 
 $targetAuthKey = "";
 if ($playerID == 1 && isset($_SESSION["p1AuthKey"])) $targetAuthKey = $_SESSION["p1AuthKey"];
 else if ($playerID == 2 && isset($_SESSION["p2AuthKey"])) $targetAuthKey = $_SESSION["p2AuthKey"];
-if($authKey != $targetAuthKey) exit;
+if ($authKey != $targetAuthKey) exit;
 
 $uid = "-";
 if (isset($_SESSION['useruid'])) $uid = $_SESSION['useruid'];
@@ -30,10 +30,10 @@ $displayName = ($uid != "-" ? $uid : "Player " . $playerID);
 $contributors = array("sugitime", "OotTheMonk", "Launch", "LaustinSpayce", "Star_Seraph", "Tower", "Etasus", "scary987", "Celenar");
 
 //its sort of sloppy, but it this will fail if you're in the contributors array because we want to give you the contributor icon, not the patron icon.
-if (isset($_SESSION["isPatron"]) && isset($_SESSION['useruid']) && !in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Patron' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./Images/patronHeart.webp' /> " . $displayName;
+if (isset($_SESSION["isPatron"]) && isset($_SESSION['useruid']) && !in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Patron' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./images/patronHeart.webp' /> " . $displayName;
 
 //This is the code for Contributor's icon.
-if (isset($_SESSION['useruid']) && in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Contributor' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./Images/copper.webp' /> " . $displayName;
+if (isset($_SESSION['useruid']) && in_array($_SESSION['useruid'], $contributors)) $displayName = "<img title='Contributor' style='margin-bottom:-2px; margin-right:-4px; height:18px;' src='./images/copper.webp' /> " . $displayName;
 
 $filteredChatText = str_replace("fuck", "****", $chatText);
 $filteredChatText = str_replace("shit", "****", $filteredChatText);
@@ -50,8 +50,8 @@ $filename = "./Games/" . $gameName . "/gamelog.txt";
 $handler = fopen($filename, "a");
 $output = "<span style='font-weight:bold; color:<PLAYER" . $playerID . "COLOR>;'>" . $displayName . ": </span>" . $filteredChatText;
 fwrite($handler, $output . "\r\n");
-if(GetCachePiece($gameName, 11) >= 3) fwrite($handler, "The lobby is reactivated.\r\n");
+if (GetCachePiece($gameName, 11) >= 3) fwrite($handler, "The lobby is reactivated.\r\n");
 fclose($handler);
 
 GamestateUpdated($gameName);
-if($playerID == 1) SetCachePiece($gameName, 11, 0);
+if ($playerID == 1) SetCachePiece($gameName, 11, 0);
