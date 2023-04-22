5da3243051f582b725b1c9c2c358b20cad11f01e
OotTheMonk <nathanandrecoder@gmail.com>
2023-04-22 11:12:48 -0500

    Create Trie library

--- zzCardCodeGenerator.php
+++ zzCardCodeGenerator.php
@@ -1,6 +1,7 @@
 <?php
 
   include './zzImageConverter.php';
+  include './Libraries/Trie.php';
 
   //$jsonUrl = "https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/v5.0.0/json/english/card.json";
   $jsonUrl = "https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/outsiders/json/english/card.json";
@@ -142,41 +143,6 @@
     else TraverseTrie($trie, "", $handler, $isString, $defaultValue);
 
     fwrite($handler, "}\r\n\r\n");
-  }
-
-  function TraverseTrie(&$trie, $keySoFar, &$handler=null, $isString=true, $defaultValue="")
-  {
-    $default = ($defaultValue != "" ? ($isString ? "\"" . $defaultValue . "\"" : $defaultValue) : ($isString ? "\"\"" : "0"));
-    $depth = strlen($keySoFar);
-    if(is_array($trie))
-    {
-      fwrite($handler, "switch(\$cardID[" . $depth . "]) {\r\n");
-      foreach ($trie as $key => $value)
-      {
-        fwrite($handler, "case \"" . $key . "\":\r\n");
-        TraverseTrie($trie[$key], $keySoFar . $key, $handler, $isString, $defaultValue);
-      }
-      fwrite($handler, "default: return " . $default . ";\r\n");
-      fwrite($handler, "}\r\n");
-    }
-    else
-    {
-      if($handler != null)
-      {
-        if($isString) fwrite($handler, "return \"" . $trie . "\";\r\n");
-        else fwrite($handler, "return " . $trie . ";\r\n");
-      }
-    }
-  }
-
-  function AddToTrie(&$trie, $cardID, $depth, $value)
-  {
-    if($depth < strlen($cardID)-1)
-    {
-      if(!array_key_exists($cardID[$depth], $trie)) $trie[$cardID[$depth]] = [];
-      AddToTrie($trie[$cardID[$depth]], $cardID, $depth+1, $value);
-    }
-    else if(!isset($trie[$cardID[$depth]])) $trie[$cardID[$depth]] = $value;
   }
 
   function MapType($card)
