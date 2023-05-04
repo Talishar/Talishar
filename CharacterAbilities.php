c3e7cee24f2d529c3bc746d8caace780125106be
OotTheMonk <nathanandrecoder@gmail.com>
2023-05-03 19:04:17 -0500

    Fix equipment with ward interaction with unpreventable damage

--- CharacterAbilities.php
+++ CharacterAbilities.php
@@ -93,7 +93,6 @@ function CharacterTakeDamageAbility($player, $index, $damage, $preventable)
   $otherPlayer = $player == 1 ? 1 : 2;
   //CR 2.1 6.4.10f If an effect states that a prevention effect can not prevent the damage of an event, the prevention effect still applies to the event but its prevention amount is not reduced. Any additional modifications to the event by the prevention effect still occur.
   $type = "-";//Add this if it ever matters
-  $preventable = CanDamageBePrevented($otherPlayer, $damage, $type);
   switch ($char[$index]) {
     case "DYN213":
       if ($damage > 0) {
