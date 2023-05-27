9c95efb1aacd874c624328c053f232448d9a2831
Tobias Reincke <tobias.reincke@uni-rostock.de>
2023-05-27 11:28:23 +0200

    added script to generate php function to get hero type attribute Young/Adult

---
+++ GeneratedCode/hero_type.php
@@ -0,0 +1,881 @@
+
+function return_Hero_Type($Cardcode){
+    switch($Cardcode){
+
+	case "LSS001":
+		return Adult;
+		break;
+	case "LSS003":
+		return Young;
+		break;
+	case "LSS003":
+		return Young;
+		break;
+	case "LSS004":
+		return Young;
+		break;
+	case "LSS004":
+		return Young;
+		break;
+	case "JDG001":
+		return Adult;
+		break;
+	case "JDG001":
+		return Adult;
+		break;
+	case "JDG002":
+		return Adult;
+		break;
+	case "JDG006":
+		return Adult;
+		break;
+	case "HER001":
+		return Adult;
+		break;
+	case "HER021":
+		return Adult;
+		break;
+	case "WIN001":
+		return Adult;
+		break;
+	case "RNR001":
+		return Adult;
+		break;
+	case "1HP001":
+		return Adult;
+		break;
+	case "WTR001":
+		return Adult;
+		break;
+	case "WTR001":
+		return Adult;
+		break;
+	case "CRU001":
+		return Adult;
+		break;
+	case "CRU001":
+		return Adult;
+		break;
+	case "HER005":
+		return Young;
+		break;
+	case "HER054":
+		return Young;
+		break;
+	case "XXX003":
+		return Young;
+		break;
+	case "RNR002":
+		return Young;
+		break;
+	case "RVD001":
+		return Young;
+		break;
+	case "1HP002":
+		return Young;
+		break;
+	case "WTR002":
+		return Young;
+		break;
+	case "WTR002":
+		return Young;
+		break;
+	case "HER002":
+		return Adult;
+		break;
+	case "HER022":
+		return Adult;
+		break;
+	case "WIN002":
+		return Adult;
+		break;
+	case "BVO001":
+		return Adult;
+		break;
+	case "1HP043":
+		return Adult;
+		break;
+	case "WTR038":
+		return Adult;
+		break;
+	case "WTR038":
+		return Adult;
+		break;
+	case "CRU022":
+		return Adult;
+		break;
+	case "CRU022":
+		return Adult;
+		break;
+	case "HER006":
+		return Young;
+		break;
+	case "HER055":
+		return Young;
+		break;
+	case "XXX002":
+		return Young;
+		break;
+	case "BVO002":
+		return Young;
+		break;
+	case "1HP044":
+		return Young;
+		break;
+	case "WTR039":
+		return Young;
+		break;
+	case "WTR039":
+		return Young;
+		break;
+	case "HER003":
+		return Adult;
+		break;
+	case "HER023":
+		return Adult;
+		break;
+	case "WIN003":
+		return Adult;
+		break;
+	case "WTR076":
+		return Adult;
+		break;
+	case "KSU001":
+		return Adult;
+		break;
+	case "1HP086":
+		return Adult;
+		break;
+	case "WTR076":
+		return Adult;
+		break;
+	case "CRU045":
+		return Adult;
+		break;
+	case "CRU045":
+		return Adult;
+		break;
+	case "OUT045":
+		return Adult;
+		break;
+	case "HER007":
+		return Young;
+		break;
+	case "HER056":
+		return Young;
+		break;
+	case "XXX004":
+		return Young;
+		break;
+	case "KSU002":
+		return Young;
+		break;
+	case "1HP087":
+		return Young;
+		break;
+	case "WTR077":
+		return Young;
+		break;
+	case "WTR077":
+		return Young;
+		break;
+	case "OUT046":
+		return Young;
+		break;
+	case "KAT001":
+		return Young;
+		break;
+	case "HER004":
+		return Adult;
+		break;
+	case "HER024":
+		return Adult;
+		break;
+	case "WIN004":
+		return Adult;
+		break;
+	case "TEA001":
+		return Adult;
+		break;
+	case "1HP138":
+		return Adult;
+		break;
+	case "WTR113":
+		return Adult;
+		break;
+	case "WTR113":
+		return Adult;
+		break;
+	case "CRU076":
+		return Adult;
+		break;
+	case "CRU076":
+		return Adult;
+		break;
+	case "HER008":
+		return Young;
+		break;
+	case "HER057":
+		return Young;
+		break;
+	case "XXX001":
+		return Young;
+		break;
+	case "TEA002":
+		return Young;
+		break;
+	case "1HP139":
+		return Young;
+		break;
+	case "WTR114":
+		return Young;
+		break;
+	case "WTR114":
+		return Young;
+		break;
+	case "HER009":
+		return Adult;
+		break;
+	case "HER017":
+		return Adult;
+		break;
+	case "WIN005":
+		return Adult;
+		break;
+	case "1HP180":
+		return Adult;
+		break;
+	case "ARC001":
+		return Adult;
+		break;
+	case "ARC001":
+		return Adult;
+		break;
+	case "CRU098":
+		return Adult;
+		break;
+	case "CRU098":
+		return Adult;
+		break;
+	case "HER013":
+		return Young;
+		break;
+	case "HER070":
+		return Young;
+		break;
+	case "1HP181":
+		return Young;
+		break;
+	case "ARC002":
+		return Young;
+		break;
+	case "ARC002":
+		return Young;
+		break;
+	case "HER010":
+		return Adult;
+		break;
+	case "HER018":
+		return Adult;
+		break;
+	case "WIN006":
+		return Adult;
+		break;
+	case "1HP221":
+		return Adult;
+		break;
+	case "ARC038":
+		return Adult;
+		break;
+	case "ARC038":
+		return Adult;
+		break;
+	case "CRU119":
+		return Adult;
+		break;
+	case "CRU119":
+		return Adult;
+		break;
+	case "OUT089":
+		return Adult;
+		break;
+	case "HER014":
+		return Young;
+		break;
+	case "HER071":
+		return Young;
+		break;
+	case "1HP222":
+		return Young;
+		break;
+	case "ARC039":
+		return Young;
+		break;
+	case "ARC039":
+		return Young;
+		break;
+	case "OUT090":
+		return Young;
+		break;
+	case "AZL001":
+		return Young;
+		break;
+	case "HER011":
+		return Adult;
+		break;
+	case "HER019":
+		return Adult;
+		break;
+	case "WIN007":
+		return Adult;
+		break;
+	case "1HP258":
+		return Adult;
+		break;
+	case "ARC075":
+		return Adult;
+		break;
+	case "ARC075":
+		return Adult;
+		break;
+	case "CRU138":
+		return Adult;
+		break;
+	case "CRU138":
+		return Adult;
+		break;
+	case "HER015":
+		return Young;
+		break;
+	case "1HP259":
+		return Young;
+		break;
+	case "ARC076":
+		return Young;
+		break;
+	case "ARC076":
+		return Young;
+		break;
+	case "HER012":
+		return Adult;
+		break;
+	case "HER020":
+		return Adult;
+		break;
+	case "WIN008":
+		return Adult;
+		break;
+	case "1HP301":
+		return Adult;
+		break;
+	case "ARC113":
+		return Adult;
+		break;
+	case "ARC113":
+		return Adult;
+		break;
+	case "CRU158":
+		return Adult;
+		break;
+	case "CRU158":
+		return Adult;
+		break;
+	case "HER016":
+		return Young;
+		break;
+	case "HER072":
+		return Young;
+		break;
+	case "1HP302":
+		return Young;
+		break;
+	case "ARC114":
+		return Young;
+		break;
+	case "ARC114":
+		return Young;
+		break;
+	case "HER025":
+		return Young;
+		break;
+	case "1HP003":
+		return Young;
+		break;
+	case "CRU002":
+		return Young;
+		break;
+	case "CRU002":
+		return Young;
+		break;
+	case "HER000":
+		return Young;
+		break;
+	case "IRA001":
+		return Young;
+		break;
+	case "1HP089":
+		return Young;
+		break;
+	case "CRU046":
+		return Young;
+		break;
+	case "CRU046":
+		return Young;
+		break;
+	case "HER026":
+		return Young;
+		break;
+	case "1HP088":
+		return Young;
+		break;
+	case "CRU047":
+		return Young;
+		break;
+	case "CRU047":
+		return Young;
+		break;
+	case "CRU047":
+		return Young;
+		break;
+	case "OUT047":
+		return Young;
+		break;
+	case "BEN001":
+		return Young;
+		break;
+	case "HER029":
+		return Young;
+		break;
+	case "1HP140":
+		return Young;
+		break;
+	case "CRU077":
+		return Young;
+		break;
+	case "CRU077":
+		return Young;
+		break;
+	case "CRU077":
+		return Young;
+		break;
+	case "CRU097":
+		return Young;
+		break;
+	case "CRU097":
+		return Young;
+		break;
+	case "HER027":
+		return Young;
+		break;
+	case "1HP182":
+		return Young;
+		break;
+	case "CRU099":
+		return Young;
+		break;
+	case "CRU099":
+		return Young;
+		break;
+	case "HER028":
+		return Young;
+		break;
+	case "1HP220":
+		return Young;
+		break;
+	case "CRU118":
+		return Young;
+		break;
+	case "CRU118":
+		return Young;
+		break;
+	case "CRU118":
+		return Young;
+		break;
+	case "HER030":
+		return Adult;
+		break;
+	case "HER038":
+		return Adult;
+		break;
+	case "HER069":
+		return Adult;
+		break;
+	case "HER069":
+		return Adult;
+		break;
+	case "WIN009":
+		return Adult;
+		break;
+	case "MON001":
+		return Adult;
+		break;
+	case "MON001":
+		return Adult;
+		break;
+	case "HER034":
+		return Young;
+		break;
+	case "PSM001":
+		return Young;
+		break;
+	case "MON002":
+		return Young;
+		break;
+	case "MON002":
+		return Young;
+		break;
+	case "HER031":
+		return Adult;
+		break;
+	case "HER039":
+		return Adult;
+		break;
+	case "WIN010":
+		return Adult;
+		break;
+	case "MON029":
+		return Adult;
+		break;
+	case "MON029":
+		return Adult;
+		break;
+	case "HER035":
+		return Young;
+		break;
+	case "BOL001":
+		return Young;
+		break;
+	case "MON030":
+		return Young;
+		break;
+	case "MON030":
+		return Young;
+		break;
+	case "HER032":
+		return Adult;
+		break;
+	case "HER040":
+		return Adult;
+		break;
+	case "WIN011":
+		return Adult;
+		break;
+	case "MON119":
+		return Adult;
+		break;
+	case "MON119":
+		return Adult;
+		break;
+	case "HER036":
+		return Young;
+		break;
+	case "LEV001":
+		return Young;
+		break;
+	case "MON120":
+		return Young;
+		break;
+	case "MON120":
+		return Young;
+		break;
+	case "HER033":
+		return Adult;
+		break;
+	case "HER041":
+		return Adult;
+		break;
+	case "WIN012":
+		return Adult;
+		break;
+	case "MON153":
+		return Adult;
+		break;
+	case "MON153":
+		return Adult;
+		break;
+	case "HER037":
+		return Young;
+		break;
+	case "CHN001":
+		return Young;
+		break;
+	case "MON154":
+		return Young;
+		break;
+	case "MON154":
+		return Young;
+		break;
+	case "HER042":
+		return Adult;
+		break;
+	case "HER048":
+		return Adult;
+		break;
+	case "WIN013":
+		return Adult;
+		break;
+	case "ELE001":
+		return Adult;
+		break;
+	case "ELE001":
+		return Adult;
+		break;
+	case "HER045":
+		return Young;
+		break;
+	case "OLD001":
+		return Young;
+		break;
+	case "ELE002":
+		return Young;
+		break;
+	case "ELE002":
+		return Young;
+		break;
+	case "HER043":
+		return Adult;
+		break;
+	case "HER049":
+		return Adult;
+		break;
+	case "WIN014":
+		return Adult;
+		break;
+	case "ELE031":
+		return Adult;
+		break;
+	case "ELE031":
+		return Adult;
+		break;
+	case "HER046":
+		return Young;
+		break;
+	case "LXI001":
+		return Young;
+		break;
+	case "ELE032":
+		return Young;
+		break;
+	case "ELE032":
+		return Young;
+		break;
+	case "HER044":
+		return Adult;
+		break;
+	case "HER050":
+		return Adult;
+		break;
+	case "HER063":
+		return Adult;
+		break;
+	case "WIN015":
+		return Adult;
+		break;
+	case "XXX012":
+		return Adult;
+		break;
+	case "ELE062":
+		return Adult;
+		break;
+	case "ELE062":
+		return Adult;
+		break;
+	case "HER047":
+		return Young;
+		break;
+	case "HER064":
+		return Young;
+		break;
+	case "XXX011":
+		return Young;
+		break;
+	case "BRI001":
+		return Young;
+		break;
+	case "ELE063":
+		return Young;
+		break;
+	case "ELE063":
+		return Young;
+		break;
+	case "WIN016":
+		return Adult;
+		break;
+	case "EVR017":
+		return Adult;
+		break;
+	case "EVR017":
+		return Adult;
+		break;
+	case "HER051":
+		return Young;
+		break;
+	case "EVR019":
+		return Young;
+		break;
+	case "HER053":
+		return Young;
+		break;
+	case "EVR085":
+		return Young;
+		break;
+	case "HER052":
+		return Young;
+		break;
+	case "EVR120":
+		return Young;
+		break;
+	case "UPR103":
+		return Young;
+		break;
+	case "UPR103":
+		return Young;
+		break;
+	case "UPR103":
+		return Young;
+		break;
+	case "DVR001":
+		return Young;
+		break;
+	case "HER059":
+		return Adult;
+		break;
+	case "HER066":
+		return Adult;
+		break;
+	case "WIN018":
+		return Adult;
+		break;
+	case "UPR001":
+		return Adult;
+		break;
+	case "HER062":
+		return Young;
+		break;
+	case "DRO001":
+		return Young;
+		break;
+	case "UPR002":
+		return Young;
+		break;
+	case "HER058":
+		return Adult;
+		break;
+	case "HER065":
+		return Adult;
+		break;
+	case "WIN017":
+		return Adult;
+		break;
+	case "UPR044":
+		return Adult;
+		break;
+	case "HER061":
+		return Young;
+		break;
+	case "FAI001":
+		return Young;
+		break;
+	case "UPR045":
+		return Young;
+		break;
+	case "HER060":
+		return Adult;
+		break;
+	case "HER067":
+		return Adult;
+		break;
+	case "WIN019":
+		return Adult;
+		break;
+	case "UPR102":
+		return Adult;
+		break;
+	case "DYN001":
+		return Young;
+		break;
+	case "DYN001":
+		return Young;
+		break;
+	case "DYN001":
+		return Young;
+		break;
+	case "HER075":
+		return Young;
+		break;
+	case "DYN025":
+		return Young;
+		break;
+	case "HER074":
+		return Adult;
+		break;
+	case "WIN020":
+		return Adult;
+		break;
+	case "DYN113":
+		return Adult;
+		break;
+	case "HER073":
+		return Young;
+		break;
+	case "DYN114":
+		return Young;
+		break;
+	case "HER077":
+		return Adult;
+		break;
+	case "WIN021":
+		return Adult;
+		break;
+	case "OUT001":
+		return Adult;
+		break;
+	case "HER076":
+		return Young;
+		break;
+	case "HER083":
+		return Young;
+		break;
+	case "OUT002":
+		return Young;
+		break;
+	case "UZU001":
+		return Young;
+		break;
+	case "HER079":
+		return Young;
+		break;
+	case "HER082":
+		return Young;
+		break;
+	case "OUT003":
+		return Young;
+		break;
+	case "ARA001":
+		return Young;
+		break;
+	case "HER078":
+		return Adult;
+		break;
+	case "WIN022":
+		return Adult;
+		break;
+	case "OUT091":
+		return Adult;
+		break;
+	case "HER080":
+		return Young;
+		break;
+	case "HER081":
+		return Young;
+		break;
+	case "OUT092":
+		return Young;
+		break;
+	case "RIP001":
+		return Young;
+		break;
+        default:
+            return False;}
+            }
+
