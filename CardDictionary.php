3ae24e0964c6408e0c19d66523c9b2875715b83c
OotTheMonk <nathanandrecoder@gmail.com>
2023-06-27 18:23:00 -0500

    Fix defense reaction being playable to blue back stab

--- CardDictionary.php
+++ CardDictionary.php
@@ -804,7 +804,7 @@ function IsPlayRestricted($cardID, &$restriction, $from = "", $index = -1, $play
 function IsDefenseReactionPlayable($cardID, $from)
 {
   global $combatChain, $mainPlayer;
-  if(($combatChain[0] == "ARC159" || $combatChain[0] == "OUT015" || $combatChain[0] == "OUT016" || $combatChain[0] == "OUT016" || $combatChain[0] == "OUT133"|| $combatChain[0] == "OUT134" || $combatChain[0] == "OUT135" || $combatChain[0] == "OUT198" || $combatChain[0] == "OUT199" || $combatChain[0] == "OUT200") && CardType($cardID) == "DR") return false;
+  if(($combatChain[0] == "ARC159" || $combatChain[0] == "OUT015" || $combatChain[0] == "OUT016" || $combatChain[0] == "OUT017" || $combatChain[0] == "OUT133"|| $combatChain[0] == "OUT134" || $combatChain[0] == "OUT135" || $combatChain[0] == "OUT198" || $combatChain[0] == "OUT199" || $combatChain[0] == "OUT200") && CardType($cardID) == "DR") return false;
   if($combatChain[0] == "MON245") if (!ExudeConfidenceReactionsPlayable()) return false;
   if($from == "HAND" && CardSubType($combatChain[0]) == "Arrow" && SearchCharacterForCard($mainPlayer, "EVR087")) return false;
   if(CurrentEffectPreventsDefenseReaction($from)) return false;
