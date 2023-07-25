58202fa00fb16ebc47ccc7cb7e2979e1e08f8bcd
Luke Putz <ltputz@gmail.com>
2023-07-25 13:24:53 -0700

    Fixes to Imposing Visage so that it correctly selects a card from deck based on the resources paid as well as adds fix to add dynamic cost to the base cost when a base cost exists

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1233,7 +1233,7 @@ function PlayCard($cardID, $from, $dynCostResolved = -1, $index = -1, $uniqueID 
           $baseCost = 0;
           AddAdditionalCost($currentPlayer, "ALTERNATIVECOST");
         }
-        $resources[1] += ($dynCostResolved > 0 ? $dynCostResolved : $baseCost) + CurrentEffectCostModifiers($cardID, $from) + $frostbitesPaid + CharacterCostModifier($cardID, $from) + BanishCostModifier($from, $index);
+        $resources[1] += ($dynCostResolved > 0 ? $dynCostResolved + $baseCost : $baseCost) + CurrentEffectCostModifiers($cardID, $from) + $frostbitesPaid + CharacterCostModifier($cardID, $from) + BanishCostModifier($from, $index);
         if($isAlternativeCostPaid && $resources[1] > 0) WriteLog("<span style='color:red;'>Alternative costs do not offset additional costs.</span>");
       }
       if($resources[1] < 0) $resources[1] = 0;
