0a763dd66bb9bf7bdbf02acb4add7e9b174f3214
OotTheMonk <nathanandrecoder@gmail.com>
2023-07-27 18:35:58 -0500

    Fix star struck off by 1 error

--- CurrentEffectAbilities.php
+++ CurrentEffectAbilities.php
@@ -1095,7 +1095,7 @@ function EffectAttackRestricted()
       $effectArr = explode(",", $currentTurnEffects[$i]);
       $effectID = $effectArr[0];
       switch($effectID) {
-        case "DTD203": if($combatChainState[$CCS_LinkBaseAttack] < $effectArr[1]) $restrictedBy = "DTD203"; break;
+        case "DTD203": if($combatChainState[$CCS_LinkBaseAttack] <= $effectArr[1]) $restrictedBy = "DTD203"; break;
         case "DTD230-Peace": $restrictedBy = "DTD230"; break;
         default:
           break;
