63bc665417bb0db97317e3b10c8fa6025a08ae14
KuKetto <domonkos115@gmail.com>
2023-09-27 19:46:12 +0200

    Added blade brake to teklo base equipments

--- CardDictionary.php
+++ CardDictionary.php
@@ -889,6 +889,7 @@ function HasBladeBreak($cardID)
     case "DTD200": return true;
     case "DTD222": case "DTD223": case "DTD224": case "DTD225": return true;
     case $CID_TekloHead: case $CID_TekloChest: case $CID_TekloArms: case $CID_TekloLegs: return true;
+    case "EVO018": case "EVO019": case "EVO020": case "EVO021": return true;
     case "EVO434": case "EVO435": case "EVO436": case "EVO437": return true;
     case "EVO418": case "EVO419": case "EVO420": case "EVO421": return true;
     case "EVO446": case "EVO447": case "EVO448": case "EVO449": return true;
