6e2c4385f048d752e338d7c82377d8dd873dcead
OotTheMonk <nathanandrecoder@gmail.com>
2023-09-30 12:45:15 -0500

    Make load bug report pull all the gamestate backup files

--- LoadBugReport.php
+++ LoadBugReport.php
@@ -22,35 +22,44 @@ if (!$source || !$target) {
   exit();
 }
 
-$source_path = "https://legacy.talishar.net/game/BugReports/{$source}-0/gamestate.txt";
-$target_path = "./Games/{$target}/gamestate.txt";
-$target_realpath = "./$target_path";
-
-$target_file = fopen("{$target_realpath}", 'r+');
-if (!$target_file) {
-  http_response_code(500);
-  echo json_encode(array('error' => 'Failed to open target file on local'));
-  exit();
-}
-
-//Try using cURL instead of ssh
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $source_path); // Set the URL
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Return the response as a string
-$source_file_contents = curl_exec($ch);
-if (curl_errno($ch)) {
-    echo 'cURL error: ' . curl_error($ch);
-}
-curl_close($ch);
-
-$target_file_contents = stream_get_contents($target_file);
-
-$source_file_contents = preg_replace("/[a-zA-Z0-9]{64}[\s\S]*$/", '', $source_file_contents);
-
-$target_file_contents = preg_replace("/^[\s\S]*?([a-zA-Z0-9]{64})/", $source_file_contents . '$1', $target_file_contents, 1);
-fclose($target_file);
-
-file_put_contents("{$target_realpath}", $target_file_contents);
-
+SaveFile($source, $target, "gamestate.txt");
+SaveFile($source, $target, "gamestateBackup.txt");
+SaveFile($source, $target, "beginTurnGamestate.txt");
+SaveFile($source, $target, "lastTurnGamestate.txt");
 
 echo json_encode(array('success' => true));
+
+function SaveFile($source, $target, $file) {
+  $source_path = "https://legacy.talishar.net/game/BugReports/{$source}-0/{$file}";
+  $target_path = "./Games/{$target}/{$file}";
+  $target_realpath = "./$target_path";
+  $gs_path = "./Games/{$target}/gamestate.txt";
+
+  $gs_file = fopen("{$gs_path}", 'r+');
+  if (!$gs_file) {
+    http_response_code(500);
+    echo json_encode(array('error' => 'Failed to open target file on local'));
+    exit();
+  }
+
+  //Try using cURL instead of ssh
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $source_path); // Set the URL
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // Return the response as a string
+  $source_file_contents = curl_exec($ch);
+  if (curl_errno($ch)) {
+      echo 'cURL error: ' . curl_error($ch);
+  }
+  curl_close($ch);
+
+  $gs_file_contents = stream_get_contents($gs_file);
+
+  $source_file_contents = preg_replace("/[a-zA-Z0-9]{64}[\s\S]*$/", '', $source_file_contents);
+
+  $target_file_contents = preg_replace("/^[\s\S]*?([a-zA-Z0-9]{64})/", $source_file_contents . '$1', $gs_file_contents, 1);
+  fclose($gs_file);
+
+  file_put_contents("{$target_realpath}", $target_file_contents);
+}
+
+?>
