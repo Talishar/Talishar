e5c1c461acaecacf789f2d33fc50be2b3aeb7d5c
PvtVoid <jotrickr@gmail.com>
2023-10-02 11:57:41 +0200

    Add Moonshot restriction

--- CardDictionary.php
+++ CardDictionary.php
@@ -876,6 +876,7 @@ function IsPlayRestricted($cardID, &$restriction, $from = "", $index = -1, $play
     case "EVO003": return $character[$index+2] <= 0;
     case "EVO014": case "EVO015": case "EVO016": case "EVO017": return $character[$index+2] == 0 || GetClassState($player, $CS_NumBoosted) == 0;
     case "EVO071": case "EVO072": if ($from == "PLAY") return $myItems[$index+2] != 2; else return false;
+    case "EVO140": return GetClassState($player, $CS_NumBoosted) < 0;
     case "EVO235": return !$CombatChain->HasCurrentLink() || !ClassContains($CombatChain->AttackCard()->ID(), "ASSASSIN", $mainPlayer) || CardType($CombatChain->AttackCard()->ID()) != "AA";
     case "EVO434": case "EVO435": case "EVO436": case "EVO437": return !EvoHasUnderCard($currentPlayer, $index);
     default: return false;
