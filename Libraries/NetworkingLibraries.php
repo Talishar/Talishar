da9ba0436997e6f697baa49cb3a5a69a117e12f0
OotTheMonk <nathanandrecoder@gmail.com>
2023-10-28 10:31:28 -0500

    Tekloboss blocking fix giving attacker go again

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -2033,7 +2033,7 @@ function PayAdditionalCosts($cardID, $from)
 
 function PlayCardEffect($cardID, $from, $resourcesPaid, $target = "-", $additionalCosts = "-", $uniqueID = "-1", $layerIndex = -1)
 {
-  global $turn, $combatChain, $currentPlayer, $defPlayer, $combatChainState, $CCS_AttackPlayedFrom, $CS_PlayIndex;
+  global $turn, $combatChain, $currentPlayer, $mainPlayer, $defPlayer, $combatChainState, $CCS_AttackPlayedFrom, $CS_PlayIndex;
   global $CS_CharacterIndex, $CS_NumNonAttackCards, $CS_PlayCCIndex, $CS_NumAttacks, $CCS_LinkBaseAttack;
   global $CCS_WeaponIndex, $EffectContext, $CCS_AttackFused, $CCS_AttackUniqueID, $CS_NumLess3PowAAPlayed, $layers;
   global $CS_NumDragonAttacks, $CS_NumAttackCards, $CS_NumIllusionistAttacks, $CS_NumIllusionistActionCardAttacks, $CCS_IsBoosted;
@@ -2042,7 +2042,7 @@ function PlayCardEffect($cardID, $from, $resourcesPaid, $target = "-", $addition
   if($layerIndex > -1) SetClassState($currentPlayer, $CS_PlayIndex, $layerIndex);
   $index = SearchForUniqueID($uniqueID, $currentPlayer);
   if($cardID == "ARC003" || $cardID == "CRU101") $index = FindCharacterIndex($currentPlayer, $cardID); //TODO: Fix this. This is an issue with the entire "multiple abilities" framework
-  if(CardClass($cardID) == "MECHANOLOGIST" && CardType($cardID) == "AA") {
+  if($currentPlayer == $mainPlayer && CardClass($cardID) == "MECHANOLOGIST" && CardType($cardID) == "AA") {
     $index = FindCharacterIndex($currentPlayer, "EVO410");
     if ($index != -1) {
       GiveAttackGoAgain();
