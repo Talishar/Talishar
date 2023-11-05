3a01c213402cc2395b4f4979de40970773dbf101
OotTheMonk <nathanandrecoder@gmail.com>
2023-11-05 09:15:54 -0600

    Don't show useless administrative adaptive plating effect

--- GetNextTurn3.php
+++ GetNextTurn3.php
@@ -740,6 +740,7 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
     $cardID = explode("-", $currentTurnEffects[$i])[0];
     $cardID = explode(",", $cardID)[0];
     $cardID = explode("_", $cardID)[0];
+    if($cardID == "EVO013") continue;//Don't show useless administrative effect
     if ($playerID == $currentTurnEffects[$i + 1] || $playerID == 3 && $otherPlayer != $currentTurnEffects[$i + 1]) array_push($playerEffects, JSONRenderedCard($cardID));
     else array_push($opponentEffects, JSONRenderedCard($cardID));
   }
