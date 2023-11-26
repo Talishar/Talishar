9f0a01894914c978a7d2a1a3068acea4598dc85b
OotTheMonk <nathanandrecoder@gmail.com>
2023-11-25 18:47:36 -0600

    Change timeout to 45s (from 30s)

--- GetNextTurn2.php
+++ GetNextTurn2.php
@@ -83,7 +83,7 @@ while ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
     }
     //Handle server timeout
     $lastUpdateTime = $cacheArr[5];
-    if ($currentTime - $lastUpdateTime > 30000 && $cacheArr[11] != "1")//30 seconds
+    if ($currentTime - $lastUpdateTime > 45000 && $cacheArr[11] != "1")//45 seconds
     {
       SetCachePiece($gameName, 12, "1");
       $opponentInactive = true;
