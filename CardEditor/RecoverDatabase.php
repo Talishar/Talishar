eeb4aeeb54b245d6a3a77cf1828ab0d6ab442999
OotTheMonk <nathanandrecoder@gmail.com>
2023-12-09 10:42:22 -0600

    Add database recovery tool

---
+++ CardEditor/RecoverDatabase.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once '../includes/dbh.inc.php';
+include_once "CardEditorDatabase.php";
+include_once '../GeneratedCode/DatabaseGeneratedCardDictionaries.php';
+
+$sets = ["WTR", "ARC", "CRU", "MON", "ELE", "EVR", "UPR", "DYN", "OUT", "DVR", "RVD", "DTD", "LGS", "HER", "FAB", "TCC", "EVO", "HVY"];
+
+foreach($sets as &$set) {
+    for($i=0; $i<800; ++$i) {
+      $cardID = $set;
+      if($i<100) $cardID .= "0";
+      if($i<10) $cardID .= "0";
+      $cardID .= $i;
+      if(GeneratedGoAgain($cardID)) CreateEditCard($cardID, 1);
+    }
+}
+
+?>
