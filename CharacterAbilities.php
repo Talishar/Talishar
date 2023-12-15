0546c4ae061d53c97aa7f9d994455545ca62e973
Zachary Cauchi <Thatzacharycauchi@gmail.com>
2023-12-15 13:08:06 +1000

    fix bug where kassai only has her cost reduction apply if she has drawn exactly 1 card

--- CharacterAbilities.php
+++ CharacterAbilities.php
@@ -502,7 +502,7 @@ function CharacterCostModifier($cardID, $from)
       case "TCC001": if(SubtypeContains($cardID, "Evo")) --$modifier; break;
       case "TCC408": if($cardID == "TCC002") --$modifier; break;
       case "EVO001": case "EVO002": if($from == "DECK" && SubtypeContains($cardID, "Item", $currentPlayer) && CardCost($cardID) < 2) ++$modifier; break;
-      case "HVY090": case "HVY091": if(CardSubtype($cardID) == "Sword" && GetClassState($currentPlayer, $CS_NumCardsDrawn) == 1) --$modifier; break;
+      case "HVY090": case "HVY091": if(CardSubtype($cardID) == "Sword" && GetClassState($currentPlayer, $CS_NumCardsDrawn) >= 1) --$modifier; break;
       default: break;
     }
   }
