fb70466fd441045751e85346894d86cbf87f4809
PvtVoid <jotrickr@gmail.com>
2024-01-17 07:07:18 +0100

    Rising Power

--- CombatChain.php
+++ CombatChain.php
@@ -167,6 +167,7 @@ function AttackModifier($cardID, $from = "", $resourcesPaid = 0, $repriseActive 
     case "HVY112": return 3;
     case "HVY113": return 2;
     case "HVY114": return 1;
+    case "HVY146": case "HVY147": case "HVY148": return GetClassState($mainPlayer, $CS_NumCardsDrawn) >= 1 ? 1 : 0;
     default: return 0;
   }
 }
