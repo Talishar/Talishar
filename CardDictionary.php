a08bd422470b3050e1fae3e4673c4be03bf2e877
PvtVoid <jotrickr@gmail.com>
2024-01-19 19:16:18 +0100

    Fix Prized Galea restriction

--- CardDictionary.php
+++ CardDictionary.php
@@ -926,6 +926,7 @@ function IsPlayRestricted($cardID, &$restriction, $from = "", $index = -1, $play
     case "EVO434": case "EVO435": case "EVO436": case "EVO437": return !EvoHasUnderCard($currentPlayer, $index);
     case "HVY055": return CountItem("DYN243", $currentPlayer) <= 0;
     case "HVY090": case "HVY091": return SearchCount(SearchDiscard($currentPlayer, pitch:1)) < 2 || SearchCount(SearchDiscard($currentPlayer, pitch:2)) < 2;
+    case "HVY098": return !TypeContains($CombatChain->AttackCard()->ID(), "W", $currentPlayer);
     case "HVY099": return CardSubtype($cardID) == "Sword";
     case "HVY101": return !$CombatChain->HasCurrentLink() || CardType($CombatChain->AttackCard()->ID()) != "W";
     case "HVY102": return !$CombatChain->HasCurrentLink() || !ClassContains($CombatChain->AttackCard()->ID(), "WARRIOR", $mainPlayer) || CachedTotalAttack() <= AttackValue($CombatChain->AttackCard()->ID());
