0b8ee718b53abd20387e5ddd7d3d72a07a3b1016
PvtVoid <jotrickr@gmail.com>
2024-01-19 06:47:15 +0100

    Fix Down But Not Out

--- Search.php
+++ Search.php
@@ -1152,7 +1152,7 @@ function GetPlayerNumEquipment($player)
   $characters = &GetPlayerCharacter($player);
   $count = 0;
   for($i = 0; $i < count($characters); $i += CharacterPieces()) {
-    if(TypeContains($characters[$i], "E", $player)) ++$count;
+    if(TypeContains($characters[$i], "E", $player) && $characters[$i+1] != 0) ++$count;
   }
   return $count;
 }
