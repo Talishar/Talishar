a0512e071f20c05a0527d1b946cddf8ceeca4dec
PvtVoid <jotrickr@gmail.com>
2024-01-19 10:43:40 +0100

    Fix fully blocking a Swing Big with Swing Big giving 2x Quicken

--- CombatChain.php
+++ CombatChain.php
@@ -639,7 +639,7 @@ function CombatChainCloseAbilities($player, $cardID, $chainLink)
   global $chainLinkSummary, $mainPlayer, $defPlayer, $chainLinks;
   switch($cardID) {
     case "EVR002":
-      if($chainLinkSummary[$chainLink*ChainLinkSummaryPieces()] == 0 && $chainLinks[$chainLink][0] == $cardID) {
+      if($chainLinkSummary[$chainLink*ChainLinkSummaryPieces()] == 0 && $chainLinks[$chainLink][0] == $cardID && $chainLinks[$chainLink][1] == $player) {
         PlayAura("WTR225", $defPlayer);
       }
       break;
