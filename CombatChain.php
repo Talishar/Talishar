6b837579413a25c0e80e61cd2b1c63ae12404833
PvtVoid <jotrickr@gmail.com>
2024-01-28 16:17:48 +0100

    Fix Graven Call missing Piercing 1

--- CombatChain.php
+++ CombatChain.php
@@ -169,6 +169,7 @@ function AttackModifier($cardID, $from = "", $resourcesPaid = 0, $repriseActive 
     case "HVY113": return 2;
     case "HVY114": return 1;
     case "HVY146": case "HVY147": case "HVY148": return GetClassState($mainPlayer, $CS_NumCardsDrawn) >= 1 ? 1 : 0;
+    case "HVY245": return NumEquipBlock() > 0 ? 1 : 0;
     default: return 0;
   }
 }
