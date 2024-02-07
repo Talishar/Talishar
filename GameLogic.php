ed75f6631fae5afb55c2e3fcbdbab7d431b43171
PvtVoid <jotrickr@gmail.com>
2024-02-07 15:53:22 +0100

    Fix Cash In to not ask for Gold if players paid in Copper

--- GameLogic.php
+++ GameLogic.php
@@ -571,6 +571,8 @@ function DecisionQueueStaticEffect($phase, $player, $parameter, $lastResult)
       $params = explode("!", $parameter);
       AddCurrentTurnEffect($params[0], $player, (count($params) > 1 ? $params[1] : ""));
       return "1";
+    case "SEARCHCURRENTEFFECTPASS":
+      return SearchCurrentTurnEffects($parameter, $player) ? "PASS" : "1";
     case "REMOVECURRENTTURNEFFECT":
       SearchCurrentTurnEffects($parameter, $player, true);
       return $lastResult;
