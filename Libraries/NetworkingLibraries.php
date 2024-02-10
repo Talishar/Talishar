d97d73252dceee863c5304337ed398d1a6e5ad5b
PvtVoid <jotrickr@gmail.com>
2024-02-10 13:00:08 +0100

    More AttackStep info

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -2315,7 +2315,7 @@ function PlayCardEffect($cardID, $from, $resourcesPaid, $target = "-", $addition
     CopyCurrentTurnEffectsFromAfterResolveEffects();
     CacheCombatResult();
     if(!$isBlock) ProcessAllMirage();
-    if($definedCardType == "AA") AddLayer("ATTACKSTEP", $mainPlayer, "-");
+    if($definedCardType == "AA") AddLayer("ATTACKSTEP", $mainPlayer, "-"); //I haven't added this for weapon. I don't think it's needed yet.
   }
   if($CS_CharacterIndex != -1 && CanPlayAsInstant($cardID)) RemoveCharacterEffects($currentPlayer, GetClassState($currentPlayer, $CS_CharacterIndex), "INSTANT");
   //Now determine what needs to happen next
