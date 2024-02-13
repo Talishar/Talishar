3f29bfe0d7b36726e429b2ddf61ca9ec638f95c8
PvtVoid <jotrickr@gmail.com>
2024-02-13 21:16:20 +0100

    Fix Raise an Army + Ripple Away interaction preventing all Cintari Sellswords creation

--- CardDictionaries/PlayAbilities.php
+++ CardDictionaries/PlayAbilities.php
@@ -131,9 +131,7 @@
         AddCurrentTurnEffect($cardID . "-BUFF", $currentPlayer);
         return "";
       case "HVY105":
-        for($i=0; $i<intval($additionalCosts); ++$i) {
-          PlayAlly("HVY134", $currentPlayer);
-        }
+        PlayAlly("HVY134", $currentPlayer, number:intval($additionalCosts));
         return "";
       case "HVY106": case "HVY107": case "HVY108":
         AddCurrentTurnEffect($cardID, $currentPlayer);
