c8e6dbd36cdc99fd107ad513184634307c35578a
PvtVoid <jotrickr@gmail.com>
2024-02-19 09:34:45 +0100

    Stop Oasis from preventing multiple Runechants/Ashwings until we code it with Unique ID

--- CurrentEffectAbilities.php
+++ CurrentEffectAbilities.php
@@ -672,6 +672,7 @@ function CurrentEffectDamagePrevention($player, $type, $damage, $source, $preven
               $currentTurnEffects[$i+3] -= $sourceDamage;
             }
             if($currentTurnEffects[$i+3] <= 0) $remove = true;
+            if($source == "ARC112" || $source == "UPR042") $remove = true; //To be removed when coded with Unique ID instead of cardID name as $source
           }
           break;
         case "OUT175": case "OUT176": case "OUT177": case "OUT178":
