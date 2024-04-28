72e8bed2e1ed205a73041b3a557efdebec4f8b6f
OotTheMonk <nathanandrecoder@gmail.com>
2024-04-28 18:49:27 -0500

    Add game stats api

---
+++ zzGameStatsAPI.php
@@ -0,0 +1,25 @@
+<?php
+
+  include "Libraries/HTTPLibraries.php";
+
+  $playerID = $_GET["playerID"];
+  $gameName = $_GET["gameName"];
+  if (!IsGameNameValid($gameName)) {
+    echo ("Invalid game name.");
+    exit;
+  }
+
+  include "./Libraries/SHMOPLibraries.php";
+  include "./ParseGamestate.php";
+  include_once "./includes/functions.inc.php";
+  include "./GameLogic.php";
+  include "./Libraries/UILibraries2.php";
+  include "./Libraries/StatFunctions.php";
+  include "./Libraries/PlayerSettings.php";
+  include_once 'Assets/patreon-php-master/src/PatreonDictionary.php';
+  include "./GameTerms.php";
+  include "./HostFiles/Redirector.php";
+
+  echo(SerializeGameResult($playerID, "", file_get_contents("./Games/" . $gameName . "/p" . $playerID . "Deck.txt"), $gameName, includeFullLog:true));
+
+?>
