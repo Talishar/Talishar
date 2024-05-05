156bc4fd66fdc1dbe28c19c99facca6fe640e78d
PvtVoid <jotrickr@gmail.com>
2024-05-05 10:02:36 +0200

    More changes for Aqua equipments

--- CoreLogic.php
+++ CoreLogic.php
@@ -168,7 +168,6 @@ function StartTurnAbilities()
   global $mainPlayer, $defPlayer;
   $mainCharacter = &GetPlayerCharacter($mainPlayer);
   for($i=count($mainCharacter) - CharacterPieces(); $i>=0; $i -= CharacterPieces()) CharacterStartTurnAbility($i);
-  CharacterStartTurnAbilities();
   ArsenalStartTurnAbilities();
   DefCharacterStartTurnAbilities();
   ArsenalStartTurnAbilities();
