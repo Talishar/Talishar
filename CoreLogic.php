3b24741057c3e6d946b54214303a13bda69588a2
PvtVoid <jotrickr@gmail.com>
2024-05-07 21:23:15 +0200

    Fix php error

--- CoreLogic.php
+++ CoreLogic.php
@@ -1272,7 +1272,7 @@ function DoesAttackHaveGoAgain()
     $auras = &GetAuras($mainPlayer);
     if(SearchCharacterForCard($mainPlayer, "MON003") && SearchPitchForColor($mainPlayer, 2) > 0) return true;
     if($isAura && SearchCharacterForCard($mainPlayer, "MON088")) return true;
-    if($isAura && SearchCharacterForCard($mainPlayer, "MST130") && $auras[$combatChainState[$CCS_WeaponIndex]+3] > 0) return true;
+    if($isAura && SearchCharacterForCard($mainPlayer, "MST130") && isset($auras[$combatChainState[$CCS_WeaponIndex]+3]) > 0) return true;
   }
   if($combatChainState[$CCS_CurrentAttackGainedGoAgain] == 1 || CurrentEffectGrantsGoAgain() || MainCharacterGrantsGoAgain()) {
     $combatChainState[$CCS_CurrentAttackGainedGoAgain] = 1;
