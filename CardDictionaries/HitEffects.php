dfb69fc515c2a62f9bd40104d06de02015aa2632
PvtVoid <jotrickr@gmail.com>
2024-05-14 14:32:03 +0200

    Fix Mist Bloom effect + hurl

--- CardDictionaries/HitEffects.php
+++ CardDictionaries/HitEffects.php
@@ -7,7 +7,8 @@ function MSTHitEffect($cardID, $from)
   $discard = new Discard($defPlayer);
   switch ($cardID){
     case "MST003":
-      if($from != "OUT139") AddCurrentTurnEffect($cardID, $mainPlayer);
+      WriteLog($from);
+      if($from != "OUT139" && $from != "OUT148" && $from != "OUT149" && $from != "OUT150") AddCurrentTurnEffect($cardID, $mainPlayer);
       else AddCurrentTurnEffectNextAttack($cardID, $mainPlayer);
       break;
     case "MST103":
