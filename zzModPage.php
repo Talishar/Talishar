d9f07e17dae274cd2f8133e9f0e8e547df1e0159
PvtVoid <jotrickr@gmail.com>
2024-05-17 15:37:48 +0200

    Add SalisburyBavo to mods

--- zzModPage.php
+++ zzModPage.php
@@ -10,15 +10,36 @@ if (!isset($_SESSION["useruid"])) {
   exit;
 }
 $useruid = $_SESSION["useruid"];
-if ($useruid != "OotTheMonk" && $useruid != "Launch" && $useruid != "LaustinSpayce" && $useruid != "Star_Seraph" && $useruid != "Tower" && $useruid != "PvtVoid") {
+if ($useruid != "OotTheMonk" && $useruid != "Launch" && $useruid != "LaustinSpayce" && $useruid != "Star_Seraph" && $useruid != "bavverst" && $useruid != "Tower" && $useruid != "PvtVoid") {
   echo ("You must log in to use this page.");
   exit;
 }
 
-echo ("<h1>Banned players:</h1>");
-$banfileHandler = fopen("./HostFiles/bannedPlayers.txt", "r");
 echo ("<div style='position:absolute; padding:10px; z-index:1; top:15%; left:35%; width:500px; height:650px;
   background-color:rgba(74, 74, 74, 0.9); border: 2px solid #1a1a1a; border-radius: 5px; overflow-y: scroll;'>");
+
+?>
+  <br>
+  <form action='./BanPlayer.php'>
+    <label for="ipToBan" style='font-weight:bolder; margin-left:10px;'>Game to IP ban from:</label>
+    <input type="text" id="ipToBan" name="ipToBan" value="">
+    <label for="playerNumberToBan" style='font-weight:bolder; margin-left:10px;'>Player to ban? (1 or 2):</label>
+    <input type="text" id="playerNumberToBan" name="playerNumberToBan" value="">
+    <input type="submit" value="Ban">
+  </form>
+  
+  <br>
+  
+  <form action='./CloseGame.php'>
+    <label for="gameToClose" style='font-weight:bolder; margin-left:10px;'>Game to close:</label>
+    <input type="text" id="gameToClose" name="gameToClose" value="">
+    <input type="submit" value="Close Game">
+  </form>
+
+<?php
+
+echo ("<h1>Banned players:</h1>");
+$banfileHandler = fopen("./HostFiles/bannedPlayers.txt", "r");
 while (!feof($banfileHandler)) {
   $bannedPlayer = fgets($banfileHandler);
   echo ($bannedPlayer . "<BR>");
@@ -63,22 +84,5 @@ while (!feof($banfileHandler)) {
 }
 fclose($banfileHandler);
 ?>
-
-<br>
-<form action='./BanPlayer.php'>
-  <label for="ipToBan" style='font-weight:bolder; margin-left:10px;'>Game to IP ban from:</label>
-  <input type="text" id="ipToBan" name="ipToBan" value="">
-  <label for="playerNumberToBan" style='font-weight:bolder; margin-left:10px;'>Player to ban? (1 or 2):</label>
-  <input type="text" id="playerNumberToBan" name="playerNumberToBan" value="">
-  <input type="submit" value="Ban">
-</form>
-
-<br>
-
-<form action='./CloseGame.php'>
-  <label for="gameToClose" style='font-weight:bolder; margin-left:10px;'>Game to close:</label>
-  <input type="text" id="gameToClose" name="gameToClose" value="">
-  <input type="submit" value="Close Game">
-</form>
 
 </div>
