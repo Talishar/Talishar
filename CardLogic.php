7604f5602c8dcdceba6e063f5812303488839d93
PvtVoid <jotrickr@gmail.com>
2024-05-17 22:37:12 +0200

    Fix dice not always being cleared

--- CardLogic.php
+++ CardLogic.php
@@ -398,7 +398,8 @@ function ContinueDecisionQueue($lastResult = "")
           ProcessDecisionQueue();
         } else {
           SetClassState($player, $CS_AbilityIndex, $params[2]); //This is like a parameter to PlayCardEffect and other functions
-          PlayCardEffect($cardID, $params[0], $params[1], $target, $additionalCosts, isset($params[3]) ? $params[3] : "-1", isset($params[2]) ? $params[2] : -1);          if(RequiresDieRoll($cardID, $params[0], $player)) ClearDieRoll($player);
+          PlayCardEffect($cardID, $params[0], $params[1], $target, $additionalCosts, isset($params[3]) ? $params[3] : "-1", isset($params[2]) ? $params[2] : -1);  
+          ClearDieRoll($player);
         }
       }
     } else if(count($decisionQueue) > 0 && $decisionQueue[0] == "RESUMEPLAY") {
