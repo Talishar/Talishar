7a05471529783739d0f60d5873878c2892056ead
PvtVoid <jotrickr@gmail.com>
2024-05-31 08:07:45 +0200

    Another AdditionalCosts fix

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -2503,7 +2503,7 @@ function PlayCardEffect($cardID, $from, $resourcesPaid, $target = "-", $addition
   global $CS_NumDragonAttacks, $CS_NumAttackCards, $CS_NumIllusionistAttacks, $CS_NumIllusionistActionCardAttacks, $CCS_IsBoosted;
   global $SET_PassDRStep, $CS_NumBlueDefended, $CS_AdditionalCosts;
   $otherPlayer = $currentPlayer == 1 ? 2 : 1;
-  $additionalCosts = GetClassState($currentPlayer, $CS_AdditionalCosts);
+  if($additionalCosts == "-" || $additionalCosts == "") $additionalCosts = GetClassState($currentPlayer, $CS_AdditionalCosts);
   if($layerIndex > -1) SetClassState($currentPlayer, $CS_PlayIndex, $layerIndex);
   $index = SearchForUniqueID($uniqueID, $currentPlayer);
   if($cardID == "ARC003" || $cardID == "CRU101") $index = FindCharacterIndex($currentPlayer, $cardID); //TODO: Fix this. This is an issue with the entire "multiple abilities" framework
