f1485347a1e8283cd5f8ae8b9493085b903c5f93
PvtVoid <jotrickr@gmail.com>
2024-06-08 06:47:57 +0200

    Fix Koi shuffling the deck more than needed

--- CharacterAbilities.php
+++ CharacterAbilities.php
@@ -279,7 +279,7 @@ function CharacterStartTurnAbility($index)
         AddDecisionQueue("TURNCHARACTERFACEUP", $mainPlayer, "-", 1);
         AddDecisionQueue("DESTROYCHARACTER", $mainPlayer, "-", 1);
         MZMoveCard($mainPlayer, "MYDECK:isSameName=MST499", "MYHAND", may:true, isReveal:true, isSubsequent:true);
-        AddDecisionQueue("SHUFFLEDECK", $mainPlayer, "-");
+        if($character[$index+12] == "UP") AddDecisionQueue("SHUFFLEDECK", $mainPlayer, "-");
       }
       break;
     default: break;
