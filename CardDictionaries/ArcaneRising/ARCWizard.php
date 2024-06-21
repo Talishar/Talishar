3e384a4be953699a029354043746b533ebc22dbe
Trent Lennon <trent.lennon@gmail.com>
2024-06-21 16:11:57 -0400

    Removed unused debugging.

--- CardDictionaries/ArcaneRising/ARCWizard.php
+++ CardDictionaries/ArcaneRising/ARCWizard.php
@@ -276,14 +276,6 @@
     return implode(",", $targets);
   }
 
-  function debug_to_console($data) {
-    $output = $data;
-    if (is_array($output))
-        $output = implode(',', $output);
-
-    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-  }
-
   function CurrentEffectArcaneModifier($source, $player)
   {
     global $currentTurnEffects;
