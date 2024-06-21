5e0eea765d558720d2f82159b5b3a880ecdefe81
Trent Lennon <trent.lennon@gmail.com>
2024-06-21 15:13:35 -0400

    Some changes with help of Jotrick. Now it's not pitching correctly.

--- CardDictionaries/Rosetta/ROSTalent.php
+++ CardDictionaries/Rosetta/ROSTalent.php
@@ -1,33 +1,14 @@
 <?php
-
-
-  function ROSRunebladePlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalCosts = "")
+  function ROSPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalCosts = "")
   {
     global $currentPlayer;
     switch($cardID)
     {
       case "ROS033":
-        PlayAura("ARC112", $currentPlayer);
-        return true;
+        AddCurrentTurnEffect($cardID, $currentPlayer);
+        return "";
       default: return "";
     }
-  }
-
-  function ARCRunebladeHitEffect($cardID)
-  {
-    global $combatChainState, $mainPlayer, $CCS_DamageDealt;
-    switch($cardID)
-    {
-      case "ARC077":
-        PlayAura("ARC112", $mainPlayer);
-        break;
-      case "ARC080":
-        $damageDone = $combatChainState[$CCS_DamageDealt];
-        PlayAura("ARC112", $mainPlayer, $damageDone);
-        break;
-      default: break;
-    }
-    return "";
   }
 
 ?>
