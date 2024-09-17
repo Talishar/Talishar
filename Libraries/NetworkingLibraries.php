32b52fe385f38bc1a6e144f5b53f5f3fc115bde2
PvtVoid <jotrickr@gmail.com>
2024-09-17 11:01:44 +0200

    More Meld fixed

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1821,6 +1821,7 @@ function GetLayerTarget($cardID, $from)
 function AddPrePitchDecisionQueue($cardID, $from, $index = -1)
 {
   global $currentPlayer, $CS_NumActionsPlayed, $CS_AdditionalCosts, $turn, $combatChainState, $CCS_EclecticMag, $CS_NextWizardNAAInstant, $CS_NextNAAInstant;
+  global $actionPoints;
   if (IsStaticType(CardType($cardID), $from, $cardID)) {
     $names = GetAbilityNames($cardID, $index);
     if ($names != "") {
@@ -1833,11 +1834,11 @@ function AddPrePitchDecisionQueue($cardID, $from, $index = -1)
   if(HasMeld($cardID)) {
     $names = explode(" // ", CardName($cardID));
     $option = "Both,".$names[0].",".$names[1];
-    if(DelimStringContains(CardType($cardID), "A") 
-    && $turn[0] != "M" 
-    && !$combatChainState[$CCS_EclecticMag] 
+    if(!$combatChainState[$CCS_EclecticMag] 
     && GetClassState($currentPlayer, $CS_NextWizardNAAInstant) == 0 
     && GetClassState($currentPlayer, $CS_NextNAAInstant) == 0
+    && $actionPoints < 1
+    || $turn[0] != "M"
     ){
       $option = $names[1];
     }
