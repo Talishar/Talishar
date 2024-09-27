072642cd1417da65e1e7996fc168b3ab35dab824
PvtVoid <jotrickr@gmail.com>
2024-09-27 08:51:38 +0200

    fix Annihilator Engine

--- CardDictionaries/HitEffects.php
+++ CardDictionaries/HitEffects.php
@@ -56,14 +56,12 @@ function EVOHitEffect($cardID)
     case "EVO054":
       if (IsHeroAttackTarget() && EvoUpgradeAmount($mainPlayer) >= 1) {
         global $combatChain, $CombatChain;
-        $defendingCardsArr = explode(",", GetChainLinkCards($defPlayer, exclCardTypes: "C"));
-        rsort($defendingCardsArr);
+        $defendingCardsArr = array_reverse(explode(",", GetChainLinkCards($defPlayer, exclCardTypes: "C")));
         foreach ($defendingCardsArr as $defendingCard) {
           if (CardType($combatChain[$defendingCard]) == "E") {
             WriteLog(CardLink("EVO054", "EVO054") . " destroyed " . CardLink($combatChain[$defendingCard], $combatChain[$defendingCard]) . ".");
             $charID = FindCharacterIndex($defPlayer, $combatChain[$defendingCard]);
             DestroyCharacter($defPlayer, $charID);
-            $CombatChain->Remove($defendingCard);
           } else {
             WriteLog(CardLink("EVO054", "EVO054") . " destroyed " . CardLink($combatChain[$defendingCard], $combatChain[$defendingCard]) . ".");
             AddGraveyard($combatChain[$defendingCard], $defPlayer, "CC");
