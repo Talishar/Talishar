ef3226add921cb8d30e4bd84f179d7c27dbc991c
PvtVoid <jotrickr@gmail.com>
2024-10-09 11:25:09 +0200

    Fix Double Current Funnel removing the go again effect from the 2nd one

--- CardDictionaries/Rosetta/ROSShared.php
+++ CardDictionaries/Rosetta/ROSShared.php
@@ -50,7 +50,7 @@ function ROSCombatEffectActive($cardID, $attackID): bool
   return match ($cardID) {
     "ROS010-GOAGAIN" => TypeContains($attackID, "AA", $mainPlayer) || TypeContains($attackID, "A", $mainPlayer), //Arc Lightning giving next action go again.
     "ROS064", "ROS065", "ROS066", "ROS012", "ROS076" => true,
-    "ROS074" => $actionsPlayed[$numActions-2] == "ROS074" && (TypeContains($attackID, "AA", $mainPlayer) || TypeContains($attackID, "AA", $mainPlayer)),
+    "ROS074" => $actionsPlayed[$numActions-2] == "ROS074" && $actionsPlayed[$numActions-1] != "ROS074" && (TypeContains($attackID, "AA", $mainPlayer) || TypeContains($attackID, "AA", $mainPlayer)),
     "ROS110", "ROS111", "ROS112" => CardType($attackID) == "AA" && CardCost($attackID) <= 1,
     "ROS118" => CardType($attackID) == "AA" && ClassContains($attackID, "RUNEBLADE", $mainPlayer),
     "ROS127", "ROS128", "ROS129", "ROS119" => ClassContains($attackID, "RUNEBLADE", $mainPlayer),
