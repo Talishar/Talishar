ce968b17f9bdea6391684d64feb205f1c3fb36a0
PvtVoid <jotrickr@gmail.com>
2024-10-18 11:11:24 +0200

    Fix May ability of AJV018

--- CardDictionaries/ArmoryDecks/AJVShared.php
+++ CardDictionaries/ArmoryDecks/AJVShared.php
@@ -7,7 +7,7 @@ function AJVPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
     case "AJV018":
       AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "THEIRCHAR:type=E&MYCHAR:type=E");
       AddDecisionQueue("SETDQCONTEXT", $currentPlayer, "Choose an equipment to add a -1 defense counter", 1);
-      AddDecisionQueue("CHOOSEMULTIZONE", $currentPlayer, "<-", 1);
+      AddDecisionQueue("MAYCHOOSEMULTIZONE", $currentPlayer, "<-", 1);
       AddDecisionQueue("MODDEFCOUNTER", $currentPlayer, "-1", 1);
       return "";
     default:
