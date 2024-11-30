5179ae408a3042d58cf06004f7f452ead155b69d
Ballin2much <smomark12@gmail.com>
2024-11-30 01:42:42 -0500

    Updated script to automatically create games folder and text file

--- start.sh
+++ start.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 cp -n HostFiles/RedirectorTemplate.php HostFiles/Redirector.php
 
+# Create a text file with the contents "1" if it doesn't exist
+if [ ! -f "HostFiles/GameIDCounter.txt" ]; then
+  echo "1" > HostFiles/GameIDCounter.txt
+fi
+
+# Set permissions for the file so anyone can update
+chmod 666 HostFiles/GameIDCounter.txt
+
+# Create a blank folder if it doesn't exist
+mkdir -p Games
+
+# Set permissions for the folder so anyone can update
+chmod 777 Games
+
 docker compose up -d
