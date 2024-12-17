d3f0e53940c3e86b791938420c032e05e3519d10
PvtVoid <jotrickr@gmail.com>
2024-12-17 12:34:16 +0100

    Add Ally check for Provoke

--- CardDictionaries/Hunted/HNTShared.php
+++ CardDictionaries/Hunted/HNTShared.php
@@ -122,7 +122,7 @@ function HNTPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
       break;
     case "HNT117":
       $otherPlayer = ($currentPlayer == 1 ? 2 : 1);
-      if (TypeContains($CombatChain->AttackCard()->ID(), "W", $currentPlayer) & CanRevealCards($otherPlayer)) {
+      if (TypeContains($CombatChain->AttackCard()->ID(), "W", $currentPlayer) && CanRevealCards($otherPlayer) && !IsAllyAttacking()) {
         AddDecisionQueue("MULTIZONEINDICES", $otherPlayer, "MYHAND", 1);
         AddDecisionQueue("SETDQCONTEXT", $otherPlayer, "Choose a card from hand, action card will be blocked with, non-actions discarded");
         AddDecisionQueue("CHOOSEMULTIZONE", $otherPlayer, "<-", 1);
