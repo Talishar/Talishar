f34ade782fa60d38c8d646331bb73390d824d4ea
PvtVoid <jotrickr@gmail.com>
2024-12-23 08:57:56 +0100

    Update CardDictionary.php

--- CardDictionary.php
+++ CardDictionary.php
@@ -2195,32 +2195,32 @@ function IsPlayRestricted($cardID, &$restriction, $from = "", $index = -1, $play
     case "HNT003":
     case "HNT004":
     case "HNT006":
-      if (!($CombatChain->HasCurrentLink() && ClassContains($CombatChain->AttackCard()->ID(), "ASSASSIN"))) return true;
+      if (!($CombatChain->HasCurrentLink() && ClassContains($CombatChain->AttackCard()->ID(), "ASSASSIN", $currentPlayer))) return true;
       if (SearchHand($currentPlayer, class:"ASSASSIN") == "") return true;
       return false;
     case "HNT005":
       if (SearchHand($currentPlayer, class:"ASSASSIN") == "") return true;
       return false;
     case "HNT007":
-      if (!($CombatChain->HasCurrentLink() && SubtypeContains($CombatChain->AttackCard()->ID(), "Dagger"))) return true;
+      if (!($CombatChain->HasCurrentLink() && SubtypeContains($CombatChain->AttackCard()->ID(), "Dagger", $currentPlayer))) return true;
       if (SearchHand($currentPlayer, class:"ASSASSIN") == "") return true;
       return false;
     case "HNT015":
       if (!$CombatChain->HasCurrentLink()) return true;
       if (HasStealth($CombatChain->AttackCard()->ID()) && NumCardsBlocking() > 0) return false;
-      if (SubtypeContains($CombatChain->AttackCard()->ID(), "Dagger")) return false;
+      if (SubtypeContains($CombatChain->AttackCard()->ID(), "Dagger", $currentPlayer)) return false;
       return true;
     case "HNT102":
       if (!$CombatChain->HasCurrentLink()) return true;
       // This next line is based on my interpretation of the card. It seems to require you to pick all 3 modes
       // if you have 3 draconic chain links, and you can't pick the first mode without a dagger attack
       // this could change with release notes
-      if (NumDraconicChainLinks() > 2 && !SubtypeContains($CombatChain->CurrentAttack(), "Dagger")) return true;
+      if (NumDraconicChainLinks() > 2 && !SubtypeContains($CombatChain->CurrentAttack(), "Dagger", $currentPlayer)) return true;
       if (NumDraconicChainLinks() > 0) {
         // make sure you have at least one dagger equipped
         $mainCharacter = &GetPlayerCharacter($mainPlayer);
         for ($i = 0; $i < count($mainCharacter); $i += CharacterPieces()) {
-          if (SubtypeContains($mainCharacter[$i], "Dagger")) return false;
+          if (SubtypeContains($mainCharacter[$i], "Dagger", $mainPlayer)) return false;
         }
         return true;
       }
