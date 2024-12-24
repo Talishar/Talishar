27bf7c698d82c73df03e9e7b6f695d9cb20f1a35
PvtVoid <jotrickr@gmail.com>
2024-12-24 12:02:38 +0100

    Add overlay to facedown cards showing face-up for players

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -562,7 +562,10 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
       $border = 0;
     }
     elseif (isFaceDownMod($mod) && $playerID == 3) $cardID = "CardBack";
-    if ($myBanish[$i + 1] == "INT") $label = "Intimidated";
+    if ($mod == "INT") {
+      $overlay = 1;
+      $label = "Intimidated";
+    }
     array_push($playerBanishArr, JSONRenderedCard($cardID, $action, $overlay, borderColor: $border, actionDataOverride: strval($i), label: $label));
   }
   $response->playerBanish = $playerBanishArr;
