be3fadc78d81e70832acb67a3fb53f209af002f5
PvtVoid <jotrickr@gmail.com>
2025-01-14 14:57:19 +0100

    Fix the sideboarding against AI

--- APIs/GetLobbyRefresh.php
+++ APIs/GetLobbyRefresh.php
@@ -168,7 +168,10 @@ if ($lastUpdate != 0 && $cacheVal < $lastUpdate) {
   else if ($playerID == 2 && $gameStatus >= $MGS_ReadyToStart) $response->myPriority = false;
 
   $response->isMainGameReady = ($gameStatus == $MGS_ReadyToStart && $p1SideboardSubmitted == "1" && $p2SideboardSubmitted == "1");
-  $response->canSubmitSideboard = ($gameStatus > $MGS_ChooseFirstPlayer && $gameStatus != $MGS_ReadyToStart);
+  if($p1IsAI || $p2IsAI) {
+    $response->canSubmitSideboard =($gameStatus > $MGS_ChooseFirstPlayer && ($playerID == 1 ? $p1SideboardSubmitted == "0" : $p2SideboardSubmitted == "0"));
+  }
+  else $response->canSubmitSideboard = ($gameStatus > $MGS_ChooseFirstPlayer && $gameStatus != $MGS_ReadyToStart);
 
   $decklink = ($playerID == 1 ? $p1DeckLink : $p2DeckLink);
   $matchups = ($playerID == 1 ? $p1Matchups : $p2Matchups);
