6f052c37f1c159bd004f20b81e66aca634eae7e3
Lucas B. <155521696+xdBananaBread@users.noreply.github.com>
2025-01-17 15:51:24 -0800

    Update CoreLogic.php

--- CoreLogic.php
+++ CoreLogic.php
@@ -2477,6 +2477,8 @@ function SelfCostModifier($cardID, $from)
     case "HNT061":
     case "HNT057":
       return (-1 * NumDraconicChainLinks());
+    case "HNT109":
+      return (-1 * NumDraconicChainLinks());
     case "HNT155":
       return (-1 * NumDraconicChainLinks());
     default:
