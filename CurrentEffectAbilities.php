a742ecfa16d058b9fb7ce90ad9dab5604f322278
Paul Gibby <pgibby8@hotmail.com>
2025-01-18 15:36:33 -0500

    rake should always be persistent

--- CurrentEffectAbilities.php
+++ CurrentEffectAbilities.php
@@ -1990,6 +1990,7 @@ function IsCombatEffectPersistent($cardID)
     case "HNT137-MARKEDBUFF":
     case "HNT138-MARKEDBUFF":
     case "HNT139-MARKEDBUFF":
+    case "HNT156":
     case "HNT185":
     case "HNT186":
     case "HNT187":
@@ -1997,8 +1998,6 @@ function IsCombatEffectPersistent($cardID)
     case "HNT258-BUFF":
     case "HNT258-DMG":
       return true;
-    case "HNT156":
-      return CardTalent($cardID) == "Draconic";
     //Roguelike
     case "ROGUE018":
     case "ROGUE601":
