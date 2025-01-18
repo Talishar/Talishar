454dda8f03ee4f97900a4d95713b161b9ae965cd
Paul Gibby <pgibby8@hotmail.com>
2025-01-18 14:58:54 -0500

    simplifying

--- CardDictionaries/Hunted/HNTShared.php
+++ CardDictionaries/Hunted/HNTShared.php
@@ -249,8 +249,7 @@ function HNTPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
       break;
     case "HNT005":
       EquipWeapon($currentPlayer, "HNT053");
-      if (!$CombatChain->HasCurrentLink()) AddCurrentTurnEffect($cardID, $currentPlayer);
-      else AddCurrentTurnEffectNextAttack($cardID, $currentPlayer);
+      AddCurrentTurnEffectNextAttack($cardID, $currentPlayer);
       break;
     case "HNT006":
       AddCurrentTurnEffect($cardID, $currentPlayer);
