86cd153b63c426e89a14a73fa5536d4a03a46806
Paul Gibby <pgibby8@hotmail.com>
2025-02-14 17:58:01 -0500

    updating zzCardCodeGenerator to output an associative array with new cardIDs

--- zzCardCodeGenerator.php
+++ zzCardCodeGenerator.php
@@ -48,6 +48,8 @@
 
   function GenerateFunction(&$cardArray, $handler, $functionName, $propertyName, $defaultValue="", $sparse=false)
   {
+    $SEP = "_";
+    $rarityDict = ["T"=>0, "C"=>1, "R"=>2, "M"=>3, "L"=>4, "F"=>5, "V"=>6, "P"=>7, "S"=>8, "-"=>9];
     echo("<BR>" . $functionName . "<BR>");
     fwrite($handler, "function Generated" . $functionName . "(\$cardID) {\r\n");
     $originalSets = ["WTR", "ARC", "CRU", "MON", "ELE", "EVR", "UPR", "DYN", "OUT", "DVR", "RVD", "DTD", "LGS", "HER", "FAB", "JDG", "TCC", "EVO", "HVY", "MST", "AKO", "ASB", "AAZ", "TER", "AUR", "AIO", "ROS", "AJV", "HNT", "ARK"];
@@ -57,89 +59,65 @@
     if($propertyName == "1H" || $propertyName == "specialization") $isBool = true;
     fwrite($handler, "if(\$cardID !== null && strlen(\$cardID) < 6) return " . ($isString ? "\"\"" : "0") . ";\r\n");
     fwrite($handler, "if(is_int(\$cardID)) return " . ($isString ? "\"\"" : "0") . ";\r\n");
-    if($sparse) fwrite($handler, "switch(\$cardID) {\r\n");
-    $trie = [];
-    $cardsSeen = [];
+    fwrite($handler, "return match(\$cardID) {\r\n");
+    $associativeArray = [];
     for($i=0; $i<count($cardArray); ++$i)
     {
-      $cardRarity = "NA";
-      $cardPrintings = [];
+      $cardRarity = "-";
+      $cardID = strtolower($cardArray[$i]->name);
+      $setID = $cardArray[$i]->printings[0]->id;
+      if ($cardArray[$i]->pitch != "") {
+        $cardID .= $SEP . $cardArray[$i]->pitch;
+      }
       for($j=0; $j<count($cardArray[$i]->printings); ++$j)
       {
-        $cardRarity = $cardArray[$i]->printings[$j]->rarity;
-        $cardID = $cardArray[$i]->printings[$j]->id;
-        $set = substr($cardID, 0, 3);
-        $cardNumber = substr($cardID, 3, 3);
-        if(!in_array($set, $originalSets)) continue;
-        if($set == "LSS" && $cardNumber != 004) continue;
-        if($set == "LGS" && $cardNumber < 176) continue;
-        if($set == "LGS" && $cardNumber > 178) continue;
-        if($set == "HER" && $cardNumber != 117 && $cardNumber != 100 && $cardNumber != 123 && $cardNumber != 130) continue;
-        if($set == "FAB" && $cardNumber < 500) continue;
-        if(isset($cardArray[$i]->printings[0]->double_sided_card_info) && !$cardArray[$i]->printings[0]->double_sided_card_info[0]->is_front && $cardArray[$i]->printings[0]->rarity != "T") { $cardNumber += 400; $cardID = $set . $cardNumber; }
-        else {
-          $duplicate = false;
-          for($k=0; $k<count($cardPrintings); ++$k)
-          {
-            if($cardPrintings[$k] == $cardID) $duplicate = true;
-          }
-          for($k=0; $k<count($cardsSeen); ++$k)
-          {
-            if($cardsSeen[$k] == $cardID) $duplicate = true;
-          }
-          if($duplicate && $set != "DVR" && $set != "RVD") continue;
-        }
-        array_push($cardPrintings, $cardID);
-        array_push($cardsSeen, $cardID);
-        PopulateTrie($cardArray, $handler, $trie, $propertyName, $cardID, $i, $sparse, $isBool, $isString, $defaultValue, $cardRarity);
-        if(ShouldDuplicate($cardArray[$i])) PopulateTrie($cardArray, $handler, $trie, $propertyName, $set . ($cardNumber + 400), $i, $sparse, $isBool, $isString, $defaultValue, $cardRarity, true);
+        $printingRarity = $cardArray[$i]->printings[$j]->rarity;
+        $cardRarity = $rarityDict[$printingRarity] < $rarityDict[$cardRarity] ? $printingRarity : $cardRarity;
+      }
+      PopulateAssociativeArray($cardArray, $setID, $associativeArray, $propertyName, $cardID, $i, $isBool, $isString, $defaultValue, $cardRarity);
+      if(isset($cardArray[$i]->printings[0]->double_sided_card_info) && !$cardArray[$i]->printings[0]->double_sided_card_info[0]->is_front && $cardArray[$i]->printings[0]->rarity != "T") {
+        $backID = strtolower($cardArray[$i]->name) . $SEP . "back";
+        PopulateAssociativeArray($cardArray, $setID, $associativeArray, $propertyName, $backID, $i, $isBool, $isString, $defaultValue, $cardRarity,true);
       }
     }
-    if($sparse) fwrite($handler, "default: return " . ($isString ? "\"$defaultValue\"" : $defaultValue) . ";}\r\n");
-    else TraverseTrie($trie, "", $handler, $isString, $defaultValue);
-
-    fwrite($handler, "}\r\n\r\n");
+    TraverseAssociativeArray($associativeArray, $handler, $isString, $defaultValue);
+    fwrite($handler, "};\r\n}\r\n");
   }
 
-  function PopulateTrie(&$cardArray, $handler, &$trie, $propertyName, $cardID, $i, $sparse, $isBool, $isString, $defaultValue, $cardRarity, $isDuplicate=false) {
-    if($propertyName == "type") $data = MapType($cardArray[$i], $cardID);
-    else if($propertyName == "attack") {
+function PopulateAssociativeArray($cardArray, $setID, &$AA, $propertyName, $cardID, $i, $isBool, $isString, $defaultValue, $cardRarity, $isDuplicate=false) {
+  switch ($propertyName) {
+    case "type":
+      $data = MapType($cardArray[$i], $cardID);
+      break;
+    case "attack":
       $data = $cardArray[$i]->power;
       if($data == "X") $data = 0;
-    }
-    else if($propertyName == "block")
-    {
+      break;
+    case "block":
       $data = $cardArray[$i]->defense;
       if($data == "") $data = -1;
       if($data == "*") $data = 0;
-    }
-    else if($propertyName == "name") $data = $cardArray[$i]->name;
-    else if($propertyName == "pitch")
-    {
+      break;
+    case "name":
+      $data = $cardArray[$i]->name;
+      break;
+    case "pitch":
       $data = $cardArray[$i]->pitch;
       if($data == "") $data = 0;
-    }
-    else if($propertyName == "cost")
-    {
+      break;
+    case "cost":
       $data = $cardArray[$i]->cost;
       if($data == "") $data = -1;
       $data = intval($data);
-    }
-    else if($propertyName == "health")
-    {
+      break;
+    case "health":
       $data = $cardArray[$i]->health;
-      CheckImage($cardID, $isDuplicate);
-    }
-    else if($propertyName == "rarity")
-    {
+      CheckImage($setID, $isDuplicate);
+      break;
+    case "rarity":
       $data = $cardRarity;
-    }
-    else if($propertyName == "rarity")
-    {
-      $data = $cardRarity;
-    }
-    else if($propertyName == "subtype")
-    {
+      break;
+    case "subtype":
       $data = "";
       for($k=0; $k<count($cardArray[$i]->types); ++$k)
       {
@@ -150,18 +128,16 @@
           $data .= $type;
         }
       }
-    }
-    else if($propertyName == "1H")
-    {
+      break;
+    case "1H":
       $data = "false";
       for($k=0; $k<count($cardArray[$i]->types); ++$k)
       {
         $type = $cardArray[$i]->types[$k];
         if($type == "1H") $data = "true";
       }
-    }
-    else if($propertyName == "specialization")
-    {
+      break;
+    case "specialization":
       $data = "false";
       for($k=0; $k<count($cardArray[$i]->card_keywords); ++$k)
       {
@@ -170,9 +146,8 @@
           if($keywordArray[$l] == "Specialization") $data = "true";
         }
       }
-    }
-    else if($propertyName == "cardClass")
-    {
+      break;
+    case "cardClass":
       $data = "";
       for($k=0; $k<count($cardArray[$i]->types); ++$k)
       {
@@ -183,9 +158,8 @@
           $data .= strtoupper($type);
         }
       }
-    }
-    else if($propertyName == "cardTalent")
-    {
+      break;
+    case "cardTalent":
       $data = "";
       for($k=0; $k<count($cardArray[$i]->types); ++$k)
       {
@@ -196,15 +170,23 @@
           $data .= strtoupper($type);
         }
       }
-    }
-    if($isBool);
-    else if(($isString == false && !is_numeric($data) && $data != "") || $data == "-" || $data == "*" || $data == "X") echo("Exception with property name " . $propertyName . " data " . $data . " card " . $cardID . "<BR>");
-    if(($isBool && $data == "true") || ($data != "-" && $data != "" && $data != "*" && $data != $defaultValue))
-    {
-      if($sparse) fwrite($handler, "case \"" . $cardID . "\": return " . ($isString ? "\"$data\"" : $data) . ";\r\n");
-      else AddToTrie($trie, $cardID, 0, $data);
-    }
+      break;
+    default:
+      break;
   }
+  if($isBool);
+  else if(($isString == false && !is_numeric($data) && $data != "") || $data == "-" || $data == "*" || $data == "X") echo("Exception with property name " . $propertyName . " data " . $data . " card " . $cardID . "<BR>");
+  if(($isBool && $data == "true") || ($data != "-" && $data != "" && $data != "*" && $data != $defaultValue))
+  {
+    $AA[$cardID] = $data;
+  }
+}
+
+function TraverseAssociativeArray($AA, $handler, $isString, $defaultValue) {
+  foreach ($AA as $cardID => $data) {
+    fwrite($handler, "\"$cardID\" => \"$data\",\r\n");
+  }
+}
 
   function MapType($card, $cardID)
   {
