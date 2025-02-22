e2ae7b02cbcea86389be0e9914b4154f41a62e3a
Paul Gibby <pgibby8@hotmail.com>
2025-02-22 18:49:09 -0500

    updating jp card image script

--- zzJPImageConverter.php
+++ zzJPImageConverter.php
@@ -1,21 +1,17 @@
 <?php
-$filename = "../jp_hnt_image_locs.json";
+$filename = "../jp_ajv_image_locs.json";
 $jsonObject = file_get_contents($filename);
 $imageLocs = json_decode($jsonObject, true);
 
 foreach ($imageLocs as $cardID => $imageURL) {
     CheckImageJP($cardID, $imageURL);
-    break;
 }
 
 function CheckImageJP($cardID, $imageURL, $isDuplicate=false)
 {
   $filename = "./WebpImages/" . $cardID . ".webp";
-//   $filenameNew = "./New Cards/" . $cardID . ".webp";
   $cardImagesUploadedFolder = "../CardImages/media/uploaded/public/cardimages/japanese/" . $cardID . ".webp"; // !! CardImages/ to be changed for your own folder name
   $cardImagesMissingFolder = "../CardImages/media/missing/cardimages/japanese/" . $cardID . ".webp"; // !! CardImages/ to be changed for your own folder name
-//   if(!file_exists($filename) || !file_exists($cardImagesUploadedFolder))
-  // if (!file_exists($cardImagesUploadedFolder))
   if (true)
   {
     echo("Image for " . $cardID . " does not exist.<BR>");
@@ -33,9 +29,7 @@ function CheckImageJP($cardID, $imageURL, $isDuplicate=false)
       echo("Normalizing file size for " . $cardID . ".<BR>");
       $image = imagecreatefromwebp($filename);
       $image = imagescale($image, 450, 628);
-    //   imagewebp($image, $filename);
       imagewebp($image, $cardImagesMissingFolder);
-    //   if(!file_exists($filenameNew)) imagewebp($image, $filenameNew);
       // Free up memory
       imagedestroy($image);
     }
@@ -43,8 +37,6 @@ function CheckImageJP($cardID, $imageURL, $isDuplicate=false)
   $concatFilename = "./concat/" . $cardID . ".webp";
   $cardSquaresUploadedFolder = "../CardImages/media/uploaded/public/cardsquares/japanese/" . $cardID . ".webp"; // !! CardImages/ to be changed for your own folder name
   $cardSquaresMissingFolder = "../CardImages/media/missing/cardsquares/japanese/" . $cardID . ".webp"; // !! CardImages/ to be changed for your own folder name
-//   if(!file_exists($concatFilename) || !file_exists($cardSquaresUploadedFolder))
-  // if (!file_exists($cardSquaresUploadedFolder))
   if (true)
   {
     echo("Concat image for " . $cardID . " does not exist.<BR>");
@@ -52,14 +44,17 @@ function CheckImageJP($cardID, $imageURL, $isDuplicate=false)
     {
       echo("Attempting to convert image for " . $cardID . " to concat.<BR>");
       $image = imagecreatefromwebp($filename);
-      $imageTop = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 450, 'height' => 372]);
-      $imageBottom = imagecrop($image, ['x' => 0, 'y' => 550, 'width' => 450, 'height' => 628]);
+      $width = 540;
+      $topHeight = 440;
+      $botHeight = $width - $topHeight;
+      
+      $imageTop = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => $width, 'height' => $topHeight]);
+      $imageBottom = imagecrop($image, ['x' => 0, 'y' => 670, 'width' => $width, 'height' => $botHeight]);
 
-      $dest = imagecreatetruecolor(450, 450);
-      imagecopy($dest, $imageTop, 0, 0, 0, 0, 450, 372);
-      imagecopy($dest, $imageBottom, 0, 373, 0, 0, 450, 78);
+      $dest = imagecreatetruecolor($width, $width);
+      imagecopy($dest, $imageTop, 0, 0, 0, 0, $width, $topHeight);
+      imagecopy($dest, $imageBottom, 0, $topHeight, 0, 0, $width, $botHeight);
 
-    //   if(!file_exists($concatFilename)) imagewebp($dest, $concatFilename);
       imagewebp($dest, $cardSquaresMissingFolder);
       // Free up memory
       imagedestroy($image);
@@ -69,24 +64,6 @@ function CheckImageJP($cardID, $imageURL, $isDuplicate=false)
       if(file_exists($concatFilename)) echo("Image for " . $cardID . " successfully converted to concat.<BR>");
     }
   }
-//   $cropFilename = "./crops/" . $cardID . "_cropped.png";
-//   $cardCropsUploadedFolder = "../CardImages/media/uploaded/public/crops/" . $cardID . "_cropped.png"; // !! CardImages/ to be changed for your own folder name
-//   $cardCropsMissingFolder = "../CardImages/media/missing/crops/" . $cardID . "_cropped.png"; // !! CardImages/ to be changed for your own folder name
-//   if(!file_exists($cropFilename) || !file_exists($cardCropsUploadedFolder))
-//   // if (!file_exists($cardCropsUploadedFolder))
-//   {
-//     echo("Crop image for " . $cardID . " does not exist.<BR>");
-//     if(file_exists($filename))
-//     {
-//       echo("Attempting to convert image for " . $cardID . " to crops.<BR>");
-//       $image = imagecreatefromwebp($filename);
-//       $image = imagecrop($image, ['x' => 50, 'y' => 100, 'width' => 350, 'height' => 270]);
-//       if(!file_exists($cropFilename)) imagepng($image, $cropFilename);
-//       if(!file_exists($cardCropsUploadedFolder)) imagepng($image, $cardCropsMissingFolder);
-//       imagedestroy($image);
-//       if(file_exists($cropFilename)) echo("Image for " . $cardID . " successfully converted to crops.<BR>");
-//     }
-//   }
 }
 
 
