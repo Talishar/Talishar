a826a29734be8c6223a1b0bf7b8a5f7873cdaf2c
Paul Gibby <pgibby8@hotmail.com>
2025-03-02 18:23:33 -0500

    fixing somersault on a popper

--- CoreLogic.php
+++ CoreLogic.php
@@ -1043,15 +1043,17 @@ function ChainLinkResolvedEffects()
 
 function ResolutionStepEffectTriggers()
 {
-  global $currentTurnEffects;
+  global $currentTurnEffects, $chainLinks;
   for ($i = count($currentTurnEffects) - CurrentTurnEffectsPieces(); $i >= 0; $i -= CurrentTurnEffectsPieces()) {
     $currentEffect = explode("-", $currentTurnEffects[$i]);
     switch ($currentEffect[0]) {
       case "electromagnetic_somersault_red":
       case "electromagnetic_somersault_yellow":
       case "electromagnetic_somersault_blue":
-        $player = $currentTurnEffects[$i + 1];
-        AddLayer("TRIGGER", $player, $currentEffect[0], $currentEffect[1]);
+        if (count($chainLinks) > 0) { //only do this if the chain wasn't forced closed
+          $player = $currentTurnEffects[$i + 1];
+          AddLayer("TRIGGER", $player, $currentEffect[0], $currentEffect[1]);
+        }
         RemoveCurrentTurnEffect($i);
         break;
       default:
