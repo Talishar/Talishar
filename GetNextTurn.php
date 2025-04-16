68d020f2e086d5f1d17a84a021128e54bd171aa7
PvtVoid <jotrickr@gmail.com>
2025-04-16 14:26:58 +0200

    Reapply "Show as damage prevention cards like shelter from the storm"
    This reverts commit f8b74c075ba60db5a0e834bc6920ddb00a7a4f40.

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -266,7 +266,12 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
   }
   $activeChainLink->reactions = $combatChainReactions;
   $activeChainLink->attackTarget = CardName(GetMZCard($mainPlayer, GetAttackTarget()));
-  $activeChainLink->damagePrevention = GetDamagePrevention($defPlayer);
+  if (count($combatChain) > 0) {
+    $activeChainLink->damagePrevention = GetDamagePrevention($defPlayer) + CurrentEffectPreventDamagePrevention($defPlayer, 100, $combatChain[0], true);
+  } 
+  else  {
+      $activeChainLink->damagePrevention = GetDamagePrevention($defPlayer);
+  }
   $activeChainLink->goAgain = DoesAttackHaveGoAgain();
   $activeChainLink->dominate = CachedDominateActive();
   $activeChainLink->overpower = CachedOverpowerActive();
