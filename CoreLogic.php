87b24a78b129ffd9bdbb1e4178ba5bce08dd4226
PvtVoid <jotrickr@gmail.com>
2025-04-22 15:51:20 +0200

    Fix Goldkiss Rum + go again??? Might break something else

--- CoreLogic.php
+++ CoreLogic.php
@@ -2313,7 +2313,7 @@ function ResolveGoAgain($cardID, $player, $from="", $additionalCosts="-")
   $goAgainPrevented = CurrentEffectPreventsGoAgain($cardID, $from);
   if (IsStaticType($cardType, $from, $cardID)) {
     $hasGoAgain = AbilityHasGoAgain($cardID);
-    if (!$hasGoAgain && GetResolvedAbilityType($cardID, $from) == "A") $hasGoAgain = CurrentEffectGrantsNonAttackActionGoAgain($cardID, $from);
+    if (GetResolvedAbilityType($cardID, $from) == "A") $hasGoAgain = CurrentEffectGrantsNonAttackActionGoAgain($cardID, $from);
   } else {
     $hasGoAgain = HasMeld($cardID) ? 0 : HasGoAgain($cardID);
     if (GetClassState($player, $CS_NextNAACardGoAgain) && (DelimStringContains($cardType, "A") || $from == "MELD")) {
