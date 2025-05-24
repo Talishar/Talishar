012b526f9a30588c1169a68e9806c034ab2a246d
Paul Gibby <pgibby8@hotmail.com>
2025-05-24 17:26:52 -0400

    fixing divvy up

--- CardDictionaries/HighSeas/SEAShared.php
+++ CardDictionaries/HighSeas/SEAShared.php
@@ -302,7 +302,7 @@ function SEAPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
       $treasureID = SearchLandmarksForID("treasure_island");
       $char = GetPlayerCharacter($currentPlayer);
       if ($treasureID != -1) {
-        ClassContains($char[0], "Thief", $currentPlayer) ? $numGold = $landmarks[$treasureID + 3] : round($landmarks[$treasureID + 3] / 2);
+        $numGold = ClassContains($char[0], "Thief", $currentPlayer) ? $numGold = $landmarks[$treasureID + 3] : round($landmarks[$treasureID + 3] / 2);
         $landmarks[$treasureID + 3] -= $numGold;
         PutItemIntoPlayForPlayer("gold", $currentPlayer, number:$numGold, isToken:true);
         WriteLog("Player $currentPlayer plundered $numGold " . CardLink("gold", "gold") . " from " . CardLink("treasure_island", "treasure_island"));
