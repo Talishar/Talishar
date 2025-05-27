7be3bd5eb5a4d6bf6eded04053bd22fefa674ba8
PvtVoid <jotrickr@gmail.com>
2025-05-27 12:52:34 +0200

    Fix Crown of Dominion making it impossible for Gold Hook to be destroyed turn 0

--- CardLogic.php
+++ CardLogic.php
@@ -2359,7 +2359,7 @@ function ProcessTrigger($player, $parameter, $uniqueID, $target = "-", $addition
       DestroyAuraUniqueID($player, $uniqueID);
       break;
     case "crown_of_dominion":
-      PutItemIntoPlayForPlayer("gold", $player, effectController: $player);
+      PutItemIntoPlayForPlayer("gold", $player, mainPhase:"Skip", effectController: $player);
       WriteLog(CardLink($parameter, $parameter) . " created a Gold token");
       break;
     case "ponder":
