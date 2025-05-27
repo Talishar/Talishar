09c01a141c324eece027a8660c07e52af4e1aa55
PvtVoid <jotrickr@gmail.com>
2025-05-27 10:12:58 +0200

    Update CoreLogic.php

--- CoreLogic.php
+++ CoreLogic.php
@@ -3248,7 +3248,6 @@ function Draw($player, $mainPhase = true, $fromCardEffect = true, $effectSource 
   }
   $hand = array_values($hand);
   $index = count($hand) - 1 ;
-  WriteLog("Player " . $player . " draw a card from " . CardLink($context, $context));
   return $index >= 0 ? $hand[$index] : -1;
 }
 
