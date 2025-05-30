41de92ce1d7f859ad4565dbae69504f3065362b9
Paul Gibby <pgibby8@hotmail.com>
2025-05-30 13:59:50 -0400

    revert last change

--- CardDictionaries/HighSeas/SEAShared.php
+++ CardDictionaries/HighSeas/SEAShared.php
@@ -925,7 +925,7 @@ function SEAPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
     case "nimby_yellow":
     case "nimby_blue":
       MZMoveCard($currentPlayer, "MYDECK:isSameName=nimblism_red", "MYHAND", may:true, isReveal:true);
-      AddDecisionQueue("SHUFFLEDECK", $currentPlayer, "-", 1);
+      AddDecisionQueue("SHUFFLEDECK", $currentPlayer, "-");
       break;
     case "jack_be_nimble_red":
     case "jack_be_quick_red":
