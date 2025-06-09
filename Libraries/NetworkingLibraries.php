1fc4f4bc86fa49414ba4c480a53d979ae3831022
Paul Gibby <pgibby8@hotmail.com>
2025-06-09 08:15:26 -0400

    fix shortcut for burn bare from arsenal

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1534,7 +1534,7 @@ function PlayCard($cardID, $from, $dynCostResolved = -1, $index = -1, $uniqueID 
         "teklo_plasma_pistol", "plasma_barrel_shot" => !str_contains(GetAbilityNames($cardID, $index, $from), "Attack"),
         default => false,
       };
-      if ($from != "PLAY" && DelimStringContains($cardType, "A") && !GoesOnCombatChain($turn[0], $cardID, $from, $currentPlayer) && GetAbilityTypes($cardID, $index, $from) == "" && !HasMeld($cardID)) {
+      if ($from != "PLAY" && DelimStringContains($cardType, "A") && !GoesOnCombatChain($turn[0], $cardID, $from, $currentPlayer) && (GetAbilityTypes($cardID, $index, $from) == "" || GetAbilityNames($cardID, $index, $from) == "-,Action") && !HasMeld($cardID)) {
         if ($from == "HAND") AddPlayerHand($cardID, $currentPlayer, "HAND"); //card is still getting removed from hand, just put it back
         elseif ($from == "ARS") AddArsenal($cardID, $currentPlayer, "ARS", $facing);
         $layerPriority[$currentPlayer - 1] = "0";
