7d2840288c11d707d7b3e55361de25dffd1aee29
Paul Gibby <pgibby8@hotmail.com>
2025-06-15 20:26:04 -0400

    another bug fix to trigger ordering

--- GameLogic.php
+++ GameLogic.php
@@ -2749,8 +2749,8 @@ function DecisionQueueStaticEffect($phase, $player, $parameter, $lastResult)
       if ($ind != "-") {
         $currentInd = -1 * LayerPieces();
         for ($i = 0; $i < count($layers); $i += LayerPieces()) {
-          if ($layers[$i] == "PRETRIGGER" && $layers[$i + 1] == $player) $currentInd += LayerPieces();
-          if ($currentInd == $ind) {
+          if ($layers[$i] == "PRETRIGGER") $currentInd += LayerPieces();
+          if (($ind == "FIRST" && $layers[$i+1] == $player) || $currentInd == $ind) {
             $pretrigger = array_slice($layers, $i, LayerPieces());
             $pretrigger[0] = "TRIGGER";
             for ($j = $i + LayerPieces() - 1; $j >= $i; --$j) {
