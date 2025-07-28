0a55d5fdc077af43e0bab657fddf1f1a286c85ae
Paul Gibby <pgibby8@hotmail.com>
2025-07-28 15:48:25 -0400

    fixing dig in set

--- APIs/JoinGame.php
+++ APIs/JoinGame.php
@@ -639,7 +639,6 @@ function isUnimplemented($cardID) {
         "clash_of_mountains_red", "clash_of_mountains_yellow", "clash_of_mountains_blue" => false,
         "clash_of_bravado_yellow", "break_stature_yellow", "test_of_iron_grip_red", "visit_anvilheim_blue" => false,
         "fault_line_red", "renounce_grandeur_red", "hostile_encroachment_red" => false,
-        "dig_in_red", "dig_in_yellow", "dig_in_blue" => false,
         default => true
       };
     case "SUP":
@@ -672,6 +671,7 @@ function isUnimplemented($cardID) {
         "helm_of_hindsight" => false,
         "punching_gloves" => false,
         "garland_of_spring" => false,
+        "dig_in_red", "dig_in_yellow", "dig_in_blue" => false,
         default => true
       };
     case "APS":
