0026d4aba37f120f84d43b6cea89cf08b53867be
Paul Gibby <pgibby8@hotmail.com>
2025-09-02 11:44:51 -0400

    fixing spoiled skull with equipment removing

--- CharacterAbilities.php
+++ CharacterAbilities.php
@@ -1226,7 +1226,6 @@ function EquipPayAdditionalCosts($cardIndex)
       $char = new Character($currentPlayer, $cardIndex);
       $char->status = 0;
       BanishCardForPlayer($char->cardID, $currentPlayer, "EQUIP", "NA");
-      $char->Finished();
       break;
     case "flail_of_agony":
       LoseHealth(1, $currentPlayer);
