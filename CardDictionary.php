7f30d1228416986d0ac7955003c3b08419e4ba28
Paul Gibby <pgibby8@hotmail.com>
2025-09-22 15:49:15 -0400

    fix full of bravado's power

--- CardDictionary.php
+++ CardDictionary.php
@@ -1029,6 +1029,8 @@ function PowerValue($cardID, $player="-", $from="CC", $index=-1, $base=false, $a
     "wrenchtastic" => 4, // AI custom weapon
     "story_beats_blue" => 5, //fabcube error
     "story_beats_yellow" => 6, //fabcube error
+    "full_of_bravado_yellow" => 6, //fabcube error
+    "full_of_bravado_blue" => 5, //fabcube error
     default => $basePower,
   };
   // Lyath ability is handled elsewhere while attacking
