cf47dbe6e814da77d416101c78eb37d55e18de1f
Paul Gibby <pgibby8@hotmail.com>
2025-09-22 20:58:14 -0400

    fix scour when played off kano's ability

--- GameLogic.php
+++ GameLogic.php
@@ -322,6 +322,7 @@ function DecisionQueueStaticEffect($phase, $player, $parameter, $lastResult)
         if (!in_array($ind, $currentTargets)) array_push($rv, $ind);
       }
       $rv = implode(",", $rv);
+      WriteLog("HERRE: $choices, $rv");
       return $rv == "" ? "PASS" : $rv;
     case "CUTTINGINDICES":
       $currentNames = [];
