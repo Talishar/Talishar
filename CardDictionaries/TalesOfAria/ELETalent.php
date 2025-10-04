0f71184b5d8641b4b72a60c29726c5220fbf451c
Paul Gibby <pgibby8@hotmail.com>
2025-10-03 20:07:06 -0400

    fix lightning press targeting

--- CardDictionaries/TalesOfAria/ELETalent.php
+++ CardDictionaries/TalesOfAria/ELETalent.php
@@ -167,8 +167,10 @@
         if($cardID == "lightning_press_yellow") $amount = 2;
         else if($cardID == "lightning_press_blue") $amount = 1;
         $targetIndex = intval(explode("-", $target)[1]);
+        $targetCard = $CombatChain->FindCardUID($targetIndex);
+        $index = $targetCard != "" ? $targetCard->Index() : -1;
         if (explode("-", $target)[0] == "COMBATCHAINLINK") {
-          if ($CombatChain->HasCurrentLink()) CombatChainPowerModifier($targetIndex, $amount);
+          if ($CombatChain->HasCurrentLink() && $index != -1) CombatChainPowerModifier($targetIndex, $amount);
         }
         elseif (explode("-", $target)[0] == "COMBATCHAINATTACKS") {
           // targeting a past chain link, do nothing for now
