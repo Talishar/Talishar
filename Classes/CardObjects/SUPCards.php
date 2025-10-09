3483d3b545e6beab77dadf63e677c9fb2d24f63e
Paul Gibby <pgibby8@hotmail.com>
2025-10-08 22:37:44 -0400

    refactor bait to take into account it being made at instant speed

--- Classes/CardObjects/SUPCards.php
+++ Classes/CardObjects/SUPCards.php
@@ -69,16 +69,16 @@ class bait extends Card {
     }
 
   function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1) {
+    global $CombatChain;
     if (IsReactionPhase()) {
-      // GiveAttackGoAgain();
-      // CombatChainPowerModifier(0, 1);
-      AddCurrentTurnEffect($this->cardID, $this->controller);
+      if ($CombatChain->AttackCard()->ID() == $this->cardID) AddCurrentTurnEffect("$this->cardID-BUFF", $this->controller);
+      else AddCurrentTurnEffectFromCombat("$this->cardID-BUFF", $this->controller);
     }
     return "";
   }
 
   function EffectPowerModifier($param, $attached = false) {
-    return 1;
+    return $param == "BUFF" ? 1 : 0;
   }
 
   function CombatEffectActive($parameter = '-', $defendingCard = '', $flicked = false) {
@@ -87,7 +87,7 @@ class bait extends Card {
   }
 
   function CurrentEffectGrantsGoAgain($param) {
-    return true;
+    return $param == "BUFF";
   }
 
   function NumUses() {
