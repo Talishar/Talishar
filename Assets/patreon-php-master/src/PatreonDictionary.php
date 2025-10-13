363e616d4edb5698ff7e477cba9e18b41fc1ad5a
PvtVoid <jotrickr@gmail.com>
2025-10-13 10:11:03 +0200

    Fix typo

--- Assets/patreon-php-master/src/PatreonDictionary.php
+++ Assets/patreon-php-master/src/PatreonDictionary.php
@@ -321,7 +321,7 @@ enum PatreonCampaign : string
       case "7329070": return ($userName == "GoAgainGamingAz");
       case "1787491": return ($userName == "RedZoneRogue");
       case "8997252": return ($userName == "phillip");
-      case "8955846": return ($userName == "Man_Sant" || "PollyCranka" || $userName == "Aegisworn" || $userName == "PvtVoid");
+      case "8955846": return ($userName == "Man_Sant" || $userName == "PollyCranka" || $userName == "Aegisworn" || $userName == "PvtVoid");
       case "6839952": return ($userName == "chonigman" || $userName == "Ijaque");
       case "7285727": return ($userName == "Brendan" || $userName == "TheClub");
       case "8635931": return ($userName == "TheTekloFoundry");
