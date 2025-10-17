4da64f7ebd7cf771c4b1c38fa74afe70b6f6e98c
Paul Gibby <pgibby8@hotmail.com>
2025-10-17 11:26:03 -0400

    fix scour possible cost counting

--- CardDictionary.php
+++ CardDictionary.php
@@ -758,6 +758,7 @@ function AbilityCost($cardID)
 function DynamicCost($cardID)
 {
   global $currentPlayer;
+  $otherPlayer = $currentPlayer == 1 ? 2 : 1;
   $cardID = BlindCard($cardID, true);
   switch ($cardID) {
     case "staunch_response_red":
@@ -775,8 +776,10 @@ function DynamicCost($cardID)
       return "3,4,5,6,7,8,9,10,11,12,13,14,15";
     case "scour_blue":
       $myAurasCount = SearchCount(SearchAura($currentPlayer, "", "", 0));
-      $otherPlayerAurasCount = SearchCount(SearchAura($currentPlayer == 1 ? 2 : 1, "", "", 0));
-      return $myAurasCount > $otherPlayerAurasCount ? GetIndices(SearchCount(SearchAura($currentPlayer, "", "", 0)) + 1) : GetIndices(SearchCount(SearchAura($currentPlayer == 1 ? 2 : 1, "", "", 0)) + 1);
+      $myAurasCount += SearchCount(SearchLayer($currentPlayer, subtype:"Aura"));
+      $otherPlayerAurasCount = SearchCount(SearchAura($otherPlayer, "", "", 0));
+      $otherPlayerAurasCount += SearchCount(SearchLayer($otherPlayer, subtype:"Aura"));
+      return $myAurasCount > $otherPlayerAurasCount ? GetIndices($myAurasCount + 1) : GetIndices($otherPlayerAurasCount + 1);
     case "ice_eternal_blue":
       return "0,2,4,6,8,10,12,14,16,18,20";
     case "hyper_scrapper_blue":
