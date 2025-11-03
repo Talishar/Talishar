f30a8f9f670d5599b0b9ced1d2d6837e91b813e1
Paul Gibby <pgibby8@hotmail.com>
2025-11-03 15:44:29 -0500

    still call the database fabonline in dev environments

--- Database/ConnectionManager.php
+++ Database/ConnectionManager.php
@@ -5,7 +5,7 @@
      $hostname = (!empty(getenv("MYSQL_SERVER_NAME")) ? getenv("MYSQL_SERVER_NAME") : "localhost");
      $username = (!empty(getenv("MYSQL_SERVER_USER_NAME")) ? getenv("MYSQL_SERVER_USER_NAME") : "root");
      $password = (!empty(getenv("MYSQL_ROOT_PASSWORD")) ? getenv("MYSQL_ROOT_PASSWORD") : "");
-     $database = "fabonline2";
+     $database = "fabonline";
 	   try {
        return mysqli_connect($hostname, $username, $password, $database);
      } catch (\Exception $e) {
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,12 +3,12 @@
 $servername = (!empty(getenv("MYSQL_SERVER_NAME")) ? getenv("MYSQL_SERVER_NAME") : "localhost");
 $dBUsername = (!empty(getenv("MYSQL_SERVER_USER_NAME")) ? getenv("MYSQL_SERVER_USER_NAME") : "root");
 $dBPassword = (!empty(getenv("MYSQL_ROOT_PASSWORD")) ? getenv("MYSQL_ROOT_PASSWORD") : "");
-$dBName = "fabonline2";
+$dBName = "fabonline";
 
 $reportingServername = (!empty(getenv("MYSQL_SERVER_NAME")) ? getenv("MYSQL_SERVER_NAME") : "localhost");
 $reportingDBUsername = (!empty(getenv("MYSQL_SERVER_USER_NAME")) ? getenv("MYSQL_SERVER_USER_NAME") : "root");
 $reportingDBPassword = (!empty(getenv("MYSQL_ROOT_PASSWORD")) ? getenv("MYSQL_ROOT_PASSWORD") : "");
-$reportingDBName = "fabonline2";
+$reportingDBName = "fabonline";
 
 /*
 $conn = GetDBConnection();
