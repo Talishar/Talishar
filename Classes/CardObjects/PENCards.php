59a01bea1cc541d52785ff2f7dcf7875796f4a7f
Timothy Treahy <timothytreahyii@gmail.com>
2025-11-16 11:55:01 -0500

    Search Multizone

--- Classes/CardObjects/PENCards.php
+++ Classes/CardObjects/PENCards.php
@@ -1,26 +1,34 @@
 <?php
 
-class synapse_sparkcap extends Card {
-  function __construct($controller) {
+class synapse_sparkcap extends Card
+{
+  function __construct($controller)
+  {
     $this->cardID = "synapse_sparkcap";
     $this->controller = $controller;
   }
 
-  function AbilityType($index = -1, $from = '-') {
+  function AbilityType($index = -1, $from = '-')
+  {
     return "A";
   }
 
-  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false) {
-    if (CheckTapped("MYCHAR-$index", $this->controller)) return true;
+  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false)
+  {
+    if (CheckTapped("MYCHAR-$index", $this->controller) && SearchMultizone($this->controller, "MYHAND:subtype=Evo") != "")
+      return true;
+    return false;
   }
 
-  function PayAdditionalCosts($from, $index = '-') {
+  function PayAdditionalCosts($from, $index = '-')
+  {
     Tap("MYCHAR-$index", $this->controller);
-    MZChooseAndBanish($currentPlayer, "MYHAND:subtype=Evo", "HAND,-");
+    MZChooseAndBanish($this->controller, "MYHAND:subtype=Evo", "HAND,-");
   }
 
-  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1) {
-    PlayAura("ponder", $currentPlayer);
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
+  {
+    PlayAura("ponder", $this->controller);
   }
 }
 ?>
