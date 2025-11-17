2aa834ee90ef38a84839f142cafe9f6226d2278f
Timothy Treahy <timothytreahyii@gmail.com>
2025-11-17 14:21:08 -0500

    Add Frosthaven, Leaven, Stormwind Sheath

--- Classes/CardObjects/PENCards.php
+++ Classes/CardObjects/PENCards.php
@@ -64,14 +64,17 @@ class savage_claw extends Card
 
   function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
   {
-    if (SearchCardList($additionalCosts, $this->controller, minAttack: 6) != "") AddCurrentTurnEffect($this->cardID, $this->controller);
+    if (SearchCardList($additionalCosts, $this->controller, minAttack: 6) != "")
+      AddCurrentTurnEffect($this->cardID, $this->controller);
   }
 
-  function CombatEffectActive($parameter = '-', $defendingCard = '', $flicked = false) {
+  function CombatEffectActive($parameter = '-', $defendingCard = '', $flicked = false)
+  {
     return true;
   }
 
-  function EffectPowerModifier($param, $attached = false) {
+  function EffectPowerModifier($param, $attached = false)
+  {
     return 1;
   }
 }
@@ -99,7 +102,8 @@ class grimoire_of_fellingsong extends Card
     PlayAura("runechant", $this->controller, 1, true);
   }
 
-  function EquipPayAdditionalCosts($cardIndex = '-') {
+  function EquipPayAdditionalCosts($cardIndex = '-')
+  {
     DestroyCharacter($this->controller, $cardIndex);
   }
 }
@@ -127,17 +131,20 @@ class boltn_boots extends Card
     GiveAttackGoAgain();
   }
 
-  function EquipPayAdditionalCosts($cardIndex = '-') {
+  function EquipPayAdditionalCosts($cardIndex = '-')
+  {
     DestroyCharacter($this->controller, $cardIndex);
   }
 
-  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false) {
+  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false)
+  {
     global $CombatChain;
     return !$CombatChain->HasCurrentLink() || CachedTotalPower() <= PowerValue($CombatChain->AttackCard()->ID(), $this->controller, "CC") || !CardSubType($CombatChain->AttackCard()->ID()) == "Arrow";
   }
 }
 
-class magmatic_carapace extends Card {
+class magmatic_carapace extends Card
+{
 
   function __construct($controller)
   {
@@ -145,18 +152,23 @@ class magmatic_carapace extends Card {
     $this->controller = $controller;
   }
 
-  function CardPlayTrigger($cardID, $from, $index) {
+  function CardPlayTrigger($cardID, $from, $index)
+  {
     $char = GetPlayerCharacter($this->controller);
     if (!CheckTapped("MYCHAR-$index", $this->controller)) {
-      if (SubtypeContains($cardID, "Aura", $this->controller)) AddLayer("TRIGGER", $this->controller, $this->cardID, "-", "-", $char[$index + 11]);
+      if (SubtypeContains($cardID, "Aura", $this->controller))
+        AddLayer("TRIGGER", $this->controller, $this->cardID, "-", "-", $char[$index + 11]);
     }
   }
 
-  function ProcessTrigger($uniqueID, $target = '-', $additionalCosts = '-', $from = '-') {
+  function ProcessTrigger($uniqueID, $target = '-', $additionalCosts = '-', $from = '-')
+  {
     $index = SearchCharacterForUniqueID($uniqueID, $this->controller);
     $char = GetPlayerCharacter($this->controller);
-    if ($index == -1) return;
-    if (CheckTapped("MYCHAR-$index", $this->controller)) return;
+    if ($index == -1)
+      return;
+    if (CheckTapped("MYCHAR-$index", $this->controller))
+      return;
     AddDecisionQueue("SETDQCONTEXT", $this->controller, "if you want to tap " . CardLink("magmatic_carapace", "magmatic_carapace") . " and pay 1 to create a " . CardLink("seismic_surge", "seismic_surge"));
     AddDecisionQueue("YESNO", $this->controller, "-", 1);
     AddDecisionQueue("NOPASS", $this->controller, "-", 1);
@@ -166,6 +178,84 @@ class magmatic_carapace extends Card {
     AddDecisionQueue("PAYRESOURCES", $this->controller, "<-", 1);
     AddDecisionQueue("WRITELOG", $this->controller, CardLink("magmatic_carapace", "magmatic_carapace") . " created a " . CardLink("seismic_surge", "seismic_surge"), 1);
     AddDecisionQueue("PLAYAURA", $this->controller, "seismic_surge", 1);
+  }
+}
+
+class frosthaven_sheath extends Card
+{
+  function __construct($controller)
+  {
+    $this->cardID = "frosthaven_sheath";
+    $this->controller = $controller;
+  }
+
+  function AbilityType($index = -1, $from = '-')
+  {
+    return "DR";
+  }
+
+  function AbilityCost()
+  {
+    return 1;
+  }
+
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
+  {
+    global $defPlayer, $mainPlayer;
+    if (SearchCardList($additionalCosts, $this->controller, talent: "ICE") != "")
+      PlayAura("frostbite", $mainPlayer, effectController: $defPlayer);
+  }
+}
+
+class leaven_sheath extends Card
+{
+  function __construct($controller)
+  {
+    $this->cardID = "leaven_sheath";
+    $this->controller = $controller;
+  }
+
+  function AbilityType($index = -1, $from = '-')
+  {
+    return "DR";
+  }
+
+  function AbilityCost()
+  {
+    return 1;
+  }
+
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
+  {
+    global $defPlayer;
+    if (SearchCardList($additionalCosts, $this->controller, talent: "EARTH") != "")
+      PlayAura("embodiment_of_earth", $defPlayer);
+  }
+}
+
+class stormwind_sheath extends Card
+{
+  function __construct($controller)
+  {
+    $this->cardID = "stormwind_sheath";
+    $this->controller = $controller;
+  }
+
+  function AbilityType($index = -1, $from = '-')
+  {
+    return "DR";
+  }
+
+  function AbilityCost()
+  {
+    return 1;
+  }
+
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
+  {
+    global $defPlayer;
+    if (SearchCardList($additionalCosts, $this->controller, talent: "LIGHTNING") != "")
+      PlayAura("embodiment_of_lightning", $defPlayer);
   }
 }
 ?>
