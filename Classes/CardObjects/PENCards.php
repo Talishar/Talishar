c46c621c2a3574f15c32c34bc0e3efbb7340ec11
Paul Gibby <pgibby8@hotmail.com>
2025-11-17 10:47:33 -0500

    fix grimoire of fellingsong

--- Classes/CardObjects/PENCards.php
+++ Classes/CardObjects/PENCards.php
@@ -64,7 +64,7 @@ class grimoire_of_fellingsong extends Card
 
   function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1)
   {
-    PlayAura("ponder", $this->controller, 1, true);
+    PlayAura("runechant", $this->controller, 1, true);
   }
 
   function EquipPayAdditionalCosts($cardIndex = '-') {
