e9690d20982429fd9f1574aeb9d35bd5cacce061
PvtVoid <jotrickr@gmail.com>
2025-11-23 13:48:08 +0100

    Fix missing space on arcane barrier message

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -1295,7 +1295,7 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
         $caption = "";
         if ($turnPhase == "CHOOSEARCANE") {
           $vars = explode("-", $dqVars[0]);
-          $caption .= "Source: " . CardLink($vars[1], $vars[1]) . "&nbsp | &nbspTotal Damage: " . $vars[0];
+          $caption .= "Source: " . CardLink($vars[1], $vars[1]) . "&nbsp | &nbspTotal Damage: " . $vars[0] . "&nbsp | &nbsp";
           if(!CanDamageBePrevented($playerID, $vars[0], "ARCANE", $vars[1])) {
             $caption .= "&nbsp | &nbsp <span style='font-size: 0.8em; color:red;'>**WARNING: THIS DAMAGE IS UNPREVENTABLE**</span><br>";
           } else {
