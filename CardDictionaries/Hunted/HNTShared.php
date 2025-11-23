ed5f165dc2ccad45291d390034c410be3874cfc2
PvtVoid <jotrickr@gmail.com>
2025-11-23 10:35:09 +0100

    Add anaphylactic_shock_blue ally damage to stats

--- CardDictionaries/Hunted/HNTShared.php
+++ CardDictionaries/Hunted/HNTShared.php
@@ -369,7 +369,10 @@ function HNTPlayAbility($cardID, $from, $resourcesPaid, $target = "-", $addition
       if (GetClassState($otherPlayer, $CS_DamageDealtToOpponent)) LoseHealth(1, $otherPlayer);
       $allies = GetAllies($otherPlayer);
       for ($j = 0; $j < count($allies); $j += AllyPieces()) {
-        if ($allies[$j + 10] > 0) --$allies[$j+2];
+        if ($allies[$j + 10] > 0) {
+          LogDamageStats($otherPlayer, 0, 1);
+          --$allies[$j+2];
+        }
         if ($allies[$j+2] == 0) DestroyAlly($otherPlayer, $j);
       }
       break;
