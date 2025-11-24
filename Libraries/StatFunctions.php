2145e9d57fa9d7929edfb3e53db70ef9090faf5e
PvtVoid <jotrickr@gmail.com>
2025-11-24 15:00:04 +0100

    Remove unneeded LogDamagePreventedStats

--- Libraries/StatFunctions.php
+++ Libraries/StatFunctions.php
@@ -91,7 +91,7 @@ function LogResourcesUsedStats($player, $resourcesUsed)
 function LogDamageStats($player, $damageThreatened, $damageDealt)
 {
   global $currentTurn, $TurnStats_DamageThreatened, $TurnStats_DamageDealt;
-  //WriteLog("Logging damage stats for player $player: Threatened $damageThreatened, Dealt $damageDealt");
+  //WriteLog("Logging damage stats for player $player: Threatened $damageThreatened, Dealt $damageDealt", highlight:true);
   $baseIndex = ($currentTurn-1) * TurnStatPieces();
   $damagerStats = &GetTurnStats($player == 1 ? 2 : 1);
   if(count($damagerStats) <= $baseIndex) StatsStartTurn();
@@ -120,6 +120,7 @@ function LogLifeLossStats($player, $healthLost)
 function LogDamagePreventedStats($player, $damagePrevented)
 {
   global $currentTurn, $TurnStats_DamagePrevented;
+  //WriteLog("Player $player stats added: $damagePrevented damage prevented", highlight:true);
   $baseIndex = ($currentTurn-1) * TurnStatPieces();
   $preventedStats = &GetTurnStats($player);
   if(count($preventedStats) <= $baseIndex) StatsStartTurn();
