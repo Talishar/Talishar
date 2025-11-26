b6c5d1ab5f0199f1654f342cc254f6605d3dea4d
PvtVoid <jotrickr@gmail.com>
2025-11-26 15:57:59 +0100

    Fix LogPlayCardStats for EFFECTHITEFFECT being placed too low and only checking effect that get removed

--- CardLogic.php
+++ CardLogic.php
@@ -1989,12 +1989,12 @@ function ProcessTrigger($player, $parameter, $uniqueID, $target = "-", $addition
   }
   elseif ($additionalCosts == "EFFECTHITEFFECT") {
     if(isset($combatChain) && count($combatChain) > 2) {
+      $index = FindCurrentTurnEffectIndex($player, $target);
+      // Remove "-string" and "string-" suffixes from the effect ID
+      $effectID = preg_replace('/-[^-]*$/', '', $currentTurnEffects[$index]);
+      LogPlayCardStats($player, $effectID, "CC", "HIT");
       if (EffectHitEffect($target, $combatChain[2], $uniqueID, effectSource:$combatChain[0])) {
-        $index = FindCurrentTurnEffectIndex($player, $target);
         if ($index != -1) {
-          // Remove "-string" and "string-" suffixes from the effect ID
-          $effectID = preg_replace('/-[^-]*$/', '', $currentTurnEffects[$index]);
-          LogPlayCardStats($player, $effectID, "CC", "HIT");
           RemoveCurrentTurnEffect($index);
         }
       }
