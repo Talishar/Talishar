c7e0badf4359183007ffb12c1f5d8d2070d9edf1
PvtVoid <jotrickr@gmail.com>
2025-12-02 16:03:06 +0100

    Merge pull request #1329 from timtreahy/modular-workflow-update

--- GetNextTurn.php
+++ GetNextTurn.php
@@ -725,7 +725,7 @@ if ($lastUpdate != 0 && $cacheVal <= $lastUpdate) {
       $weaponPowerModifiers = [];
     if (!$playable) {
         if (MainCharacterPowerModifiers($weaponPowerModifiers, $i, true, $playerID) > 0 ||
-            SearchCurrentTurnEffectsForPartielID($myCharacter[$i + 11] ?? "-")) {
+            SearchCurrentTurnEffectsForPartialId($myCharacter[$i + 11] ?? "-")) {
             $border = 5;
         }
     }
