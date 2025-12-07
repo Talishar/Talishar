a39ccfe4b45f48501ab01cf6f8fd7207e9d67a0b
Timothy Treahy <timothytreahyii@gmail.com>
2025-12-07 14:41:35 -0500

    Fix Singularity and Nitro Mechanoid

--- MenuFiles/StartHelper.php
+++ MenuFiles/StartHelper.php
@@ -19,6 +19,7 @@ function initializePlayerState($handler, $deckHandler, $player)
   $hero = "";
   for ($i = 0; $i < count($charEquip); ++$i) {
     if(TypeContains($charEquip[$i], "C")) $hero = $charEquip[$i];
+    if (IsModular($charEquip[$i])) $charEquip[$i] = "NONE00";
     fwrite($handler, $charEquip[$i] . " 2 0 0 0 " . CharacterNumUsesPerTurn($charEquip[$i]) . " 0 0 0 " . CharacterDefaultActiveState($charEquip[$i]) . " - " . GetUniqueId() . " " . HasCloaked($charEquip[$i], hero:$hero) . " 0 0" . ($i < count($charEquip) - 1 ? " " : "\r\n"));
   }
   //Character and equipment. First is ID. Four numbers each. First is status (0=Destroy/unavailable, 1=Used, 2=Unused, 3=Disabled). Second is num counters
