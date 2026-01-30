f00f29420f5ee50d181251ad312942f8fbb94852
OotTheMonk <nathanandrecoder@gmail.com>
2026-01-30 08:56:51 -0600

    Require auth key for submit chat endpoint

--- SubmitChat.php
+++ SubmitChat.php
@@ -41,7 +41,7 @@ if ($authKey == "") $authKey = isset($_COOKIE["lastAuthKey"]) ? $_COOKIE["lastAu
 $targetAuthKey = "";
 if ($playerID == 1 && $sessionP1AuthKey !== null) $targetAuthKey = $sessionP1AuthKey;
 else if ($playerID == 2 && $sessionP2AuthKey !== null) $targetAuthKey = $sessionP2AuthKey;
-if ($targetAuthKey != "" && $authKey != $targetAuthKey) exit;
+if ($authKey != $targetAuthKey) exit;
 
 $uid = "-";
 if ($sessionUserUid !== null) $uid = $sessionUserUid;
