b7d4f024b5bb7584ba8cd8c6cdb0de1b50ecc656
PvtVoid <jotrickr@gmail.com>
2026-02-12 11:45:16 +0100

    PEN launch day!

--- APIs/JoinGame.php
+++ APIs/JoinGame.php
@@ -757,7 +757,7 @@ function isSpecialUsePromo($cardID) {
       "good_deeds_don't_go_unnoticed_yellow", "pink_visor", "diamond_hands", "hummingbird_call_of_adventure", "shitty_xmas_present_yellow", "squizzy_&_floof",
       "fabric_of_spring_yellow", "venomback_fabric_yellow", "silversheen_needle", "bank_breaker"
   ];
-  $unreleasedSets = ["AHA", "PEN"];
+  $unreleasedSets = ["AHA"];
   // promos that are made legal ahead of thier set
   $specialReleases = ["batter_to_a_pulp_red"];
   if (in_array($cardID, $specialReleases)) return false;
@@ -768,9 +768,6 @@ function isUnimplemented($cardID) {
   // by default cards from new sets are unimplemented
   switch (CardSet($cardID)) {
     case "AHA":
-      $card = GetClass($cardID, 0);
-      return $card == "-";
-    case "PEN":
       $card = GetClass($cardID, 0);
       return $card == "-";
     default:
