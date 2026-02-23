8889104e6fe35a46e09f979d3ccf63d90c12b02f
Paul Gibby <pgibby8@hotmail.com>
2026-02-23 17:09:24 -0500

    trying something

--- SubmitChat.php
+++ SubmitChat.php
@@ -60,7 +60,7 @@ if (in_array($origin, $allowedOrigins)) {
 $targetAuthKey = "";
 if ($playerID == 1 && $sessionP1AuthKey !== null) $targetAuthKey = $sessionP1AuthKey;
 else if ($playerID == 2 && $sessionP2AuthKey !== null) $targetAuthKey = $sessionP2AuthKey;
-if ($authKey !== $targetAuthKey) {
+if ($authKey !== $targetAuthKey && $targetAuthKey != "") {
   if (isset($_COOKIE["lastAuthKey"])) $authKey = $_COOKIE["lastAuthKey"];
   if ($authKey !== $targetAuthKey) {
     http_response_code(403);
