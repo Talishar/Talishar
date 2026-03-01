e819c562c657f4338d615dbb45e4f2366eafb1b8
Paul Gibby <pgibby8@hotmail.com>
2026-03-01 08:21:24 -0500

    fix bios update erase face

--- CardDictionaries/ArcaneRising/ARCMechanologist.php
+++ CardDictionaries/ArcaneRising/ARCMechanologist.php
@@ -172,7 +172,7 @@ function DoBoost($player, $cardID, $boostCount=1)
       //give it the uid of the banished card as a target
       AddLayer("TRIGGER", $player, "hyper_x3", $banish[$topInd + 2]);
     }
-    if(SubtypeContains($boostedCardID, "Item") && SearchCurrentTurnEffects("bios_update_red-2", $player, true)) {
+    if(SubtypeContains($boostedCardID, "Item") && CardCost($boostedCardID) && ClassContains($boostedCardID, "MECHANOLOGIST", $player) && SearchCurrentTurnEffects("bios_update_red-2", $player, true)) {
       //give it the uid of the banished card as a target
       AddLayer("TRIGGER", $player, "bios_update_red", $banish[$topInd + 2]);
     }
