8428e797f970e181a1a7b0887e9d0c8b7b1f94a4
Paul Gibby <pgibby8@hotmail.com>
2026-03-03 19:25:02 -0500

    young twoscilio

--- Classes/CardObjects/OMNCards.php
+++ Classes/CardObjects/OMNCards.php
@@ -1,12 +1,7 @@
 <?php
 
-class oscilio_forked_continuum extends Card {
-  function __construct($controller) {
-    $this->cardID = "oscilio_forked_continuum";
-    $this->controller = $controller;
-  }
-  
-  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1) {
+class twoscilio extends BaseCard {
+  function PlayAbility() {
     PummelHit($this->controller, track:true);
     Await($this->controller, $this->cardID, lastResultName:"discarded", final:true);
     PlayAura("ponder", $this->controller);
@@ -21,7 +16,7 @@ class oscilio_forked_continuum extends Card {
     if (TypeContains($Card->ID(), "I", $this->controller)) AddCurrentTurnEffect($this->cardID, $this->controller, uniqueID:$discardedUID);
   }
 
-  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false) {
+  function IsPlayRestricted($index) {
     $CharacterCard = new CharacterCard($index, $this->controller);
     if ($CharacterCard->Tapped()) return true;
     $Auras = new Auras($this->controller);
@@ -29,12 +24,72 @@ class oscilio_forked_continuum extends Card {
     return false;
   }
 
-  function PayAdditionalCosts($from, $index = '-') {
+  function PayAdditionalCosts($index = '-') {
     $CharacterCard = new CharacterCard($index, $this->controller);
     $CharacterCard->Tap();
     $Auras = new Auras($this->controller);
     $Flow = $Auras->FindCardID("lightning_flow");
     $Flow->Destroy();
+  }
+}
+
+class oscilio_forked_continuum extends Card {
+  function __construct($controller) {
+    $this->cardID = "oscilio_forked_continuum";
+    $this->controller = $controller;
+    $this->baseCard = new twoscilio($this->cardID, $this->controller);
+  }
+  
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1) {
+    return $this->baseCard->PlayAbility();
+  }
+
+  function SpecificLogic() {
+    return $this->baseCard->SpecificLogic();
+  }
+
+  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false) {
+    return $this->baseCard->IsPlayRestricted($index);
+  }
+
+  function PayAdditionalCosts($from, $index = '-') {
+    return $this->baseCard->PayAdditionalCosts($index);
+  }
+
+  function AbilityType($index = -1, $from = '-') {
+    return "I";
+  }
+
+  function AbilityCost() {
+    return 1;
+  }
+
+  function SpecialType() {
+    return "C";
+  }
+}
+
+class oscilio_scion_of_the_third_age extends Card {
+  function __construct($controller) {
+    $this->cardID = "oscilio_scion_of_the_third_age";
+    $this->controller = $controller;
+    $this->baseCard = new twoscilio($this->cardID, $this->controller);
+  }
+  
+  function PlayAbility($from, $resourcesPaid, $target = '-', $additionalCosts = '-', $uniqueID = '-1', $layerIndex = -1) {
+    return $this->baseCard->PlayAbility();
+  }
+
+  function SpecificLogic() {
+    return $this->baseCard->SpecificLogic();
+  }
+
+  function IsPlayRestricted(&$restriction, $from = '', $index = -1, $resolutionCheck = false) {
+    return $this->baseCard->IsPlayRestricted($index);
+  }
+
+  function PayAdditionalCosts($from, $index = '-') {
+    return $this->baseCard->PayAdditionalCosts($index);
   }
 
   function AbilityType($index = -1, $from = '-') {
