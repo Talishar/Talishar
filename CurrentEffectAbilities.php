0d46bf87d82cfba3d95f10018290ee6023579825
Paul Gibby <pgibby8@hotmail.com>
2026-04-09 21:47:43 -0400

    fix visual on steadfast

--- CurrentEffectAbilities.php
+++ CurrentEffectAbilities.php
@@ -1128,11 +1128,11 @@ function CurrentTurnEffectDamagePreventionAmount($player, $index, $damage, $type
       }
       break;
     case "steadfast_red":
-      return $source == $currentTurnEffects[$index + 2] && $preventable ? 6 : 0;
+      return $source == $currentTurnEffects[$index + 2] && $preventable ? $currentTurnEffects[$index + 3] : 0;
     case "steadfast_yellow":
-      return $source == $currentTurnEffects[$index + 2] && $preventable ? 5 : 0;
+      return $source == $currentTurnEffects[$index + 2] && $preventable ? $currentTurnEffects[$index + 3] : 0;
     case "steadfast_blue":
-      return $source == $currentTurnEffects[$index + 2] && $preventable ? 4 : 0;
+      return $source == $currentTurnEffects[$index + 2] && $preventable ? $currentTurnEffects[$index + 3] : 0;
     case "amulet_of_intervention_blue":
       return 1;
     case "helios_mitre":
