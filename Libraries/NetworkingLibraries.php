b19f8696d1cad38cba86716a4218cd8f5df158cc
PvtVoid <jotrickr@gmail.com>
2026-04-20 15:23:15 +0200

    Left the game log

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -865,6 +865,7 @@ function ProcessInput($playerID, $mode, $buttonInput, $cardID, $chkCount, $chkIn
     case 100001: //Main Menu
       if ($isSimulation)
         return;
+      WriteLog("Player $playerID has left the game.");
       header("Location: {$redirectPath}/MainMenu.php");
       exit;
     case 100002: //Concede
