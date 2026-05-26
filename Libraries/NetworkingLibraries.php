1a4e9c9bcf5bad8f6baeeb42aa75f9b0ba237e4e
PvtVoid <jotrickr@gmail.com>
2026-05-26 21:58:07 +0200

    Smaller turn breaklines

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -1977,7 +1977,7 @@ function FinalizeTurn()
   $MakeStartTurnBackup = true;
   $layerPriority[0] = ShouldHoldPriority(1);
   $layerPriority[1] = ShouldHoldPriority(2);
-  WriteSystemMessage("═════════════════");
+  WriteSystemMessage("═══════════════");
   if (!$extraTurn) WriteLog("Player $mainPlayer's turn $currentTurn has begun.");
   else WriteLog("Player $mainPlayer's extra turn $currentTurn has begun.");
   DoGamestateUpdate();
