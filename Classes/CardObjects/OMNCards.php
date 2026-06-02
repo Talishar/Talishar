872b8a567f4dd909f0aafebf3511b4abc6f6a4b1
PvtVoid <jotrickr@gmail.com>
2026-06-02 09:23:49 +0200

    Update OMNCards.php

--- Classes/CardObjects/OMNCards.php
+++ Classes/CardObjects/OMNCards.php
@@ -932,7 +932,7 @@ class aphrodias extends Card {
     $format = GetCachePiece($gameName, 13);
     if ($CharacterCard->Tapped()) return true;
     if (GetClassState($this->controller, $CS_HoloAurasEntered) == 0) return true;
-    if($format != FORMAT_SEALED && $format != FORMAT_DRAFT && $format != FORMAT_OPEN) {
+    if($format != FORMAT_SEALED && $format != FORMAT_DRAFT) {
       $hand = &GetHand($this->controller);
       $resources = &GetResources($this->controller);
       if (Count($hand) == 0 && $resources[0] == 0) return true;
