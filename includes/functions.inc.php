410f9a52f65bb03c9350a2f6fc159234b1ec186d
PvtVoid <jotrickr@gmail.com>
2026-06-03 22:05:45 +0200

    Fix starting health sent to frontend

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -747,8 +747,9 @@ function SerializeGameResult($player, $DeckLink, $deckAfterSB, $gameID = "", $op
 	// Add opponent's hero if provided
 	if($opposingHero != "") {
 		$deck["opponentHero"] = $opposingHero;
+		$deck["opponentStartingLife"] = CharacterHealth($opposingHero);
 	}
-	
+
 	$deck["cardResults"] = [];
 	$deck["character"] = [];
 
@@ -873,6 +874,7 @@ function SerializeDetailedGameResult($player, $DeckLink, $deckAfterSB, $gameID =
 	if($winner == "1" || $winner == "2") $deck["winner"] = intval($winner);
 	$deck["firstPlayer"] = ($player == $firstPlayer ? 1 : 0);
 	if($opposingHero != "") $deck["opposingHero"] = $opposingHero;
+	if($opposingHero != "") $deck["opponentStartingLife"] = intval(CharacterHealth($opposingHero));
 	if($playerHero != "") $deck["playerHero"] = $playerHero;
 	if($playerHero != "") $deck["startingLife"] = intval(CharacterHealth($playerHero));
 	if($deckbuilderID != "") $deck["deckbuilderID"] = $deckbuilderID;
