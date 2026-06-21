c05aca81afb06b07f7b9914edf26f9402f44cafa
PvtVoid <jotrickr@gmail.com>
2026-06-21 16:01:33 +0200

    Fix string to int operator #2

--- StartEffects.php
+++ StartEffects.php
@@ -79,8 +79,8 @@ handleCogwerxEquipment($p1Char, $p2Char);
 
 //Macro for Limited
 if($format == "draft"){
-  AddDecisionQueue("PASSPARAMETER", "-", "omens_of_arcana");
-  AddDecisionQueue("PUTPLAY", 1, "-", 1);
+  AddDecisionQueue("PASSPARAMETER", $mainPlayer, "omens_of_arcana");
+  AddDecisionQueue("PUTPLAY", $mainPlayer, "-", 1);
 }
 
 // Add final game setup decisions
