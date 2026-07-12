61b47b65acd0381527a90b590052d4193f0096cb
PvtVoid <jotrickr@gmail.com>
2026-07-12 20:36:15 +0200

    Use a dynamic divider for the chatbox to fit better on all screen

--- Libraries/NetworkingLibraries.php
+++ Libraries/NetworkingLibraries.php
@@ -2038,9 +2038,7 @@ function FinalizeTurn()
   $MakeStartTurnBackup = true;
   $layerPriority[0] = ShouldHoldPriority(1);
   $layerPriority[1] = ShouldHoldPriority(2);
-  WriteSystemMessage("═══════════════");
-  if (!$extraTurn) WriteLog("Player $mainPlayer's turn $currentTurn has begun.");
-  else WriteLog("Player $mainPlayer's extra turn $currentTurn has begun.");
+  WriteSystemMessage("[[TURN_START:$currentTurn:$mainPlayer]]");
   DoGamestateUpdate();
   ProcessDecisionQueue();
 }
