858a2acd5a8104063754c994f9ac7801bf1a9e1f
Paul Gibby <pgibby8@hotmail.com>
2026-08-11 19:16:09 -0400

    add set ids for mortimer

--- CardDictionary.php
+++ CardDictionary.php
@@ -256,7 +256,9 @@ function SetID($cardID)
       "wrenchtastic" => "DUM", // AI custom weapon
       "UPR551" => "UPR551", //ghostly touch
       "zane_broadly_beloved" => "SPW003", //temporary while waiting on fabcube
-      "killjoy_the_crooked_blade" => "SPW001"
+      "killjoy_the_crooked_blade" => "SPW001",
+      "dr_mortimer" => "MPA003",
+      "dr_mortimer_blight_of_the_pits" => "MPA002"
     ];
   }
 
